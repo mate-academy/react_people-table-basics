8484109a48452354881ff4dfb53f32f46a9dd9d6
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-11-16 10:16:56 +0300

    solution

--- src/components/PersonRow/PersonRow.jsx
+++ src/components/PersonRow/PersonRow.jsx
@@ -3,13 +3,13 @@ import './PersonRow.scss';
 import { PersonRowShape } from '../../shapes/PersonRowShape';
 
 export const PersonRow = ({ person }) => (
-  <tr className="PersonRow">
-    <td className="PersonRow__cell">{person.name}</td>
-    <td className="PersonRow__cell">{person.sex}</td>
-    <td className="PersonRow__cell">{person.born}</td>
-    <td className="PersonRow__cell">{person.died}</td>
-    <td className="PersonRow__cell">{person.motherName}</td>
-    <td className="PersonRow__cell">{person.fatherName}</td>
+  <tr className="Person">
+    <td className="Person__cell">{person.name}</td>
+    <td className="Person__cell">{person.sex}</td>
+    <td className="Person__cell">{person.born}</td>
+    <td className="Person__cell">{person.died}</td>
+    <td className="Person__cell">{person.motherName}</td>
+    <td className="Person__cell">{person.fatherName}</td>
   </tr>
 );
 
