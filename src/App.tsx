f05d911ba5ae91a28d978cd81593e028e286e7b8
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-12-23 14:29:55 +0200

    Fixed Soluion

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import {
+  HashRouter, Redirect, Route, Switch,
+}
+  from 'react-router-dom';
 import 'bulma';
 import '@fortawesome/fontawesome-free';
 import './App.scss';
@@ -8,25 +11,28 @@ import { Header } from './components/Header';
 import { HomePage } from './components/HomePage';
 import { PeoplePage } from './components/PeoplePage';
 import { NotFoundPage } from './components/NotFoundPage';
-// import { NotFoundPage } from './components/NotFoundPage';
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Switch>
-      <Route path="/people">
-        <PeoplePage />
-      </Route>
+const App = () => {
+  return (
+    <HashRouter>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/people">
+            <PeoplePage />
+          </Route>
 
-      <Route path="/" exact>
-        <HomePage />
-      </Route>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
 
-      <Redirect path="/home" to="/" />
+          <Redirect path="/home" to="/" />
 
-      <NotFoundPage />
-    </Switch>
-  </div>
-);
+          <NotFoundPage />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
+};
 
 export default App;
