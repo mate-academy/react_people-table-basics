ee928017a124f78787ce148a43b7d237797f228b
Home <Home@LT-259286.kyiv.ciklum.net>
2021-03-04 02:02:23 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.scss';
+import 'bulma';
+import { Header } from './components/Header';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+export const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <Header />
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <Switch>
+                <Route path='/' exact component={HomePage}></Route>
+                <Route path='/people' component={PeoplePage}></Route>
+                <Redirect path='/home' to='/' />
+                <p><NotFoundPage /></p>
+              </Switch>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+};
 
 export default App;
