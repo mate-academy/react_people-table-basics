0f310b0712893323672a148c9110c60a420c04e6
herkoss <dmytroherko@icloud.com>
2021-06-09 12:05:18 +0300

    Path fixed 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,12 +7,18 @@ export const Header = () => (
   <nav className="header__nav">
     <ul className="header__list">
       <li className="header__item">
-        <Link className="header__link" to="/">
+        <Link
+          className="header__link"
+          to="/react_people-table-basics/"
+        >
           Home
         </Link>
       </li>
       <li>
-        <Link className="header__link" to='/people'>
+        <Link
+          className="header__link"
+          to='/react_people-table-basics/people'
+        >
           People
         </Link>
       </li>
