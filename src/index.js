3f7ec793b0fc7fefdf81c06009687d40c2109133
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-28 14:15:45 +0300

    Added homepage attr to package.json

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter basename="/">
+  <BrowserRouter basename="/">
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root'),
 );
