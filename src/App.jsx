88713f59759c96597b221c3fee233a4d684e7740
Artem Yemelyanov <ae1666528@gmail.com>
2021-07-28 13:41:55 +0300

    Forgot to add exact attribute

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const App = () => (
             <HomePage />
           </Route>
 
-          <Redirect path="/" to="/home" />
+          <Redirect path="/" to="/home" exact/>
 
           <NotFoundPage />
         </Switch>
