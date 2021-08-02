0fb8939e580a686a2352d66d292b3f9a9e1cdda9
Artem Yemelyanov <ae1666528@gmail.com>
2021-08-02 15:07:42 +0300

    Made optimization

--- src/components/PeopleTable/PeopleTable.jsx
+++ src/components/PeopleTable/PeopleTable.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { nanoid } from 'nanoid';
 import { PersonRow } from '../PersonRow';
 import { PersonType } from '../../types';
 
 import 'bulma';
 import './PeopleTable.scss';
 
-export const PeopleTable = ({ people }) => (
-  <table className="PeopleTable">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Sex</th>
-        <th>Born</th>
-        <th>Died</th>
-        <th>Mother</th>
-        <th>Father</th>
-      </tr>
-    </thead>
-    <tbody>
-      {people.map(person => (
-        <PersonRow
-          key={nanoid()}
-          person={person}
-        />
-      ))}
-    </tbody>
-  </table>
+export const PeopleTable = React.memo(
+  ({ people }) => (
+    <table className="PeopleTable">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+      <tbody>
+        {people.map(person => (
+          <PersonRow
+            key={person.slug}
+            person={person}
+          />
+        ))}
+      </tbody>
+    </table>
+  )
 );
 
 PeopleTable.propTypes = {
