2142698cc102059b062585621624039d8c9fbcd9
John <jozaman96@gmail.com>
2021-08-02 18:56:52 +0300

    fixing bugs

--- src/components/PeopleTable/PeopleTable.jsx
+++ src/components/PeopleTable/PeopleTable.jsx
@@ -3,35 +3,37 @@ import PropTypes, { shape } from 'prop-types';
 import { PersonRow } from '../PersonRow';
 import { PersonShape } from '../../types';
 
-export const PeopleTable = ({ people }) => (
-  <table className="table">
-    <thead className="table-dark">
-      <tr>
-        <th scope="col">#</th>
-        <th scope="col">Name</th>
-        <th scope="col">Sex</th>
-        <th scope="col">Born</th>
-        <th scope="col">Died</th>
-        <th scope="col">Father</th>
-        <th scope="col">Mother</th>
-      </tr>
-    </thead>
-    <tbody>
-      {people.map((person, index) => (
-        <tr key={person.slug} className="table-info">
-          <PersonRow
-            id={index + 1}
-            name={person.name}
-            sex={person.sex}
-            born={person.born}
-            died={person.died}
-            father={person.father}
-            mother={person.mother}
-          />
+export const PeopleTable = React.memo(
+  ({ people }) => (
+    <table className="table">
+      <thead className="table-dark">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Sex</th>
+          <th scope="col">Born</th>
+          <th scope="col">Died</th>
+          <th scope="col">Father</th>
+          <th scope="col">Mother</th>
         </tr>
-      ))}
-    </tbody>
-  </table>
+      </thead>
+      <tbody>
+        {people.map((person, index) => (
+          <tr key={person.slug} className="table-info">
+            <PersonRow
+              id={index + 1}
+              name={person.name}
+              sex={person.sex}
+              born={person.born}
+              died={person.died}
+              father={person.father}
+              mother={person.mother}
+            />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  ),
 );
 
 PeopleTable.propTypes = {
--- src/components/PersonRow/PersonRow.jsx
+++ src/components/PersonRow/PersonRow.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
 import { PersonShape } from '../../types';
 
-export const PersonRow = ({
-  id,
-  name,
-  sex,
-  born,
-  died,
-  father,
-  mother,
-}) => (
-  <>
-    <th scope="row">{id}</th>
-    <td>{name}</td>
-    <td>{sex}</td>
-    <td>{born}</td>
-    <td>{died}</td>
-    <td>{father}</td>
-    <td>{mother}</td>
-  </>
+export const PersonRow = React.memo(
+  ({
+    id,
+    name,
+    sex,
+    born,
+    died,
+    father,
+    mother,
+  }) => (
+    <>
+      <th scope="row">{id}</th>
+      <td>{name}</td>
+      <td>{sex}</td>
+      <td>{born}</td>
+      <td>{died}</td>
+      <td>{father}</td>
+      <td>{mother}</td>
+    </>
+  ),
 );
 
 PersonRow.propTypes = PersonShape;
