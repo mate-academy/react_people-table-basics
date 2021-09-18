30011c83dff092fa78c2344d9404727643b8a7a5
lera-yaskevych <redrum.back@gmail.com>
2021-09-18 14:10:32 +0300

    done

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,19 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+enum Sex {
+  m = 'm',
+  f = 'f',
+};
+
+interface Person {
+  name: string;
+  sex: Sex;
+  born: number;
+  died: number;
+  fatherName: string;
+  motherName: string;
+  slug: string;
+  mother: Person | null;
+  father: Person | null;
+}
