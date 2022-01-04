f686ebd281656002922151973689dcdb2399a644
Oleh Khashchevskyi <hashchevskyj@gmail.com>
2022-01-04 13:35:30 +0200

    edit some

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter>
+  <Router>
     <App />
-  </HashRouter>,
+  </Router>,
   document.getElementById('root'),
 );
