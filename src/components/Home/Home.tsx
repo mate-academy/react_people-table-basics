112fe3a8c59f3c101ea9094921f3c7ca47ad3652
Svyatoslav Victor <victorsvyatoslav@gmail.com>
2022-01-06 16:42:18 +0200

    Task Done

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+
+export const Home: React.FC = () => {
+  return (
+    <>
+      <div className="homepage">
+        <h1 className="homepage__title">
+          This is the Home Page
+        </h1>
+      </div>
+    </>
+  );
+};
