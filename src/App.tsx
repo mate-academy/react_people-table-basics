0f2d2d14174aec1ef9c89ce607521930eb8a5b26
Artem Fedoseev <afedoseev359@gmail.com>
2022-01-10 19:39:47 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-
-import './App.scss';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Nav } from './Components/Nav';
+import { HomePage } from './Components/HomePage';
+import { PeoplePage } from './Components/PeoplePage';
+import { PageNotFound } from './Components/PageNotFound';
 
 const App = () => (
   <div className="App">
-    <h1>People table</h1>
+    <Nav />
+    <Routes>
+      <Route path="/people" element={<PeoplePage />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/home" element={<Navigate to="/" />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
   </div>
 );
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,15 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+interface Person {
+  name: string,
+  sex: string,
+  born: number,
+  died: number,
+  motherName: null | string,
+  fatherName: null | string,
+}
+
+interface ProcessedPerson extends Person {
+  father: null | Person,
+  mother: null | Person,
+}
