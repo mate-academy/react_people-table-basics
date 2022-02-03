258341340db5dc2108ecc7af81ddd0e975317810
Serhii Dvorets <dvorets.sergey@gmail.com>
2022-02-03 17:25:50 +0200

    2

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root'),
 );
