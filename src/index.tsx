d8240752eb0cdac5f5f83fdbb8680b563950c818
Samson Sim <samson.sim@ukr.net>
2022-02-04 00:38:05 +0200

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+/* eslint-disable react/jsx-filename-extension */
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
