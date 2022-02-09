918ee153a0798cf062a4186f3f7de4614e5eb903
Olesya Parfylo <olesya.parfylo@gmail.com>
2022-02-09 13:36:55 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { MainNaviganion } from './components/MainNaviganion';
+import { HomePage } from './pages/HomePage';
+import { PeoplePage } from './pages/PeoplePage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { PeopleProvider } from './PeopleContext';
 
 import './App.scss';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App: React.FC = () => {
+  return (
+    <PeopleProvider>
+      <div className="App">
+        <MainNaviganion />
+
+        <section className="section">
+          <div className="container">
+            <h1 className="title">Main title</h1>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/people" element={<PeoplePage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </div>
+        </section>
+      </div>
+    </PeopleProvider>
+  );
+};
 
 export default App;
