d42bb77a28ccdf7bcfbe6c59ee145c9a66439ec4
Bohdan Maksymiuk <bohdan.max@ukr.net>
2022-02-11 22:52:55 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import { FC } from 'react';
+import {
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from 'react-router-dom';
 
 import './App.scss';
+import { PeopleTable } from './components/PeopleTable';
 
-const App = () => (
+const App: FC = () => (
   <div className="App">
-    <h1>People table</h1>
+    <header>
+      <nav className="App__nav nav box">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <Link to="/">Home</Link>
+          </li>
+
+          <li className="nav__item">
+            <Link to="/people">People</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <Routes>
+      <Route
+        path="/"
+        element={(
+          <h2 className="title is-2 has-text-centered">
+            Home page
+          </h2>
+        )}
+      />
+
+      <Route
+        path="/people"
+        element={(
+          <>
+            <h2 className="title is-2 has-text-centered">People table</h2>
+            <PeopleTable />
+          </>
+        )}
+      />
+
+      <Route
+        path="/home"
+        element={<Navigate to="/" replace />}
+      />
+
+      <Route
+        path="*"
+        element={(
+          <h2 className="title is-2 has-text-centered">
+            <h2>Page not found</h2>
+          </h2>
+        )}
+      />
+    </Routes>
   </div>
 );
 
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,24 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+type PersonFromServer = {
+  name: string,
+  sex: string,
+  born: number,
+  died: number,
+  fatherName: string,
+  motherName: string,
+  slug: string,
+};
+
+type Person = {
+  name: string,
+  sex: string,
+  born: number,
+  died: number,
+  fatherName: string,
+  motherName: string,
+  slug: string,
+  mother: PersonFromServer | null,
+  father: PersonFromServer | null,
+};
