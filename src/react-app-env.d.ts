b880b907711434f3f6e5c9963b849d17bb414d37
SavelovSerhii <savelov12111@gmail.com>
2022-03-25 16:50:42 +0200

    First version

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,12 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+interface Person {
+  name: string,
+  sex: string,
+  born: number,
+  died: number,
+  fatherName: string | null,
+  motherName: string | null,
+  slug: string,
+}
