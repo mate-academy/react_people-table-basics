ff46f0cc6208c48e9b30b6de5706f203c1982287
Kozhukhovskyi Roman <4kroman@gmail.com>
2022-04-05 18:02:29 +0300

    solution

--- src/Components/PeoplePage/PeoplePage.tsx
+++ src/Components/PeoplePage/PeoplePage.tsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../../api/api';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
+
+function addParents(people: Person[]) {
+  const newPeople = people.map(person => {
+    if (person.motherName && person.fatherName) {
+      return {
+        ...person,
+        mother: people.find(human => human.name === person.motherName)
+          || null,
+        father: people.find(human => human.name === person.fatherName)
+          || null,
+      };
+    }
+
+    if (person.motherName && !person.fatherName) {
+      return {
+        ...person,
+        mother: people.find(human => human.name === person.motherName)
+          || null,
+      };
+    }
+
+    return {
+      ...person,
+      father: people.find(human => human.name === person.fatherName)
+          || null,
+    };
+  });
+
+  return newPeople;
+}
+
 export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    getPeople()
+      .then(data => setPeople(addParents(data)));
+  }, []);
+
   return (
-    <h1>People page</h1>
+    <>
+      <h1>People page</h1>
+      <PeopleTable people={people} />
+    </>
   );
 };
---
+++ src/Components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,32 @@
+import { PersonRow } from '../PersonRow/PersonRow';
+
+type Props = {
+  people: Person[],
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  return (
+    <div className="table-container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Sex</th>
+            <th>Born</th>
+            <th>Died</th>
+            <th>Father name</th>
+            <th>Mother name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.map(person => (
+            <tr key={person.slug}>
+              <PersonRow person={person} />
+            </tr>
+          ))}
+        </tbody>
+
+      </table>
+    </div>
+  );
+};
