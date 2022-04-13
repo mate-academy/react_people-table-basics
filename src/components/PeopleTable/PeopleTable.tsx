ba8f74f696788f9f079b3ddfe4a833bff538c90b
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-13 18:05:03 +0300

    comments were removed

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -3,8 +3,6 @@ import { FullPerson } from '../../types/Person';
 import { PersonRow } from '../PersonRow/PersonRow';
 
 export const PeopleTable: React.FC<Props> = ({ people }) => {
-  // eslint-disable-next-line no-console
-
   return (
     <table className="people__table table is-bordered">
       <thead>
