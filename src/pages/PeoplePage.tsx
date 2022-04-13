e3acac857cec59b43c541b1aa7ca59a1a8c9903b
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-04-13 18:02:47 +0300

    fixed I guess

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -1,44 +1,15 @@
 import { useEffect, useState } from 'react';
 import { getPeople } from '../api/people';
 import { PeopleTable } from '../components/PeopleTable/PeopleTable';
-import { FullPerson, Person } from '../types/Person';
-
-const findParent = (
-  child: Person,
-  arrayOfPeople: Person[],
-  sexOfParent: 'f' | 'm',
-) => {
-  const foundParent = arrayOfPeople.find(potentialParent => {
-    switch (sexOfParent) {
-      case 'f':
-        return potentialParent.name === child.motherName;
-      case 'm':
-        return potentialParent.name === child.fatherName;
-      default:
-        return null;
-    }
-  });
-
-  return foundParent || null;
-};
+import { preparePeople } from '../helpers/helpers';
+import { FullPerson } from '../types/Person';
 
 export const PeoplePage = () => {
   const [people, setPeople] = useState<FullPerson[]>([]);
 
-  const preparePeople = (arrOfPeople: Person[]): FullPerson[] => {
-    return arrOfPeople.map(person => ({
-      ...person,
-      mother: findParent(person, arrOfPeople, 'f'),
-      father: findParent(person, arrOfPeople, 'm'),
-    }));
-  };
-
   useEffect(() => {
     getPeople().then(loadedPeople => setPeople(preparePeople(loadedPeople)));
   }, []);
-
-  // eslint-disable-next-line no-console
-  console.log(people);
 
   return (
     <>
