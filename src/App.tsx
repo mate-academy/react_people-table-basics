6777e8077535de40024b8c798fa89ccc6aedac21
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2022-04-22 12:51:35 +0300

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -41,10 +41,9 @@ const App: React.FC = () => {
       </Header>
 
       <Routes>
+        <Route path="/people" element={<PeoplePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/" element={<Navigate replace to="/home" />} />
-
-        <Route path="/people" element={<PeoplePage />} />
 
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
