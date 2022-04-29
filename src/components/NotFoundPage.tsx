c23b7ab82a05f812f5e6e4eb6938437231908959
Ivan Shulhan <92580512+IvanShulhan@users.noreply.github.com>
2022-04-29 12:20:13 +0300

    solution

---
+++ src/components/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NotFoundPage: React.FC = () => (
+  <div className="NotFoundPag block">
+    <h2 className="title HomePage__title">
+      Page not found
+    </h2>
+  </div>
+);
---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { PersonRow } from './PersonRow';
+
+type Props = {
+  people: Person[]
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => (
+  <div className="wrapper">
+    <table className="PeopleTable
+    table is-hoverable
+    is-bordered is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th> Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th> Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+      <tbody>
+        {people.map((person) => (
+          <tr key={person.slug}>
+            <PersonRow person={person} />
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+
+);
