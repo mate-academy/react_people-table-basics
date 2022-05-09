8b5c585b4677956717833659ee79805df6d92681
KristinaTar <kristina.tarashchuk@gmail.com>
2022-05-09 20:43:33 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import './App.scss';
+import {
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import { Home } from './Home';
+import { People } from './People';
+import { NotFound } from './NotFound';
+import { Header } from './Header';
+// import { getPeople } from './api';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/people" element={<People />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
