949afc7a54c47710bb854c7d6fe2cc06c7df8104
KristinaTar <kristina.tarashchuk@gmail.com>
2022-05-11 17:22:32 +0300

    added hashrouter and /home

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export const Header = () => {
   return (
     <div>
-      <Link to="/">
+      <Link to="/home">
         Home
       </Link>
       <Link to="/people">
