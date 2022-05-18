fa660e0074cca1e0548afb3eee8553c4b9a5577e
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-05-18 18:11:31 +0300

    Information about parents displaying was updated

--- src/components/PersonRow/PersonRow.tsx
+++ src/components/PersonRow/PersonRow.tsx
@@ -6,37 +6,54 @@ type PersonProps = {
 };
 
 const PersonRow: React.FC<PersonProps> = ({ person }) => {
+  const getPersonParentInfo
+    = (parentName: string, parentType: 'mother' | 'father') => {
+      if (!parentName) {
+        return 'Information is absent';
+      }
+
+      switch (parentType) {
+        case 'mother':
+          if (!person.mother) {
+            return `${parentName} (Detailed info is absent)`;
+          }
+
+          return `${parentName} | ${person.mother.born} - ${person.mother.died}`;
+
+        default:
+          if (!person.father) {
+            return `${parentName} (Detailed info is absent)`;
+          }
+
+          return `${parentName} | ${person.father.born} - ${person.father.died}`;
+      }
+    };
+
   return (
-    <>
-      <tr className="Person">
-        <td>
-          {person.name}
-        </td>
-        <td>
-          {
-            person.sex === 'm'
-              ? 'male'
-              : 'female'
-          }
-        </td>
-        <td>
-          {person.born}
-        </td>
-        <td>
-          {person.died}
-        </td>
-        <td>
-          {
-            person.mother
-              ? `${person.mother.name} | ${person.mother.born} - ${person.mother.died}`
-              : 'Information is absent'
-          }
-        </td>
-        <td>
-          {person.father ? person.father.name : 'Information is absent'}
-        </td>
-      </tr>
-    </>
+    <tr className="Person">
+      <td>
+        {person.name}
+      </td>
+      <td>
+        {
+          person.sex === 'm'
+            ? 'male'
+            : 'female'
+        }
+      </td>
+      <td>
+        {person.born}
+      </td>
+      <td>
+        {person.died}
+      </td>
+      <td>
+        {getPersonParentInfo(person.motherName, 'mother')}
+      </td>
+      <td>
+        {getPersonParentInfo(person.fatherName, 'father')}
+      </td>
+    </tr>
   );
 };
 
