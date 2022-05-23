eca8c763c557471d10188f72b114c0deae1dc4fd
Maksym Yaremenko <restartermord123@ukr.net>
2022-05-23 14:57:03 +0300

    HashRouter implementation

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
 
       <Route path="/people" element={<PeoplePage />} />
 
-      <Route path="/*" element={<NotFoundPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   </div>
 );
