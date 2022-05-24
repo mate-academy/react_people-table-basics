59309db3e81ed4a348d4ceb7944e1d55c748306d
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-05-24 17:51:12 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
+import {
+  Route,
+  Switch,
+  Redirect,
+  NavLink,
+} from 'react-router-dom';
 import './App.scss';
+import { HomePage } from './Components/HomePage/HomePage';
+import { NotFoundPage } from './Components/NotFoundPage/NotFoundPage';
+import { PeoplePage } from './Components/PeoplePage/PeoplePage';
 
 const App = () => (
-  <div className="App">
-    <h1>People table</h1>
+  <div className="app">
+    <nav className="app__nav">
+      <NavLink
+        exact
+        to="/"
+        className="app__nav--item"
+      >
+        Home page
+      </NavLink>
+      <NavLink
+        to="/people"
+        className="app__nav--item"
+      >
+        People page
+      </NavLink>
+    </nav>
+
+    <Switch>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/people" component={PeoplePage} />
+      <Redirect path="/home" to="/" />
+      <NotFoundPage />
+    </Switch>
   </div>
 );
 
