3ea4a61c58b0682be2fa831c05f4e9e22549e4bb
Kirill Polozenko <kir.polozenko@icloud.com>
2022-05-26 21:59:36 +0300

    add table basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
 import './App.scss';
+import { useRoutes } from 'react-router-dom';
+import { Home } from './components/Home/Home';
+import { PeoplePage } from './components/PeoplePage/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { Header } from './components/Header/Header';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App: React.FC = () => {
+  const routes = useRoutes([
+    {
+      path: '/',
+      element: <Home />,
+    },
+    {
+      path: '/people',
+      element: <PeoplePage />,
+    },
+    {
+      path: '*',
+      element: <NotFoundPage />,
+    },
+  ]);
+
+  return (
+    <div>
+      <Header />
+      {routes}
+    </div>
+  );
+};
 
 export default App;
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,11 @@
-// eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+type People = {
+  name: string;
+  sex: string;
+  born: string;
+  died: string;
+  motherName: string;
+  fatherName: string;
+  slug: string;
+};
