8e2a6e6189de38ca537a2111bf9374356d8b196c
Vlad Skoromnyi <VectraSton@gmail.com>
2022-05-26 12:30:08 +0300

    complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,64 @@
 import './App.scss';
+import {
+  FC,
+} from 'react';
+import {
+  NavLink,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { PeopleTable } from './pages/PeopleTable';
+import { NotFound } from './pages/NotFound';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App: FC = () => {
+  return (
+    <div className="App">
+      <nav className="App__navigation">
+        <NavLink
+          to="/home"
+          className={
+            ({ isActive }: { isActive: boolean }): string | undefined => {
+              return isActive
+                ? 'App__navigation-link active'
+                : 'App__navigation-link';
+            }
+          }
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/people"
+          className={
+            ({ isActive }: { isActive: boolean }): string | undefined => {
+              return isActive
+                ? 'App__navigation-link active'
+                : 'App__navigation-link';
+            }
+          }
+        >
+          People
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+
+        <Route
+          path="/home"
+          element={<HomePage />}
+        />
+
+        <Route path="/people" element={<PeopleTable />} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react';
+
+export const NotFound: FC = () => {
+  return (
+    <h2>Page not found</h2>
+  );
+};
