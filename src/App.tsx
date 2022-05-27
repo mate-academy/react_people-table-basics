38d37a566b75dcd6fda25e4058ae58ef5e60cab3
Misha Kravtsov <aniahucm@gmail.com>
2022-05-27 09:51:06 +0300

    add redirect from "/home" to "/"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/order */
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './components/Header';
 import { HomePage } from './components/HomePage';
 import { PeoplePage } from './components/PeoplePage';
@@ -17,6 +17,8 @@ const App = () => (
       <Route path="/people" element={<PeoplePage />} />
 
       <Route path="*" element={<NotFoundPage />} />
+
+      <Route path="/home" element={<Navigate to="/" replace />} />
     </Routes>
   </div>
 );
