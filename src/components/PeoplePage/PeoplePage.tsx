c9367c864e3e9bef30f3b2a61c93bd991f60cd87
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-29 12:08:44 +0300

    Add PeoplePage component

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const PeoplePage: React.FC = () => (
+  <section className="people-page">
+    <h2 className="people-page__title">
+      People page
+    </h2>
+  </section>
+);
