5b2c363f3126890153a368946216a73b722c10ef
Oleksii Fedorenko <alex4rest09@gmail.com>
2022-05-29 14:34:28 +0300

    implement links and HashRouter, rendering only titles on pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Link, Route, Routes } from 'react-router-dom';
+
 import './App.scss';
 
 const App = () => (
   <div className="App">
-    <h1>People table</h1>
+    <h1>Header</h1>
+
+    <nav>
+      <Link to="/">Home page</Link>
+      {' '}
+      <Link to="/people">People page</Link>
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<h1>Home page</h1>} />
+      <Route path="people" element={<h1>People page</h1>} />
+
+      <Route path="*" element={<h1>Home page</h1>} />
+    </Routes>
   </div>
 );
 
