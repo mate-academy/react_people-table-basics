b00f732a479c8fc54be1316843281786bb4a81bd
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-29 12:14:07 +0300

    Add styles to HomePage component

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export const HomePage: React.FC = () => (
-  <h2>
-    Home page
-  </h2>
+  <section className="home-page">
+    <h2 className="home-page__title">
+      Home page
+    </h2>
+  </section>
 );
