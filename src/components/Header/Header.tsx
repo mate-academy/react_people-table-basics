ad5aefa01c6fdd2d982c0b96332fc76537feeb13
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-29 16:03:37 +0300

    Add some styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,23 +3,24 @@ import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
 export const Header: React.FC = () => {
-  const activeItem = 'main-navigation__item--active';
+  const activeItem = 'main-navigation__item main-navigation__item--active';
+  const inActiveItem = 'main-navigation__item';
 
   return (
     <nav className="main-navigation">
       <ul className="main-navigation__list">
-        <li className="main-navigation__item">
+        <li>
           <NavLink
             to="/"
-            className={({ isActive }) => (isActive ? activeItem : undefined)}
+            className={({ isActive }) => (isActive ? activeItem : inActiveItem)}
           >
             Home
           </NavLink>
         </li>
-        <li className="main-navigation__item">
+        <li>
           <NavLink
             to="/people"
-            className={({ isActive }) => (isActive ? activeItem : undefined)}
+            className={({ isActive }) => (isActive ? activeItem : inActiveItem)}
           >
             People
           </NavLink>
--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import { PersonWithParents } from '../../types/Person';
+import { PersonRow } from '../PersonRow';
 import './PeopleTable.scss';
 
 type Props = {
   people: PersonWithParents[];
 };
 
-export const PeopleTable: React.FC<Props> = () => (
+export const PeopleTable: React.FC<Props> = ({ people }) => (
   <table className="people-table">
-    <thead>
-      <tr>
-        <th>name</th>
-        <th>sex</th>
-        <th>born</th>
-        <th>died</th>
-        <th>mother</th>
-        <th>father</th>
+    <thead className="people-table__head">
+      <tr className="people-table__row">
+        <th className="people-table__cell">name</th>
+        <th className="people-table__cell">sex</th>
+        <th className="people-table__cell">born</th>
+        <th className="people-table__cell">died</th>
+        <th className="people-table__cell">mother</th>
+        <th className="people-table__cell">father</th>
       </tr>
     </thead>
-    <tbody>...</tbody>
+    <tbody>
+      {
+        people.map((person, index) => (
+          <PersonRow
+            person={person}
+            key={person.slug + String(index)}
+          />
+        ))
+      }
+    </tbody>
   </table>
 );
