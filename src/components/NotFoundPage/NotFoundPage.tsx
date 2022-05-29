de191151c189db38eb944b0ad88cbd1ad3d66455
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-29 12:12:25 +0300

    Add NotFoundPage component

---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const NotFoundPage: React.FC = () => (
+  <section className="not-found-page">
+    <h2 className="not-found-page__title">
+      Not found page
+    </h2>
+  </section>
+);
