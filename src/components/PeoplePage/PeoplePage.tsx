fd33db49cde25bb2f41cfe3cd88910b048b0626d
Yegor Kochetkov <kegor.fx@gmail.com>
2022-05-29 13:46:00 +0300

    Implement in PeoplePage fetching people from server

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { getPeople } from '../../api/people';
+import { People, PeopleWithParents } from '../../types/People';
 import './PeoplePage.scss';
 
-export const PeoplePage: React.FC = () => (
-  <section className="people-page">
-    <h2 className="people-page__title">
-      People page
-    </h2>
-  </section>
-);
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<People[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const getPeopleFromServer = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const peopleFromServer = await getPeople();
+      const peopleWithParents: PeopleWithParents[]
+        = peopleFromServer.map((child) => {
+          const parent = peopleFromServer.find((person) => (
+            person.name === child.fatherName
+            || person.name === child.motherName
+          ));
+          const childWithParents: PeopleWithParents = { ...child };
+
+          if (parent && parent.sex === 'm') {
+            childWithParents.father = parent;
+          }
+
+          if (parent && parent.sex === 'f') {
+            childWithParents.mother = parent;
+          }
+
+          return childWithParents;
+        });
+
+      setPeople(peopleWithParents);
+    } catch {
+      setIsLoading(false);
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPeopleFromServer();
+  }, [isError]);
+
+  return (
+    <section className="people-page">
+      <h2 className="people-page__title">
+        People page
+      </h2>
+    </section>
+  );
+};
