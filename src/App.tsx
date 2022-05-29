99d1e61a97f54f5a2848ef51da4bf0c9b4c1e248
Viacheslav Petrovskyi <v.petrovskyi@icloud.com>
2022-05-29 13:16:00 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.scss';
-import { Routes, Route, Link } from 'react-router-dom';
+import {
+  Routes,
+  Route,
+  NavLink,
+  Navigate,
+} from 'react-router-dom';
 import { PeopleTable } from './components/PeopleTable';
 
 const App = () => (
   <div className="App">
     <nav className="nav">
-      <Link className="nav__link" to="/">Home</Link>
-      <Link className="nav__link" to="/people">People page</Link>
+      <NavLink className="nav__link" to="/">Home</NavLink>
+      <NavLink className="nav__link" to="/people">People page</NavLink>
     </nav>
 
     <Routes>
+      <Route path="/home" element={<Navigate to="/" />} />
       <Route path="/" element={<h1>Home page</h1>} />
       <Route path="/people" element={<PeopleTable />} />
 
