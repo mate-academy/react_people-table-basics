8e2b4675d83fda26074ebce09345440ab5487b0e
Ruslan Hubanov <ruslangubanov88@gmail.com>
2022-06-01 10:40:47 +0300

    fix bug, write with Routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
 import './App.scss';
-import { useRoutes } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { PeoplePage } from './components/PeoplePage/PeoplePage';
 import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 import { Header } from './components/Header/Header';
 
-const App: React.FC = () => {
-  const routes = useRoutes([
-    {
-      path: '/',
-      element: <Home />,
-    },
-    {
-      path: '/home',
-      element: <Home />,
-    },
-    {
-      path: '/people',
-      element: <PeoplePage />,
-    },
-    {
-      path: '*',
-      element: <NotFoundPage />,
-    },
-  ]);
-
-  return (
-    <div>
-      <Header />
-      {routes}
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/people" element={<PeoplePage />} />
+      <Route path="*" element={<NotFoundPage />} />
+      <Route path="/home" element={<Navigate to="/" replace />} />
+    </Routes>
+  </div>
+);
 
 export default App;
