21b3033ca393f4a5e102e37abfff331d8b403218
Valerii Derkach <valerij.derkach@gmail.com>
2022-06-23 20:08:39 +0300

    solution

---
+++ src/components/HomePage/HomePage.tsx
@@ -0,0 +1,3 @@
+export const HomePage: React.FC = () => (
+  <h1 className="title mt-6">Home page</h1>
+);
---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import './NotFoundPage.scss';
+
+export const NotFoundPage: React.FC = () => (
+  <main className="mt-6">
+    <h1 className="mt-6 title is-1">
+      Page not found!
+    </h1>
+    <div className="mt-6 error" />
+  </main>
+);
