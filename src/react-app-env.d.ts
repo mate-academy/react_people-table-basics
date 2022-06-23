364b05f149bf83fc6b8df1b274a88eb6d2785c7a
Yaroslav Kvasnytsia <qwerty300067@gmail.com>
2022-06-23 11:38:37 +0300

    fix home and parents

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,24 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+export type People = {
+  name: string,
+  sex: string,
+  born: number,
+  died: number,
+  fatherName: string,
+  motherName: string,
+  slug: string,
+};
+
+export type PersonWithParents = {
+  mother: string;
+  father: string;
+  name: string,
+  sex: string,
+  born: number,
+  died: number,
+  fatherName: string,
+  motherName: string,
+  slug: string,
+};
