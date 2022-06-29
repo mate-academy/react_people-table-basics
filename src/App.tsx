256bb2f709ff03cda0384a2aba02fc931e1612c9
Andriyk0 <andriy22221@gmail.com>
2022-06-29 15:21:11 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.scss';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './HomePage';
 import { PeoplePage } from './PeoplePage';
 import { NotFoundPage } from './NotFoundPage';
@@ -11,6 +11,7 @@ const App = () => {
       <NavigationLink />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
         <Route path="people" element={<PeoplePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
