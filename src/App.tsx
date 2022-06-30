9ec1c2b6f27d1b7ef1c7b47df19b4a514fceac26
Vicky Miziukanova <enrikadej@gmail.com>
2022-06-30 17:47:22 +0300

    added links

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
 import './App.scss';
+import { NavLink, Routes, Route } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage';
 
 const App = () => (
   <div className="App">
-    <h1>People table</h1>
+    <h1 className="title">People table</h1>
+    <div>
+      <NavLink
+        to="/"
+        className={({ isActive }) => (
+          isActive ? 'activeLink' : undefined
+        )}
+      >
+        Home
+      </NavLink>
+      {' '}
+      <NavLink
+        to="/people"
+        className={({ isActive }) => (
+          isActive ? 'activeLink' : undefined
+        )}
+      >
+        People
+      </NavLink>
+    </div>
+
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/people" element={<PeoplePage />} />
+
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   </div>
 );
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.min.css';
+import { BrowserRouter as HashRouter } from 'react-router-dom';
 
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <HashRouter>
+    <App />
+  </HashRouter>,
   document.getElementById('root'),
 );
