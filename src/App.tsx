79da79fdeb47253e270fd817afaf661ca2004dcd
Swazi Kunene <kuneneswazi18@gmail.com>
2022-07-08 11:41:36 +0200

    React People Table Basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import './App.scss';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage';
+import { Header } from './components/Header';
+import 'bulma';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="title is-1">People table</h1>
+
+      <Header />
+
+      <div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <HomePage />
+            }
+          />
+
+          <Route
+            path="/people"
+            element={
+              <PeoplePage />
+            }
+          />
+
+          <Route
+            path="/home"
+            element={
+              <Navigate to="/" />
+            }
+          />
+
+          <Route
+            path="*"
+            element={
+              <NotFoundPage />
+            }
+          />
+        </Routes>
+      </div>
+    </div>
+  );
+};
 
 export default App;
