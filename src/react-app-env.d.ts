bf99e4b782a022c6f6f0aca3593dc47a7e9adb02
Sardorbek Makhamadaliev <smakhamadaliev8@gmail.com>
2022-07-14 12:18:02 +0200

    people table

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,2 +1,12 @@
 // eslint-disable-next-line
 /// <reference types="react-scripts" />
+
+interface People {
+  name: string;
+  sex: string;
+  born: number;
+  fatherName: string;
+  motherName: string;
+  slug: string;
+  died: number;
+}
