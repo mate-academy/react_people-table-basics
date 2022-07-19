04acb60008e5d6d7e55e4ccae98dd69c508c3a46
Vitaly Pasechnik <Vitaly.Pasechnik.11@gmail.com>
2022-07-19 16:29:42 +0300

    fifth saved - with Navigate

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Header } from './components/1_Header/Header';
 import { HomePage } from './components/0_HomePage/HomePage';
 import { PeoplePage } from './components/3_PeoplePage/PeoplePage';
@@ -9,11 +9,10 @@ export const App = () => (
     <Header />
 
     <Routes>
-      {/* <Redirect from="/home" to="/" /> */}
-
       <Route path="/" element={<HomePage />} />
       <Route path="people" element={<PeoplePage />} />
       <Route path="*" element={<p>Page not found</p>} />
+      <Route path="/home" element={<Navigate to="/" />} />
     </Routes>
   </div>
 );
