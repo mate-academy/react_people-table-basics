cf849bc0a3ddf8e66bbc7356ff55eba8e9bbf2d4
Kate Khon <katerynakhon@gmail.com>
2022-08-02 01:33:33 +0300

    initial commit

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Person } from '../types/Person';
+import { PersonRow } from './PersonRow';
+
+type Props = {
+  people: Person[],
+};
+
+export const PeoplePage: React.FC<Props> = ({ people }) => (
+  <>
+    <h1 className="subtitle is-3">People page</h1>
+    <table
+      className="
+        PeopleTable
+        people_table
+        table
+        is-bordered
+      "
+    >
+
+      <thead className="thead">
+        <th>name</th>
+        <th>sex</th>
+        <th>born</th>
+        <th>died</th>
+        <th>mother</th>
+        <th>father</th>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <PersonRow person={person} />
+        ))}
+      </tbody>
+
+    </table>
+  </>
+);
