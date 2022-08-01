43343c99d9ebc2a28b5bdc14a34a5c65a3f15a4e
Oleksandr Kapustei <angelion541@gmail.com>
2022-08-01 17:06:00 +0300

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,57 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
+
 import './App.scss';
+import { PeopleTable } from './components/PeopleTable';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+export const App: React.FC = () => {
+  const getLinkClass = ({ isActive }: { isActive: boolean }): string => {
+    return isActive ? 'is-info' : 'is-light';
+  };
 
-export default App;
+  return (
+    <div className="App">
+      <nav className="buttons m-5">
+        <NavLink
+          to="/"
+          className={(param) => classNames('button', getLinkClass(param))}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/people"
+          className={(param) => classNames('button', getLinkClass(param))}
+        >
+          People
+        </NavLink>
+      </nav>
+
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <h1 className="title is-3 ml-5">
+              Home page
+            </h1>
+          )}
+        />
+
+        <Route
+          path="people"
+          element={(
+            <>
+              <h1 className="title is-3 ml-5">
+                People page
+              </h1>
+
+              <PeopleTable />
+            </>
+          )}
+        />
+
+        <Route path="*" element="Page not found" />
+      </Routes>
+    </div>
+  );
+};
