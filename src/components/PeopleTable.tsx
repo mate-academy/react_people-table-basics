f7aa022dab2277b3eeb271d92c237a25f81a979d
Kris Mur <budkevych.h@gmail.com>
2022-08-01 21:25:20 +0200

    sol

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,27 @@
+import { Person } from './types/Person';
+import { PersonRow } from './PersonRow';
+import './PeopleTable.scss';
+
+type Props = {
+  people: Person[],
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  return (
+    <table>
+      <thead>
+        <th>Name</th>
+        <th>Sex</th>
+        <th>Born</th>
+        <th>Died</th>
+        <th>Mother</th>
+        <th>Father</th>
+      </thead>
+      <tbody>
+        {people.map(person => (
+          <PersonRow key={person.slug} person={person} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
