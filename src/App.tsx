9ea916fa1d1198e17c11201fad8bbe1ddda32b8b
Dmytro Mostovyi <7kozita@gmail.com>
2022-08-02 01:44:06 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
+import { Route, Routes, NavLink } from 'react-router-dom';
 import './App.scss';
+import { HomePage } from './components/HomePage/HomePage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { PeoplePage } from './components/PeoplePage/PeoplePage';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="Header">
+        <h1 className="Header__title">People table:</h1>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive
+            ? 'active-link' : 'Header__link')}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/people"
+          className={({ isActive }) => (isActive
+            ? 'active-link' : 'Header__link')}
+        >
+          People page
+        </NavLink>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/people" element={<PeoplePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
