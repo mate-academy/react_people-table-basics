f7c57ccb85bfddad4d44cbef3fa2c9242297ae91
Aljona Verstiuk <aljovapochta@gmail.com>
2022-08-02 08:09:38 +0200

    commit message

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import { getFather, getMother, getPeople } from './api';
 import './App.scss';
+import { PeopleTable } from './components/PeopleTable';
+import { Person } from './types/Person';
 
-const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
-);
+const App: React.FC = () => {
+  const [people, setPeople] = useState<Person[] | null>(null);
+
+  useEffect(() => {
+    getPeople()
+      .then(peopleFromServer => peopleFromServer
+        .map(person => ({
+          ...person,
+          mother: getMother(person.motherName),
+          father: getFather(person.fatherName),
+        })))
+      .then(peopleFromServer => {
+        setPeople([...peopleFromServer]);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>People table</h1>
+
+      <header>
+        <nav>
+          <NavLink to="/">HomePage</NavLink>
+          <NavLink to="/people">PeoplePage</NavLink>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <div>Home page</div>
+          )}
+        />
+
+        <Route
+          path="people"
+          element={(
+            <>
+              <div>Peope page</div>
+
+              {people && (
+                <PeopleTable people={people} />
+              )}
+            </>
+          )}
+        />
+
+        <Route
+          path=":invoiceId"
+          element={(
+            <div>Page not found</div>
+          )}
+        />
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/api.ts
@@ -0,0 +1,27 @@
+import { PersonData } from './types/PersonData';
+
+// eslint-disable-next-line max-len
+const API_URL = 'https://mate-academy.github.io/react_people-table/api/people.json';
+
+export function getPeople(): Promise<PersonData[]> {
+  return (
+    fetch(API_URL)
+      .then(res => res.json())
+  );
+}
+
+export const getMother = (motherName:string) => {
+  return (
+    getPeople()
+      .then(people => people
+        .find(person => person.name === motherName))
+  );
+};
+
+export const getFather = (fatherName:string) => {
+  return (
+    getPeople()
+      .then(people => people
+        .find(person => person.name === fatherName))
+  );
+};
