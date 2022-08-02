176a66e2dcd7c9783ca702f14b1946015fedc6b0
Nazar Muliar <nazarmuliar26@gmail.com>
2022-08-02 22:07:10 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './App.scss';
+import {
+  HashRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
+import { MainNavigation } from './components/MainNavigation';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage';
 
 const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
+  <HashRouter>
+    <MainNavigation />
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/people" element={<PeoplePage />} />
+        <Route path="/home" element={<Navigate to="/" />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  </HashRouter>
 );
 
 export default App;
