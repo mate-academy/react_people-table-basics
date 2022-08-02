e4bcfa735c9e59b6b7c4ae149bb06ea23e47f286
Mariia  Halytska (Nedilko) <89839710+MariiaNH@users.noreply.github.com>
2022-08-02 19:00:27 +0200

    Update api.tsx

--- src/api.tsx
+++ src/api.tsx
@@ -1,10 +1,10 @@
 /* eslint-disable max-len */
 import { Person } from './types/Person';
 
-const URL = https://mate-academy.github.io/react_people-table/api/people.json;
+const URL = 'https://mate-academy.github.io/react_people-table/api/people.json';
 
 export const getPeople = async (): Promise<Person[]> => {
-  const response = await fetch('URL');
+  const response = await fetch(URL);
 
   return response.json();
 };
