f40916f5ee529241eca3a670602977789fe6947b
SpaceHook <maxdumiak39@gmail.com>
2022-08-03 10:55:08 +0300

    fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,25 +2,27 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.scss';
 
+type LinkStatus = (isActive: { isActive: boolean }) => string;
+
 export const Header: React.FC = () => {
+  const checkStatus: LinkStatus = ({ isActive }) => (
+    isActive ? 'is-active header__nav-link' : 'header__nav-link'
+  );
+
   return (
     <header className="header">
       <nav className="header__nav">
         <ul className="header__nav-list">
           <NavLink
             to="/"
-            className={({ isActive }) => (
-              isActive ? 'is-active header__nav-link' : 'header__nav-link'
-            )}
+            className={checkStatus}
           >
             Home page
           </NavLink>
 
           <NavLink
             to="/people"
-            className={({ isActive }) => (
-              isActive ? 'is-active header__nav-link' : 'header__nav-link'
-            )}
+            className={checkStatus}
           >
             People page
           </NavLink>
