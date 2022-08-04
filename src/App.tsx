ef4bfd3703aa0e5e1e375811cd939dcc61a10ec3
Illia Bielichenko <ibelich12@gmail.com>
2022-08-04 15:39:42 +0300

    v1 was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import './App.scss';
+import {
+  HashRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import { Homepage } from './components/Homepage';
+import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage';
+import { Header } from './components/Header';
 
 const App = () => (
-  <div className="App">
-    <h1>People table</h1>
-  </div>
+  <HashRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="people" element={<PeoplePage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </HashRouter>
 );
 
 export default App;
