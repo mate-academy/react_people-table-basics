ea03ed68716ecdffa1f63675dc38617adf152746
fledith <barateon023@gmail.com>
2022-08-18 18:02:11 +0300

    added people from server, also added navigation to pages

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,42 @@
+import { Person } from '../../types';
+
+interface Props {
+  people: Person[];
+}
+
+export const PeopleTable = ({ people }: Props) => {
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr data-cy="person">
+            <td>
+              <a href="#/people/jan-van-brussel-1714">
+                {person.name}
+              </a>
+            </td>
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>{person.motherName}</td>
+            <td>{person.fatherName}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
