a04a6eb5fd422bbff9350915ecaaaf931cbdd271
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-08-28 14:51:33 +0300

    solution

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader } from './Loader';
+import { getPeople } from '../api';
+import { Person } from '../types';
+import { PeopleTable } from './PeopleTable';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  const { personSlug } = useParams();
+
+  useEffect(() => {
+    getPeople()
+      .then(res => {
+        setPeople(res);
+        setIsLoading(false);
+        setIsLoaded(true);
+      })
+      .catch(() => {
+        setHasError(true);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+      <div className="block">
+        <div className="box table-container">
+          {isLoading
+            && <Loader />}
+          {hasError
+            && (
+              <p data-cy="peopleLoadingError" className="has-text-danger">
+                Something went wrong
+              </p>
+            )}
+
+          {people.length === 0 && !hasError
+            && (
+              <p data-cy="noPeopleMessage" className="has-text-danger">
+                no people
+              </p>
+            )}
+          {isLoaded && people.length > 0
+            && (
+              <PeopleTable people={people} selectedSlug={personSlug} />
+            )}
+        </div>
+      </div>
+    </>
+  );
+};
