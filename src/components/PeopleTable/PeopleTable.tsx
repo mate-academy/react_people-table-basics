477c0f403e66c99aa4900b198530b73594d89688
Andriy <andriyhimself@gmail.com>
2022-08-29 18:07:02 +0300

    Solution_0.1

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,94 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Person } from '../../types';
+
+type Props = {
+  people: Person[];
+  slug: string;
+};
+
+export const PeopleTable: React.FC<Props> = ({ people, slug }) => {
+  const personRowRender = (person: Person) => {
+    const findPerson = (personName: string) => {
+      const someone = people.find(somebody => somebody.name === personName);
+
+      if (someone) {
+        return (
+          <Link
+            to={`/people/${someone.slug}`}
+            className={classNames({ 'has-text-danger': someone.sex === 'f' })}
+          >
+            {someone.name}
+          </Link>
+        );
+      }
+
+      return personName;
+    };
+
+    return (
+      <tr
+        data-cy="person"
+        key={person.slug}
+        className={classNames(
+          { 'has-background-warning': slug === person.slug },
+        )}
+      >
+        <td>
+          <Link
+            to={`/people/${person.slug}`}
+            className={classNames({ 'has-text-danger': person.sex === 'f' })}
+          >
+            {person.name}
+          </Link>
+        </td>
+        <td>{person.sex}</td>
+        <td>{person.born}</td>
+        <td>{person.died}</td>
+        <td>{person.motherName ? findPerson(person.motherName) : '-'}</td>
+        <td>{person.fatherName ? findPerson(person.fatherName) : '-'}</td>
+        <td>
+          <a
+            href={`/people/personalPage/${person.slug}`}
+          >
+            Link
+          </a>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    people.length > 0
+      ? (
+        <table
+          data-cy="peopleTable"
+          className="table is-striped is-hoverable is-narrow is-fullwidth"
+        >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Sex</th>
+              <th>Born</th>
+              <th>Died</th>
+              <th>Mother</th>
+              <th>Father</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {people.map(person => (
+              personRowRender(person)
+            ))}
+          </tbody>
+        </table>
+      )
+      : (
+        <p data-cy="noPeopleMessage">
+          There are no people on the server
+        </p>
+      )
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
-import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+// import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
+import ReactDOM from 'react-dom';
 import { App } from './App';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+ReactDOM.createRoot(document.querySelector('#root'))
+  .render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  );
+
+/* original code below ⬇
 createRoot(document.getElementById('root') as HTMLDivElement)
   .render(
-    <Router>
+    <BrowserRouter>
       <App />
-    </Router>,
+    </BrowserRouter>,
   );
+*/
