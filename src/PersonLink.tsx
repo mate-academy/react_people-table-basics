100dc15090f31dc8eae4e3b484e6170cf329b52c
Daria Semenii <dashasv76@gmail.com>
2022-08-31 18:02:39 +0300

    Solution

---
+++ src/PersonLink.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { Person } from './types/Person';
+
+interface Props {
+  person: Person,
+  setSelectedPerson: (selectedPerson: string) => void;
+}
+
+export const PersonLink: React.FC<Props> = ({
+  person,
+  setSelectedPerson,
+}) => {
+  return (
+    <Link
+      to={`/people/${person.slug}`}
+      className={classNames({ 'has-text-danger': person.sex === 'f' })}
+      onClick={() => setSelectedPerson(person.slug)}
+    >
+      {person.name}
+    </Link>
+  );
+};
