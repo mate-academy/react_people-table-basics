3f62e39865cfbc7ba379970feaaa7103023c339d
DolishniiVolodymyr <volodymyr.dolishnii@gmail.com>
2022-09-01 17:47:21 +0300

    Solution

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import { useEffect, useState } from 'react';
+import { Person } from '../../types';
+import { Loader } from '../Loader';
+import { getPeople } from '../../api';
+import { PeopleTable } from '../PeopleTable';
+
+export const PeoplePage = () => {
+  const [peopleList, setPeopleList] = useState<Person[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  useEffect(() => {
+    getPeople()
+      .then(response => {
+        response.length > 0
+          ? setPeopleList(response)
+          : setIsEmpty(true);
+      })
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  });
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && (
+            <Loader />
+          )}
+
+          {isError && (
+            <p
+              data-cy="peopleLoadingError"
+              className="has-text-danger"
+            >
+              Something went wrong
+            </p>
+          )}
+
+          {isEmpty && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+
+          <PeopleTable peopleList={peopleList} />
+        </div>
+      </div>
+    </>
+  );
+};
