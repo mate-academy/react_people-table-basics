daf5d4d4935d7fc74ed2005a5c91ab2ae5f9209d
sergii2705 <maniurov_sv@ukr.net>
2022-09-01 09:47:43 +0300

    Solution

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -39,7 +39,7 @@ export const PeoplePage = () => {
             </p>
           )}
 
-          {!people.length && (
+          {!people.length && !isLoading && (
             <p data-cy="noPeopleMessage">
               There are no people on the server
             </p>
