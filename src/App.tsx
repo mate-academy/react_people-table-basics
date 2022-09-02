c46fa971305ef2ebf5ec50eb3e0078ac104befb3
Oleksìi <bardyshev@gmail.com>
2022-09-02 08:45:21 +0300

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,15 @@
 import {
-  Routes, Route, Navigate, NavLink,
+  Routes, Route, Navigate,
 } from 'react-router-dom';
-import classNames from 'classnames';
-import { Loader } from './components/Loader';
 import './index.css';
-import {PeoplePage} from './components/PeoplePage';
-import {HomePage} from './components/HomePage';
-import {NotFoundPage} from './components/NotFoundPage';
+import { Navbar } from './components/Navbar';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (
-              classNames('navbar-item',
-                { 'has-background-grey-lighter': isActive })
-            )}
-          >
-            Home
-          </NavLink>
-
-          <NavLink
-            to="/people"
-            className={({ isActive }) => (
-              classNames('navbar-item',
-                { 'has-background-grey-lighter': isActive })
-            )}
-          >
-            People
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     <main className="section">
       <div className="container">
@@ -48,11 +18,16 @@ export const App = () => (
             path="/"
             element={<HomePage />}
           />
-          <Route path="home" element={<Navigate to="/" replace />} />
           <Route
-            path="/people"
-            element={<PeoplePage />}
+            path="home"
+            element={<Navigate to="/" replace />}
           />
+          <Route
+            path="people"
+          >
+            <Route index element={<PeoplePage />} />
+            <Route path=":slug" element={<PeoplePage />} />
+          </Route>
           <Route
             path="*"
             element={<NotFoundPage />}
