8f82e67a5a69240098395c3fc93163be98c5d8dc
Yura Lysenko <yuralysenko1@gmail.com>
2022-09-02 16:13:06 +0300

    solution

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+import { Loader } from './Loader';
+import { PeopleTable } from './PeopleTable';
+import { Person } from '../types';
+import { getPeople } from '../api';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getPeople()
+      .then(res => setPeople(res))
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div className="block">
+      <div className="box table-container">
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <PeopleTable people={people} />
+        )}
+
+        {isError && (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            Something went wrong
+          </p>
+        )}
+
+        {!isLoading && !isError && people.length === 0 && (
+          <p data-cy="noPeopleMessage">
+            There are no people on the server
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
