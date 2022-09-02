dad00f92aa89aaf8c5a2c8177b5758850db006c6
Tania Bukoros <tetiana.bukoros@gmail.com>
2022-09-02 11:39:17 +0300

    solution

---
+++ src/PeopleTable.tsx
@@ -0,0 +1,103 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { Person } from './types';
+
+interface Props {
+  people: Person[];
+  selectedPerson: string;
+}
+
+export const PeopleTable: React.FC<Props> = ({
+  people,
+  selectedPerson,
+}) => {
+  const isSelected = (personName: Person) => personName.slug === selectedPerson;
+
+  const getPersonParent = (parentName: string | null) => {
+    return people.find(person => person.name === parentName);
+  };
+
+  const isExistInTable = (parentName: string | null) => {
+    return people.some(person => person.name === parentName);
+  };
+
+  return (
+    <>
+      <table
+        data-cy="peopleTable"
+        className="table is-striped is-hoverable is-narrow is-fullwidth"
+      >
+
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Sex</th>
+            <th>Born</th>
+            <th>Died</th>
+            <th>Mother</th>
+            <th>Father</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {people.map(person => (
+            <tr
+              key={person.slug}
+              data-cy="person"
+              className={classNames({
+                'has-background-warning': isSelected(person),
+              })}
+            >
+              <td>
+                <Link
+                  to={`/people/${person.slug}`}
+                  className={classNames({
+                    'has-text-danger': person.sex === 'f',
+                  })}
+                >
+                  {person.name}
+                </Link>
+              </td>
+
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+              <td>{person.died}</td>
+
+              <td>
+                {isExistInTable(person.motherName) ? (
+                  <Link
+                    to={`/people/${getPersonParent(person.motherName)?.slug}`}
+                    className="has-text-danger"
+                  >
+                    {person.motherName}
+                  </Link>
+                )
+                  : (
+                    <span>
+                      {person.motherName || '-'}
+                    </span>
+                  )}
+              </td>
+
+              <td>
+                {isExistInTable(person.fatherName) ? (
+                  <Link
+                    to={`/people/${getPersonParent(person.fatherName)?.slug}`}
+                  >
+                    {person.fatherName}
+                  </Link>
+                )
+                  : (
+                    <span>
+                      {person.fatherName || '-'}
+                    </span>
+                  )}
+              </td>
+
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
