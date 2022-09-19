1fc17d1fbd6f2f6a7c2096c8d32f2bd5ac24dc30
Tania Troshchuk <troschuk95@gmail.com>
2022-09-19 12:11:34 +0300

    Solution task React People Table

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,43 @@
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink/PersonLink';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const peopleWithParents: Person[] = people.map(person => {
+    return {
+      ...person,
+      mother: people.find(mother => mother.name === person.motherName),
+      father: people.find(father => father.name === person.fatherName),
+    };
+  });
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {peopleWithParents.map(person => (
+          <PersonLink
+            key={person.slug}
+            person={person}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Person } from '../../types';
+
+type Props = {
+  person: Person,
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const { slug } = useParams();
+
+  return (
+    <tr
+      data-cy="person"
+      className={slug === person.slug ? 'has-background-warning' : ''}
+    >
+      <td>
+        <Link
+          to={`/people/${person.slug}`}
+          className={person.sex === 'f' ? 'has-text-danger' : ''}
+        >
+          {person.name}
+        </Link>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+
+      <td>
+        {person.mother ? (
+          <Link
+            to={`/people/${person.mother.slug}`}
+            className="has-text-danger"
+          >
+            {person.mother.name}
+          </Link>
+        ) : (
+          person.motherName || '-'
+        )}
+      </td>
+
+      <td>
+        {person.father ? (
+          <Link
+            to={`/people/${person.father.slug}`}
+          >
+            {person.father.name}
+          </Link>
+        ) : (
+          person.fatherName || '-'
+        )}
+      </td>
+    </tr>
+  );
+};
