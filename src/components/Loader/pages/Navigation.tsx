91d39c7dc7f1e2d429c8d7afdddc08a40dd0a6de
Nikita Havryliuk <chinter.games1@gmail.com>
2022-10-04 14:52:24 +0300

    created router navigation

---
+++ src/components/Loader/pages/Navigation.tsx
@@ -0,0 +1,30 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+export const Navigation = () => {
+  return (
+    <div className="container">
+      <div className="navbar-brand">
+        <NavLink
+          to="/"
+          className={({ isActive }) => classNames(
+            'navbar-item',
+            { 'has-background-grey-lighter': isActive },
+          )}
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/people"
+          className={({ isActive }) => classNames(
+            'navbar-item',
+            { 'has-background-grey-lighter': isActive },
+          )}
+        >
+          People
+        </NavLink>
+      </div>
+    </div>
+  );
+};
