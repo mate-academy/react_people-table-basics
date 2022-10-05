6c92982055c61ea51531d2442c90bd25f231d643
Nikita Havryliuk <chinter.games1@gmail.com>
2022-10-05 13:53:56 +0300

    some fixes

--- src/components/Loader/pages/PersonLink.tsx
+++ src/components/Loader/pages/PersonLink.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Person } from '../../../types';
 
@@ -7,16 +8,22 @@ type Props = {
 };
 
 export const PersonLink: React.FC<Props> = ({ person }) => {
+  const [isClick, setIsClick] = useState<boolean>(false);
+
+  const handleClick = (): void => {
+    setIsClick(!isClick);
+  };
+
   return (
-    <td>
-      <Link
-        to={`/people/${person.slug}`}
-        className={classNames({
-          'has-text-danger': person.sex === 'f',
-        })}
-      >
-        {person.name}
-      </Link>
-    </td>
+    <Link
+      to={`/people/${person.slug}`}
+      className={classNames({
+        'has-text-danger': person.sex === 'f',
+        'has-background-warning': isClick,
+      })}
+      onClick={handleClick}
+    >
+      {person.name}
+    </Link>
   );
 };
