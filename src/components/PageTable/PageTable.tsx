bd01e8d47ac3f9c487a1ad4dd34aeca5ba5a4d37
Tilak Kumarclear <mailtotilakkumar@gmail.com>
2022-10-16 14:44:55 +0530

    lint fixed

---
+++ src/components/PageTable/PageTable.tsx
@@ -0,0 +1,40 @@
+import { Person } from '../../types';
+import { PageLink } from '../PageLink/PageLink';
+
+type Props = {
+  people: Person[],
+  selectedMan: string | undefined,
+};
+
+export const PageTable: React.FC<Props> = ({ people, selectedMan }) => {
+  const isSelected = (person: Person) => person.slug === selectedMan;
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map((person) => (
+          <PageLink
+            people={people}
+            person={person}
+            isSelected={isSelected}
+            key={person.slug}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
