8d2cdecc2e738a12471339cbaec1602089682edf
Oleksii <oleksii@it.com>
2022-10-16 17:05:56 +0300

    For review

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../../api';
+import { Person } from '../../types/Person';
+import { Loader } from '../Loader';
+import { PeopleTable } from '../PeopleTable';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[] | null>([]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const loadPeople = async () => {
+      try {
+        setLoader(true);
+
+        const result = await getPeople();
+
+        setPeople(result);
+      } catch {
+        setError(true);
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    loadPeople();
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {loader && <Loader />}
+
+          {error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!loader && !people?.length && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+
+          {people && (<PeopleTable people={people} />)}
+        </div>
+      </div>
+    </>
+  );
+};
