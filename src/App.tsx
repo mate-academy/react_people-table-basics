e102a4551103b6c16ce661b47bfbda3a8179facd
fatihsamur <a.fatihsamur@gmail.com>
2022-11-05 18:54:22 +0300

    restructured

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,12 @@
-import './App.scss';
-import { Link } from 'react-router-dom';
-import { PeoplePage } from './components/PeoplePage';
+import { Outlet } from 'react-router-dom';
+import { Navigate } from './components/PageNav';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <Link
-            to='/'
-            className='navbar-item'
-          >
-            Home
-          </Link>
-
-          <Link
-            to='/people'
-            className='navbar-item has-background-grey-lighter'
-          >
-            People
-          </Link>
-        </div>
-      </div>
-    </nav>
-
+    <Navigate />
     <main className="section">
       <div className="container">
-
-        <h1 className="title">Home Page</h1>
-
-        <h1 className="title">Page not found</h1>
-
-        <PeoplePage />
-
+        <Outlet />
       </div>
     </main>
   </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,34 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+
+import {
+
+  HashRouter, Navigate, Route, Routes,
+
+} from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
+
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
 
+import { PagePeople } from './components/PagePeople';
+
 createRoot(document.getElementById('root') as HTMLDivElement)
   .render(
-    <Router>
-      <App />
-    </Router>,
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+          <Route index element={<h1 className="title">Home Page</h1>} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="people/">
+            <Route index element={<PagePeople />} />
+            <Route path=":personMan" element={<PagePeople />} />
+          </Route>
+        </Route>
+      </Routes>
+
+    </HashRouter>,
+
   );
