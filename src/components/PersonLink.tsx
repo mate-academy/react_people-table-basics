8174a60f6c53eae32ac2e9ace7635e8766ed25e6
Olha Chebotarova <chebotarovaolha@gmail.com>
2022-11-08 19:02:59 -0500

    Solution

---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Person } from '../types';
+
+interface Props {
+  person: Person;
+}
+
+export const PersonLink: FC<Props> = ({ person }) => {
+  return (
+    <Link
+      to={`../${person.slug}`}
+      className={classNames({ 'has-text-danger': person.sex === 'f' })}
+    >
+      {person.name}
+    </Link>
+  );
+};
---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../api';
+import { PeopleTable } from '../components/PeopleTable';
+import { Person } from '../types';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const peopleFromServer = await getPeople();
+
+        setPeople(peopleFromServer);
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <PeopleTable
+        people={people}
+        isLoading={isLoading}
+        isError={isError}
+      />
+    </>
+  );
+};
