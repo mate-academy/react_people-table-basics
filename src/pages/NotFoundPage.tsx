a497742b8d79381c80697a5aa4da2b2beb29ff0e
Killdex <kdcompany2018@gmail.com>
2022-11-09 23:48:54 +0200

    edited page not found

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,11 +1,3 @@
 export const NotFoundPage = () => (
-  <div data-cy="app">
-    <main className="section">
-      <div className="container">
-
-        <h1 className="title">Page not found</h1>
-
-      </div>
-    </main>
-  </div>
+  <h1 className="title">Page not found</h1>
 );
