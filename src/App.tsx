bbc732c19d1e10dafa276d1bbeb25bbd9b56063a
Andriy Hladkevych <andriyglad@gmail.com>
2022-11-10 23:27:00 +0200

    New components Navigation and PageNavLink

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,17 @@
-import classNames from 'classnames';
 import { FC } from 'react';
-import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import './App.scss';
+import { Navigation } from './components/Navigation';
 import { People } from './pages/People';
-
-type Props = {
-  to: string,
-  text: string,
-};
-
-export const PageNavLink: FC<Props> = ({ to, text }) => {
-  return (
-    <NavLink
-      to={to}
-      className={({ isActive }) => classNames(
-        'navbar-item',
-        {
-          'has-background-grey-lighter': isActive,
-        },
-      )}
-    >
-      {text}
-    </NavLink>
-  );
-};
 
 export const App: FC = () => {
   return (
     <div data-cy="app">
-      <nav
-        data-cy="nav"
-        className="navbar is-fixed-top has-shadow"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <PageNavLink
-              to="../"
-              text="Home"
-            />
-            <PageNavLink
-              to="../people"
-              text="People"
-            />
-          </div>
-        </div>
-      </nav>
+      <Navigation />
 
       <main className="section">
         <div className="container">
--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -1,12 +1,12 @@
-import classNames from "classnames";
-import { FC } from "react";
-import { Link } from "react-router-dom";
+import classNames from 'classnames';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 type Props = {
   slug: string,
   sex: string,
   name: string,
-}
+};
 
 export const PersonLink: FC<Props> = ({ slug, sex, name }) => (
   <Link
