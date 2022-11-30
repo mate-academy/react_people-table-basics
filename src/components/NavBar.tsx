d415abb622722f23d8032f843ba646cd2bbd0916
Andrew Zaitsev <alzay007@gmail.com>
2022-12-01 00:40:03 +0200

    splited application into components

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+export const NavBar = () => {
+  return (
+    <>
+      <a className="navbar-item" href="#/">
+        Home
+      </a>
+
+      <a
+        className="navbar-item has-background-grey-lighter"
+        href="#/people"
+      >
+        People
+      </a></>
+  )
+}
