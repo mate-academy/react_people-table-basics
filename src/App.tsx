189f429e8ee656d14a698f4f0dc235c2526cd74c
Andrew Zaitsev <alzay007@gmail.com>
2022-12-01 13:45:12 +0200

    full solution

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,24 @@ import {
   Routes,
 } from 'react-router-dom';
 import './App.scss';
-import { PageNavLink } from './components/PageNavLink';
+import { MainNav } from './components/MainNav';
 import { HomePage } from './pages/HomePage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import { PeoplePage } from './pages/PeoplePage';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <PageNavLink to="/" text="Home" />
-          <PageNavLink to="/people" text="People" />
-        </div>
-      </div>
-    </nav>
+    <MainNav />
 
     <main className="section">
       <div className="container">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/home" element={<Navigate to="/" replace />} />
-          <Route path="/people" element={<PeoplePage />} />
+          <Route path="people">
+            <Route index element={<PeoplePage />} />
+            <Route path=":personSlug" element={<PeoplePage />} />
+          </Route>
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,19 +1,19 @@
-import { Loader } from './Loader';
+import classNames from 'classnames';
+import { Person } from '../types';
+import { PersonLink } from './PersonLink';
 
-export const PeopleTable = () => {
+type Props = {
+  people: Person[];
+  personSlug: string;
+};
+
+export const PeopleTable: React.FC<Props> = ({
+  people,
+  personSlug,
+}) => {
   return (
     <div className="block">
       <div className="box table-container">
-        <Loader />
-
-        <p data-cy="peopleLoadingError" className="has-text-danger">
-          Something went wrong
-        </p>
-
-        <p data-cy="noPeopleMessage">
-          There are no people on the server
-        </p>
-
         <table
           data-cy="peopleTable"
           className="table is-striped is-hoverable is-narrow is-fullwidth"
@@ -30,109 +30,54 @@ export const PeopleTable = () => {
           </thead>
 
           <tbody>
-            <tr data-cy="person">
-              <td>
-                <a href="#/people/jan-van-brussel-1714">
-                  Jan van Brussel
-                </a>
-              </td>
+            {people.map(person => {
+              const {
+                sex, born, died, motherName, fatherName, slug,
+              } = person;
 
-              <td>m</td>
-              <td>1714</td>
-              <td>1748</td>
-              <td>Joanna van Rooten</td>
-              <td>Jacobus van Brussel</td>
-            </tr>
+              const mother = people.find(women => {
+                return motherName === women.name;
+              });
+              const father = people.find(man => fatherName === man.name);
 
-            <tr data-cy="person">
-              <td>
-                <a href="#/people/philibert-haverbeke-1907">
-                  Philibert Haverbeke
-                </a>
-              </td>
-
-              <td>m</td>
-              <td>1907</td>
-              <td>1997</td>
-
-              <td>
-                <a
-                  className="has-text-danger"
-                  href="#/people/emma-de-milliano-1876"
+              return (
+                <tr
+                  data-cy="person"
+                  key={slug}
+                  className={classNames(
+                    { 'has-background-warning': personSlug === slug },
+                  )}
                 >
-                  Emma de Milliano
-                </a>
-              </td>
+                  <td>
+                    <PersonLink person={person} />
+                  </td>
 
-              <td>
-                <a href="#/people/emile-haverbeke-1877">
-                  Emile Haverbeke
-                </a>
-              </td>
-            </tr>
+                  <td>{sex}</td>
+                  <td>{born}</td>
+                  <td>{died}</td>
+                  <td>
+                    {mother?.slug === undefined
+                      ? (
+                        motherName || '-'
+                      )
+                      : (
+                        <PersonLink person={mother} />
+                      )}
 
-            <tr data-cy="person" className="has-background-warning">
-              <td>
-                <a href="#/people/jan-frans-van-brussel-1761">
-                  Jan Frans van Brussel
-                </a>
-              </td>
+                  </td>
+                  <td>
+                    {father?.slug === undefined
+                      ? (
+                        fatherName || '-'
+                      )
+                      : (
+                        <PersonLink person={father} />
+                      )}
 
-              <td>m</td>
-              <td>1761</td>
-              <td>1833</td>
-              <td>-</td>
-
-              <td>
-                <a href="#/people/jacobus-bernardus-van-brussel-1736">
-                  Jacobus Bernardus van Brussel
-                </a>
-              </td>
-            </tr>
-
-            <tr data-cy="person">
-              <td>
-                <a
-                  className="has-text-danger"
-                  href="#/people/lievijne-jans-1542"
-                >
-                  Lievijne Jans
-                </a>
-              </td>
-
-              <td>f</td>
-              <td>1542</td>
-              <td>1582</td>
-              <td>-</td>
-              <td>-</td>
-            </tr>
-
-            <tr data-cy="person">
-              <td>
-                <a href="#/people/bernardus-de-causmaecker-1721">
-                  Bernardus de Causmaecker
-                </a>
-              </td>
-
-              <td>m</td>
-              <td>1721</td>
-              <td>1789</td>
-
-              <td>
-                <a
-                  className="has-text-danger"
-                  href="#/people/livina-haverbeke-1692"
-                >
-                  Livina Haverbeke
-                </a>
-              </td>
-
-              <td>
-                <a href="#/people/lieven-de-causmaecker-1696">
-                  Lieven de Causmaecker
-                </a>
-              </td>
-            </tr>
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
