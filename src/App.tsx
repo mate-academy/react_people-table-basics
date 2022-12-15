f7747f91d86a8543c976ec55b559365880551e8b
bohdan-tron <ikwiczzts@gmail.com>
2022-12-15 11:20:03 +0200

    another tests appearence

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/Navbar';
 export const App = () => (
   <div data-cy="app">
     <Navbar />
+
     <main className="section">
       <div className="container">
         <Outlet />
