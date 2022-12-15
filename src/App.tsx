f28021f89be16157fe826fd9b9d4ef7e8983eec9
bohdan-tron <ikwiczzts@gmail.com>
2022-12-15 11:11:43 +0200

    70/70 tests completed on PC, waiting for pass tests on Git

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Navbar } from './components/Navbar';
 export const App = () => (
   <div data-cy="app">
     <Navbar />
-
     <main className="section">
       <div className="container">
         <Outlet />
