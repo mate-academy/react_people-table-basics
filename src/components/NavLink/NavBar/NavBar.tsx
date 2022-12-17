1f4269c75a10abfcce49b7fbff7726e73ee09ccf
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-12-17 15:14:27 +0200

    Implement People table using React Router

---
+++ src/components/NavLink/NavBar/NavBar.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { PageNavLink } from '../PageNavLink';
+
+export const NavBar: FC = () => {
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <PageNavLink to="/" content="Home" />
+
+          <PageNavLink to="/people" content="People" />
+        </div>
+      </div>
+    </nav>
+  );
+};
