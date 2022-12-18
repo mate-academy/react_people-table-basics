1f95c19d6ee2055eb1b293a3a3f15f8c47932654
Svitlana Yudina <osbka31@gmail.com>
2022-12-18 19:21:04 +0200

    fix

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink/PersonLink';
+
+interface Props {
+  people: Person[],
+  selectedSlug: string,
+}
+
+export const PeopleTable: React.FC<Props> = ({ people, selectedSlug }) => {
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => {
+          const {
+            sex,
+            born,
+            died,
+            fatherName,
+            motherName,
+            slug,
+            mother,
+            father,
+          } = person;
+
+          return (
+            <tr
+              key={slug}
+              data-cy="person"
+              className={classNames({
+                'has-background-warning': selectedSlug === person.slug,
+              })}
+            >
+              <td>
+                <PersonLink person={person} />
+              </td>
+
+              <td>{sex}</td>
+              <td>{born}</td>
+              <td>{died}</td>
+
+              <td>
+                {mother && (
+                  <PersonLink person={mother} />
+                )}
+
+                {!mother && motherName}
+
+                {!motherName && '-'}
+              </td>
+
+              <td>
+                {father && (
+                  <PersonLink person={father} />
+                )}
+
+                {!father && fatherName}
+
+                {!fatherName && '-'}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
