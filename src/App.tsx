18661c16f7d4638922c2e1f0bc8a8d24b038e7a7
Vadym Mamchur <vmchr344@gmail.com>
2022-12-20 14:44:30 +0200

    created Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,18 @@
 import './App.scss';
-import cn from 'classnames';
 import {
-  Navigate, NavLink, Route, Routes,
+  Navigate,
+  Route,
+  Routes,
 } from 'react-router-dom';
 
 import { HomePage } from './components/HomePage';
 import { PeoplePage } from './components/PeoplePage';
 import { NotFoundPage } from './components/NotFoundPage';
+import { Navigation } from './components/Navigation';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) => cn(
-              'navbar-item',
-              { 'has-background-grey-lighter': isActive },
-            )}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/people"
-            className={({ isActive }) => cn(
-              'navbar-item',
-              { 'has-background-grey-lighter': isActive },
-            )}
-          >
-            People
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    <Navigation />
 
     <main className="section">
       <div className="container">
