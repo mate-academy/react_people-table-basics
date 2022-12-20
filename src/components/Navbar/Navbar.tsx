47e774148c73e5b5ac6f886e6d9b5c422e329938
Danyla Krygin <underworldsan@gmail.com>
2022-12-20 09:34:12 +0200

    made correction

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { PageNavLink } from '../../pages/PageNavLink';
 
 export const Navbar: React.FC = () => (
--- src/components/PersonInfo/PersonInfo.tsx
+++ src/components/PersonInfo/PersonInfo.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Person } from '../../types';
 import { PersonLink } from '../PersoneLink/PersonLink';
