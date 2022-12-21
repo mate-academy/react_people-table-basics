849a7c6f529df349ee83343e6431c6129917e794
Anna Shapovalovaemail <3809.kasabian.00@gmail.com>
2022-12-21 18:27:42 +0200

    v01

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,79 @@
+import classNames from 'classnames';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import { PersonLink } from '../PersonLink';
+
+import { Person } from '../../types/Person';
+
+interface Props {
+  people: Person[];
+}
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const { slug } = useParams();
+
+  const findParent = (parentName: string | null) => {
+    const parent = people.find(person => person.name === parentName);
+
+    if (parent) {
+      return (
+        <PersonLink person={parent} />
+      );
+    }
+
+    return parentName || '-';
+  };
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => {
+          const {
+            sex,
+            born,
+            died,
+            motherName,
+            fatherName,
+          } = person;
+
+          return (
+            <tr
+              data-cy="person"
+              key={person.slug}
+              className={classNames(
+                {
+                  'has-background-warning': slug === person.slug,
+                },
+              )}
+            >
+              <td>
+                <PersonLink person={person} />
+              </td>
+
+              <td>{sex}</td>
+              <td>{born}</td>
+              <td>{died}</td>
+              <td>{findParent(motherName)}</td>
+              <td>{findParent(fatherName)}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+
+import { Person } from '../../types';
+
+interface Props {
+  person: Person;
+}
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const {
+    slug,
+    sex,
+    name,
+  } = person;
+
+  return (
+    <Link
+      to={`/people/${slug}`}
+      className={classNames(
+        {
+          'has-text-danger': sex === 'f',
+        },
+      )}
+    >
+      {name}
+    </Link>
+  );
+};
