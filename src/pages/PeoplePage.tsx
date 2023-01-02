2b041c32bef2ab93a388591cf63f43d565685761
Andrew Skoryk <andrewtimelessone@gmail.com>
2023-01-02 20:37:17 +0200

    feat: implemented App with 2 pages and ability to select a person

---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,51 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getPeople } from '../api';
+import { Loader } from '../components/Loader';
+import { Person } from '../types';
+import { PeopleTable } from '../components/PeopleTable';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const getPeopleFromServer = useCallback(async () => {
+    try {
+      const peopleFromServer = await getPeople();
+
+      setPeople(peopleFromServer);
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    getPeopleFromServer();
+  }, []);
+
+  const { slug = '' } = useParams();
+  const noPeople = !people.length && !isLoading;
+
+  return (
+    <div className="block">
+      <h1 className="title">People Page</h1>
+
+      <div className="box table-container">
+        {isLoading && <Loader />}
+
+        {isError && (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            Something went wrong
+          </p>
+        )}
+        {noPeople && (
+          <p data-cy="noPeopleMessage">There are no people on the server</p>
+        )}
+
+        <PeopleTable people={people} selectedPerson={slug} />
+      </div>
+    </div>
+  );
+};
