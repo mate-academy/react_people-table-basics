2c77aaf32587f5391aac04bfe5651ba3e948060b
Nazar Kulchevych <102380127+Kulchevych@users.noreply.github.com>
2023-01-03 15:26:52 +0200

    React People Table solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,32 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
+import { PageNotFound } from './components/PageNotFound';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
   .render(
     <Router>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="*" element={<PageNotFound />} />
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<Navigate to="/" replace />} />
+          <Route path="people" element={(<PeoplePage />)}>
+            <Route index element={(<PeoplePage />)} />
+            <Route path=":slug" element={(<PeoplePage />)} />
+          </Route>
+        </Route>
+      </Routes>
     </Router>,
   );
