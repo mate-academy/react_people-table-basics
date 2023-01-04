0040a43976fad08b6f674169a8232cfa9deaa2b9
Demut <uddarendeli1@gmail.com>
2023-01-04 12:10:50 +0300

    Solution

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import { Link, useLocation } from 'react-router-dom';
+
+export const Navigation = () => {
+  const location = useLocation();
+
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <Link
+            className={classNames(
+              'navbar-item',
+              { 'has-background-grey-lighter': location.pathname === '/' },
+            )}
+            to="/"
+          >
+            Home
+          </Link>
+
+          <Link
+            className={classNames(
+              'navbar-item',
+              {
+                'has-background-grey-lighter': location
+                  .pathname.startsWith('/people'),
+              },
+            )}
+            to="/people"
+          >
+            People
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
