6560d41dcb7a4ca636306586a92246fe2bbb2a10
Nick_Radchuk <mukola.radchuk@gmail.com>
2023-01-05 20:55:05 +0200

    solution

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Person } from '../../types/Person';
+
+type Props = {
+  parent: string | null
+  getPersonByParent: (value: string | null) => Person | undefined
+};
+
+export const PersonLink: React.FC<Props> = ({
+  parent,
+  getPersonByParent,
+}) => {
+  const personIsParent = getPersonByParent(parent);
+
+  if (!parent) {
+    return (
+      <td> - </td>
+    );
+  }
+
+  return (
+    <td>
+      {personIsParent ? (
+        <Link
+          className={classNames({
+            'has-text-danger': personIsParent?.sex === 'f',
+          })}
+          to={`/people/${personIsParent?.slug}`}
+        >
+          {parent}
+        </Link>
+      ) : parent}
+    </td>
+  );
+};
