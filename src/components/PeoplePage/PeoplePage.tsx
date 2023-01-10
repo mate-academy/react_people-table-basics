3f5eb3822d71a5de67bd3743dc813d765a4d9fa5
Yura Dikanev <yuriydikanev@gmail.com>
2023-01-10 15:05:04 +0200

    react_people-table-bas

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+
+import { getPeople } from '../../api';
+
+import { Loader } from '../Loader';
+import { PeopleTable } from '../PeopleTable';
+
+import { Person } from '../../types';
+
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getPeopleFromServer = async () => {
+    try {
+      const peopleFromServer = await getPeople();
+
+      setPeople(peopleFromServer);
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getPeopleFromServer();
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && (
+            <Loader />
+          )}
+
+          {hasError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {people.length === 0 && !isLoading && !hasError && (
+            <p data-cy="noPeopleMessage">
+            There are no people on the server
+          </p>
+        )}
+
+        {people.length > 0 && (
+          <PeopleTable people={people} />
+        )}
+      </div>
+    </div>
+  </>
+);
+};
