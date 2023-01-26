484de028a3492e5c61d55da80ecd2d54daf4ec6e
mr-valentos <nerpavlad@gmail.com>
2023-01-26 15:40:11 +0200

    add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import {
-  Navigate, NavLink, Outlet, Route, Routes,
+  Navigate, Outlet, Route, Routes,
 } from 'react-router-dom';
 // import { Loader } from './components/Loader';
 
 import './App.scss';
-import cn from 'classnames';
 import { PeoplePage } from './components/PeoplePage';
+import { Navbar } from './components/Navbar';
 
 export const App = () => (
   <div data-cy="app">
@@ -16,23 +16,7 @@ export const App = () => (
       aria-label="main navigation"
     >
       <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) => cn('navbar-item',
-              { 'has-background-grey-lighter': isActive })}
-          >
-            Home
-          </NavLink>
-
-          <NavLink
-            to="people"
-            className={({ isActive }) => cn('navbar-item',
-              { 'has-background-grey-lighter': isActive })}
-          >
-            People
-          </NavLink>
-        </div>
+        <Navbar />
       </div>
     </nav>
 
