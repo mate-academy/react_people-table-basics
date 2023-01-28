7b43d8453ea4637f78a57f570f931a3c587abbbe
mr-valentos <nerpavlad@gmail.com>
2023-01-28 14:11:00 +0200

    fix

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -13,6 +13,10 @@ export const PeoplePage: FC = memo(() => {
   const [isError, setIsError] = useState(false);
 
   const { slug } = useParams();
+
+  const getPersonByName = (name: string | null, peopleArr: Person[]) => (
+    peopleArr.find(person => person.name === name)
+  );
   const getPeopleFromServer = async () => {
     setIsLoading(true);
     try {
@@ -20,7 +24,15 @@ export const PeoplePage: FC = memo(() => {
       fetch('https://mate-academy.github.io/react_people-table/api/people.json')
         .then(res => res.json());
 
-      setPeople(peopleFromServer);
+      const peopleWithParents = peopleFromServer.map((person: Person) => {
+        return {
+          ...person,
+          mother: getPersonByName(person.motherName, peopleFromServer),
+          father: getPersonByName(person.fatherName, peopleFromServer),
+        };
+      });
+
+      setPeople(peopleWithParents);
     } catch (e) {
       setIsError(true);
     } finally {
@@ -28,85 +40,76 @@ export const PeoplePage: FC = memo(() => {
     }
   };
 
-  const getPersonByName = (name: string | null) => (
-    people.find(person => person.name === name)
-  );
-
   useEffect(() => {
     getPeopleFromServer();
   }, []);
 
-  const peopleWithParents = people.map(person => {
-    return {
-      ...person,
-      mother: getPersonByName(person.motherName),
-      father: getPersonByName(person.fatherName),
-    };
-  });
+  const isPeopleArrEmpty = people.length === 0 && !isError && !isLoading;
+  const shouldPeopleBeRendered = people.length !== 0 && !isError;
 
   return (
     <div className="block">
       <div className="box table-container">
-        {isLoading ? <Loader /> : (
-          <>
-            {isError && (
-              <p data-cy="peopleLoadingError" className="has-text-danger">
-                Something went wrong
-              </p>
-            )}
+        {isLoading && <Loader />}
 
-            {people.length === 0 && !isError ? (
-              <p data-cy="noPeopleMessage">
-                There are no people on the server
-              </p>
-            ) : (
-              <table
-                data-cy="peopleTable"
-                className="table is-striped is-hoverable is-narrow is-fullwidth"
-              >
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Sex</th>
-                    <th>Born</th>
-                    <th>Died</th>
-                    <th>Mother</th>
-                    <th>Father</th>
-                  </tr>
-                </thead>
+        {isError && (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            Something went wrong
+          </p>
+        )}
 
-                <tbody>
-                  {peopleWithParents.map(person => (
-                    <tr
-                      key={person.slug}
-                      data-cy="person"
-                      className={cn(
-                        { 'has-background-warning': person.slug === slug },
-                      )}
-                    >
-                      <td>
-                        <PersonLink person={person} />
-                      </td>
+        {isPeopleArrEmpty && (
+          <p data-cy="noPeopleMessage">
+            There are no people on the server
+          </p>
+        )}
 
-                      <td>{person.sex}</td>
-                      <td>{person.born}</td>
-                      <td>{person.died}</td>
-                      <td>
-                        {person.mother
-                          ? <PersonLink person={person.mother} />
-                          : person.motherName || '-'}
-                      </td>
-                      <td>
-                        {person.father
-                          ? <PersonLink person={person.father} />
-                          : person.fatherName || '-'}
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            )}
-          </>
+        {shouldPeopleBeRendered && (
+          <table
+            data-cy="peopleTable"
+            className="table is-striped is-hoverable is-narrow is-fullwidth"
+          >
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Sex</th>
+                <th>Born</th>
+                <th>Died</th>
+                <th>Mother</th>
+                <th>Father</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {people.map(person => (
+                <tr
+                  key={person.slug}
+                  data-cy="person"
+                  className={cn(
+                    { 'has-background-warning': person.slug === slug },
+                  )}
+                >
+                  <td>
+                    <PersonLink person={person} />
+                  </td>
+
+                  <td>{person.sex}</td>
+                  <td>{person.born}</td>
+                  <td>{person.died}</td>
+                  <td>
+                    {person.mother
+                      ? <PersonLink person={person.mother} />
+                      : person.motherName || '-'}
+                  </td>
+                  <td>
+                    {person.father
+                      ? <PersonLink person={person.father} />
+                      : person.fatherName || '-'}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         )}
       </div>
     </div>
