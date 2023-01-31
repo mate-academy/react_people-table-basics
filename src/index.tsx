0b74a781c8605dca5a70e1f0a93681972983e956
Artem Rymarchuk <rumarchyk204@gmail.com>
2023-01-31 20:34:53 +0100

    Through bugs and lags

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,28 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
+import { HomePage } from './components/HomePage/HomePage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { PeoplePage } from './components/PeoplePage/PeoplePage';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
   .render(
     <Router>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="home" element={<Navigate to="/" replace/>}/>
+          <Route index element={<HomePage />}/>
+          <Route path="*" element={<NotFoundPage />}/>
+          <Route path="people">
+            <Route index element={<PeoplePage />}/>
+            <Route path=":personSlug"  element={<PeoplePage />}/>
+          </Route>
+        </Route>
+      </Routes>
     </Router>,
   );
