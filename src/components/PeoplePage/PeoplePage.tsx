eb1f9ef9e4d380ae5fff69af76f0135bfc7b2b18
Olena Yanovska <alena.yn.adw@gmail.com>
2023-02-01 12:24:53 +0200

    table with people moved to a component

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -1,17 +1,14 @@
 import React, { memo, useEffect, useState } from 'react';
-import cn from 'classnames';
-import { useParams } from 'react-router-dom';
 import { getPeople } from '../../api';
 import { Person } from '../../types';
 import { Loader } from '../Loader';
-import { PersonLink } from '../PersonLink/PersonLink';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
 
 export const PeoplePage: React.FC = memo(() => {
   const [isError, setIsError] = useState(false);
   const [people, setPeople] = useState<Person[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isLoadingFinish, setIsLoadingFinish] = useState(false);
-  const { slug } = useParams();
 
   const loadPeople = async () => {
     try {
@@ -68,53 +65,7 @@ export const PeoplePage: React.FC = memo(() => {
           )}
 
           {people.length > 0 && (
-            <table
-              data-cy="peopleTable"
-              className="table is-striped
-                  is-hoverable is-narrow is-fullwidth"
-            >
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Sex</th>
-                  <th>Born</th>
-                  <th>Died</th>
-                  <th>Mother</th>
-                  <th>Father</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {people.map((person) => (
-                  <tr
-                    data-cy="person"
-                    key={person.slug}
-                    className={cn({
-                      'has-background-warning': person.slug === slug,
-                    })}
-                  >
-                    <td>
-                      <PersonLink person={person} />
-                    </td>
-
-                    <td>{person.sex}</td>
-                    <td>{person.born}</td>
-                    <td>{person.died}</td>
-                    <td>
-                      {person.mother
-                        ? <PersonLink person={person.mother} />
-                        : person.motherName || '-'}
-                    </td>
-                    <td>
-                      {person.father
-                        ? <PersonLink person={person.father} />
-                        : person.fatherName || '-'}
-                    </td>
-                  </tr>
-                ))}
-
-              </tbody>
-            </table>
+            <PeopleTable people={people} />
           )}
         </div>
       </div>
---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,61 @@
+import { memo } from 'react';
+import cn from 'classnames';
+import { useParams } from 'react-router-dom';
+import { PersonLink } from '../PersonLink/PersonLink';
+import { Person } from '../../types';
+
+interface Props {
+  people: Person[]
+}
+export const PeopleTable: React.FC<Props> = memo(({ people }) => {
+  const { slug } = useParams();
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map((person) => (
+          <tr
+            data-cy="person"
+            key={person.slug}
+            className={cn({
+              'has-background-warning': person.slug === slug,
+            })}
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              {person.mother
+                ? <PersonLink person={person.mother} />
+                : person.motherName || '-'}
+            </td>
+            <td>
+              {person.father
+                ? <PersonLink person={person.father} />
+                : person.fatherName || '-'}
+            </td>
+          </tr>
+        ))}
+
+      </tbody>
+    </table>
+  );
+});
