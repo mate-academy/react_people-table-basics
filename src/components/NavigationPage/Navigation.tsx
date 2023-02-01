aa29b3fcfc708b3d1a5caebd2a2102e6d4b2812e
Anton Martynes <martynes1997@gmail.com>
2023-02-01 02:28:03 +0200

    solution

---
+++ src/components/NavigationPage/Navigation.tsx
@@ -0,0 +1,38 @@
+import cn from 'classnames';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export const Navigation: React.FC = () => {
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            to="/"
+            className={({ isActive }) => cn(
+              'navbar-item',
+              { 'has-background-grey-lighter': isActive },
+            )}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            to="/people"
+            className={({ isActive }) => cn(
+              'navbar-item',
+              { 'has-background-grey-lighter': isActive },
+            )}
+          >
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
