17ca16a4a4b20c50ebb5189cf67f841d414ddbe1
Anatolii <batstolya@gmail.com>
2023-02-03 00:14:40 +0200

    solution

---
+++ src/components/Loader/pages/NavBar.tsx
@@ -0,0 +1,17 @@
+import { CustomNavLink } from './CustomNavLink';
+
+export const NavBar = () => (
+  <nav
+    data-cy="nav"
+    className="navbar is-fixed-top has-shadow"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <CustomNavLink to="/" title="Home" />
+        <CustomNavLink to="/people" title="People" />
+      </div>
+    </div>
+  </nav>
+);
