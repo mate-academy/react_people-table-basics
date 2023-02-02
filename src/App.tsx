c650be37dd30149a58e89f28efbf7fbf5e55f4e7
kreisberg <anastasia.kreisberg@yahoo.com>
2023-02-02 22:37:57 +0200

    add PeoplePage

--- src/App.tsx
+++ src/App.tsx
@@ -19,18 +19,7 @@ export const App = () => (
           <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="*" element={<NotFoundPage />} />
 
-          {/* <div className="block">
-          <div className="box table-container">
-            <Loader />
-
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
-
-            <p data-cy="noPeopleMessage">
-              There are no people on the server
-            </p>
-
+          {/*
             <table
               data-cy="peopleTable"
               className="table is-striped is-hoverable is-narrow is-fullwidth"
--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -1,3 +1,56 @@
-export const PeoplePage = () => (
-  <h1 className="title">People Page</h1>
-);
+import { Loader } from "../components/Loader";
+import { getPeople } from "../api";
+import { Person } from "../types";
+import { useEffect, useState } from "react";
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isErrorNotif, setIsErrorNotif] = useState(false);
+
+  const loadingPeople = async () => {
+    try {
+      setIsLoading(true);
+
+      const loadedPeople = await getPeople();
+
+      setPeople(loadedPeople);
+    } catch {
+      setIsErrorNotif(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadingPeople();
+  }, []);
+
+  const hasNobody = !isLoading && !people.length;
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+      <div className="block">
+
+        <div className="box table-container">
+          {isLoading && <Loader />}
+          {isErrorNotif && (
+            <p
+              data-cy="peopleLoadingError"
+              className="has-text-danger"
+            >
+              Something went wrong
+            </p>
+          )}
+
+          {hasNobody && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
