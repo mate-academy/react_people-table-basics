02e3ff6cac9eaaa4466ee673c4a9f59ceeb4299f
Oleh Yablunovskyi <lifserv@gmail.com>
2023-02-03 00:49:23 +0200

    People table was rendered

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import cn from 'classnames';
+import { Person } from '../../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = React.memo(
+  ({ person }) => {
+    return (
+      <tr data-cy="person">
+        <td>
+          <a
+            href={`#/people/${person.slug}`}
+            className={cn(
+              'todo',
+              { 'has-text-danger': person.sex === 'f' },
+            )}
+          >
+            {person.name}
+          </a>
+        </td>
+
+        <td>{person.sex}</td>
+        <td>{person.born}</td>
+        <td>{person.died}</td>
+
+        {person.motherName
+          ? (
+            <td>{person.motherName}</td>
+          )
+          : (
+            <td>-</td>
+          )}
+
+        {person.fatherName
+          ? (
+            <td>{person.fatherName}</td>
+          )
+          : (
+            <td>-</td>
+          )}
+      </tr>
+    );
+  },
+);
