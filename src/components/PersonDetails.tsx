7c3b005888613b3230471a4735d6feeca2bc9713
OlhaArama <arama_o_f@ukr.net>
2023-02-02 21:46:59 +0200

    Destructuring was added

--- src/components/PersonDetails.tsx
+++ src/components/PersonDetails.tsx
@@ -15,45 +15,56 @@ export const PersonDetails: React.FC<Props> = ({
   personMother,
   personFather,
   isSelected,
-}) => (
-  <tr
-    data-cy="person"
-    key={person.name}
-    className={classNames(
-      {
-        'has-background-warning': isSelected(person),
-      },
-    )}
-  >
-    <td>
-      <PersonLink
-        person={person}
-      />
-    </td>
+}) => {
+  const {
+    name,
+    sex,
+    born,
+    died,
+    fatherName,
+    motherName,
+  } = person;
 
-    <td>{person.sex}</td>
-    <td>{person.born}</td>
-    <td>{person.died}</td>
-    <td>
+  return (
+    <tr
+      data-cy="person"
+      key={name}
+      className={classNames(
+        {
+          'has-background-warning': isSelected(person),
+        },
+      )}
+    >
+      <td>
+        <PersonLink
+          person={person}
+        />
+      </td>
 
-      {
-        personMother
-          ? (
-            <PersonLink person={personMother} />
-          ) : (
-            person.motherName || '-'
-          )
-      }
-    </td>
-    <td>
-      {
-        personFather
-          ? (
-            <PersonLink person={personFather} />
-          ) : (
-            person.fatherName || '-'
-          )
-      }
-    </td>
-  </tr>
-);
+      <td>{sex}</td>
+      <td>{born}</td>
+      <td>{died}</td>
+      <td>
+
+        {
+          personMother
+            ? (
+              <PersonLink person={personMother} />
+            ) : (
+              motherName || '-'
+            )
+        }
+      </td>
+      <td>
+        {
+          personFather
+            ? (
+              <PersonLink person={personFather} />
+            ) : (
+              fatherName || '-'
+            )
+        }
+      </td>
+    </tr>
+  );
+};
