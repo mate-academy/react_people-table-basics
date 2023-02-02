d58b2b563de57225eede7efcd0e3231755138b4a
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-02-02 13:55:23 +0200

    fixed mother and father links

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,46 +1,55 @@
-import { useState, useEffect } from "react"
-import { getPeople } from "../../api";
-import { Person } from "../../types"
-import classNames from "classnames";
-import { Link, useParams } from "react-router-dom";
-import { PersonLink } from "../PersonLink/PersonLink";
-import { Loader } from "../Loader";
+import { useState, useEffect, useMemo } from 'react';
+import classNames from 'classnames';
+import { useParams } from 'react-router-dom';
+import { getPeople } from '../../api';
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink/PersonLink';
+import { Loader } from '../Loader';
+
+const headers = [
+  { id: 1, title: 'Name' },
+  { id: 2, title: 'Sex' },
+  { id: 3, title: 'Born' },
+  { id: 4, title: 'Died' },
+  { id: 5, title: 'Mother' },
+  { id: 6, title: 'Father' },
+];
 
 export const PeopleTable: React.FC = () => {
   const [people, setPeople] = useState<Person[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('')
+  const [errorMessage, setErrorMessage] = useState('');
 
   const { slug } = useParams();
 
   useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
     getPeople()
-        .then(peopleFromServer => {
-          const preparedPeople = peopleFromServer.map(human => {
-            const mother = people.find(m => (m.name === human.motherName))
-            const father = people.find(f => (f.name === human.fatherName))
+      .then(setPeople)
+      .catch(() => setErrorMessage('Something went wrong'))
+      .finally(() => setIsLoading(false));
+  }, []);
 
-            return {
-              ...human,
-              mother,
-              father,
-            }})
+  const preparedPeople = useMemo(() => (
+    people.map(human => {
+      const mother = people.find(m => (m.name === human.motherName));
+      const father = people.find(f => (f.name === human.fatherName));
 
-        setPeople(preparedPeople)})
-        .catch(() => setErrorMessage('Something went wrong'))
-        .finally(() => setIsLoading(false))
-      },[]
-    );
+      return {
+        ...human,
+        mother,
+        father,
+      };
+    })), [people]);
 
   return (
     <div className="box table-container">
       {isLoading && <Loader />}
 
       {!people.length && !isLoading && (
-            <p data-cy="noPeopleMessage">
-              There are no people on the server
-            </p>
+        <p data-cy="noPeopleMessage">
+          There are no people on the server
+        </p>
       )}
 
       {errorMessage && (
@@ -59,53 +68,47 @@ export const PeopleTable: React.FC = () => {
             <table
               data-cy="peopleTable"
               className="table is-striped is-hoverable is-narrow is-fullwidth"
-            >
-            </table>
-              <thead>
-                <tr>
-                <th>Name</th>
-                <th>Sex</th>
-                <th>Born</th>
-                <th>Died</th>
-                <th>Mother</th>
-                <th>Father</th>
+            />
+            <thead>
+              <tr>
+                {headers.map(header => <th key={header.id}>{header.title}</th>)}
+              </tr>
+            </thead>
+
+            <tbody>
+              {preparedPeople.map(person => (
+                <tr
+                  data-cy="person"
+                  key={person.slug}
+                  className={classNames(
+                    { 'has-background-warning': person.slug === slug },
+                  )}
+                >
+                  <td>
+                    <PersonLink person={person} />
+                  </td>
+
+                  <td>{person.sex}</td>
+                  <td>{person.born}</td>
+                  <td>{person.died}</td>
+                  <td>
+                    {person.mother
+                      ? <PersonLink person={person.mother} />
+                      : person.motherName || '-'}
+                  </td>
+
+                  <td>
+                    {person.father
+                      ? <PersonLink person={person.father} />
+                      : person.fatherName || '-'}
+                  </td>
+
                 </tr>
-              </thead>
-
-        <tbody>
-          {people.map(person => (
-            <tr
-              data-cy="person"
-              key={person.slug}
-              className={classNames({'has-background-warning': person.slug === slug})}
-            >
-            <td>
-              <PersonLink person={person} />
-            </td>
-
-            <td>{person.sex}</td>
-            <td>{person.born}</td>
-            <td>{person.died}</td>
-
-            <td>
-              {person.mother
-                ? <PersonLink person={person.mother} />
-                : person.motherName || '-'
-              }
-            </td>
-
-            <td>
-              {person.father
-                ? <PersonLink person={person.father} />
-                : person.fatherName || '-'
-              }
-            </td>
-          </tr>
-          ))}
-      </tbody>
-      </>
-      )}
-    </table>
+              ))}
+            </tbody>
+          </>
+        )}
+      </table>
     </div>
-  )
-}
+  );
+};
