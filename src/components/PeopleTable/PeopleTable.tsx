0c88439d438d96196c4ad21421298a7094499537
Olena Yanovska <alena.yn.adw@gmail.com>
2023-02-02 23:23:52 +0200

    tableRow moved to a component

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,15 +1,10 @@
-import { memo } from 'react';
-import cn from 'classnames';
-import { useParams } from 'react-router-dom';
-import { PersonLink } from '../PersonLink/PersonLink';
 import { Person } from '../../types';
+import { TableRow } from '../TableRow/TableRow';
 
 interface Props {
   people: Person[]
 }
-export const PeopleTable: React.FC<Props> = memo(({ people }) => {
-  const { slug } = useParams();
-
+export const PeopleTable: React.FC<Props> = ({ people }) => {
   return (
     <table
       data-cy="peopleTable"
@@ -28,34 +23,10 @@ export const PeopleTable: React.FC<Props> = memo(({ people }) => {
 
       <tbody>
         {people.map((person) => (
-          <tr
-            data-cy="person"
-            key={person.slug}
-            className={cn({
-              'has-background-warning': person.slug === slug,
-            })}
-          >
-            <td>
-              <PersonLink person={person} />
-            </td>
-
-            <td>{person.sex}</td>
-            <td>{person.born}</td>
-            <td>{person.died}</td>
-            <td>
-              {person.mother
-                ? <PersonLink person={person.mother} />
-                : person.motherName || '-'}
-            </td>
-            <td>
-              {person.father
-                ? <PersonLink person={person.father} />
-                : person.fatherName || '-'}
-            </td>
-          </tr>
+          <TableRow person={person} />
         ))}
 
       </tbody>
     </table>
   );
-});
+};
