17ce61ee8c7dd0a6aff6df50db7b31ec8f81ad95
Oleksandr Nikora <mailnik@ukr.net>
2023-02-03 06:39:22 +0100

    fixed

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,16 +14,20 @@ export const Navbar: FC = () => (
       <div className="navbar-brand">
         <NavLink
           to="/"
-          className={({ isActive }) => cn('navbar-item',
-            { 'has-background-grey-lighter': isActive })}
+          className={({ isActive }) => cn(
+            'navbar-item',
+            { 'has-background-grey-lighter': isActive },
+          )}
         >
           Home
         </NavLink>
 
         <NavLink
           to="people"
-          className={({ isActive }) => cn('navbar-item',
-            { 'has-background-grey-lighter': isActive })}
+          className={({ isActive }) => cn(
+            'navbar-item',
+            { 'has-background-grey-lighter': isActive },
+          )}
         >
           People
         </NavLink>
