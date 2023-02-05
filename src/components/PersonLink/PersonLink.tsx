0d91af88d6c702721e94c2726c167781b82a6e9f
TetKopot <tet.kopot@gmail.com>
2023-02-05 22:38:51 +0200

    solution

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
+import { Person } from '../../types/Person';
+
+interface Props {
+  person: Person,
+};
+
+export const PersonLink: React.FC<Props> = React.memo(({ person }) => {
+  return (
+    <Link
+      to={`../${person.slug}`}
+      className={cn({
+        'has-text-danger': person.sex === 'f',
+      })}
+    >
+      {person.name}
+    </Link>
+  );
+});
