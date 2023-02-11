9a5c7aec4edd4343a7a32a5c89493916b72b499b
valntyn <romanenko.valentyn1@gmail.com>
2023-02-11 16:28:46 +0200

    solution

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Loader } from '../components/Loader';
 import { getPeople } from '../api';
