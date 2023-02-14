86f81f79f995162c2cf5a30559fa8e39e64e18a3
Elias Montauti <egsm147@gmail.com>
2023-02-14 12:33:56 +0000

    Solution

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Navigation: React.FC = () => (
+  <nav
+    data-cy="nav"
+    className="navbar is-fixed-top has-shadow"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <Link
+          className="navbar-item"
+          to="/"
+        >
+          Home
+        </Link>
+
+        <Link
+          className="navbar-item has-background-grey-lighter"
+          to="/people"
+        >
+          People
+        </Link>
+      </div>
+    </div>
+  </nav>
+);
