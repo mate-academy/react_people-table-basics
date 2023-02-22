6dde7c59cb8b06460ec5b647f75ed472c8824a7f
JRijo <99342891+John-Stiff@users.noreply.github.com>
2023-02-22 15:52:12 +0000

    created Navigation component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,19 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Loader } from './components/Loader';
 
 import './App.scss';
+import { Navigation } from './components/Navigation';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="#/">
-            Home
-          </a>
-
-          <a
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
-          >
-            People
-          </a>
-        </div>
-      </div>
-    </nav>
-
+    <Navigation />
     <main className="section">
       <div className="container">
+        <Routes>
+          <Route path="/" />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="*" element={<h1 className="title">Page not found</h1>} />
+        </Routes>
         <h1 className="title">Home Page</h1>
         <h1 className="title">People Page</h1>
         <h1 className="title">Page not found</h1>
---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,23 @@
+export const Navigation = () => (
+  <nav
+    data-cy="nav"
+    className="navbar is-fixed-top has-shadow"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div className="container">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="#/">
+          Home
+        </a>
+
+        <a
+          className="navbar-item has-background-grey-lighter"
+          href="#/people"
+        >
+          People
+        </a>
+      </div>
+    </div>
+  </nav>
+);
