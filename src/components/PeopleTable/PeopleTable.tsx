664f633402969c79c54e1076429069b945935f83
@nataliia.vyrsta <natali.vyrsta@gmail.com>
2023-02-23 17:37:03 +0200

    first commit

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink';
+
+interface Props {
+  people: Person[],
+  selectedPerson: string;
+}
+
+export const PeopleTable: React.FC<Props> = ({ people, selectedPerson }) => {
+  const findParent = (name: string | null) => {
+    return people.find(person => person.name === name) || null;
+  };
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => {
+          const mother = findParent(person.motherName);
+          const father = findParent(person.fatherName);
+
+          return (
+            <PersonLink
+              person={person}
+              key={person.slug}
+              selectedPerson={selectedPerson}
+              mother={mother}
+              father={father}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
---
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Loader } from '../../components/Loader';
+import { Person } from '../../types';
+import { getPeople } from '../../api';
+import { PeopleTable } from '../../components/PeopleTable';
+
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const { slug = '' } = useParams();
+
+  const isServerEmpty = people.length === 0 && !isLoading && !isError;
+  const isDataLoaded = people.length > 0 && !isLoading && !isError;
+
+  const loadPeopleFromServer = async () => {
+    try {
+      const loadedPeople = await getPeople();
+
+      setPeople(loadedPeople);
+      setIsLoading(false);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadPeopleFromServer();
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && <Loader />}
+
+          {isError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {isDataLoaded && (
+            <PeopleTable selectedPerson={slug} people={people} />
+          )}
+
+          {isServerEmpty && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
