2491b76a4cc8b9ad08c9b3c8512492d914f6955f
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-02-24 15:03:12 +0200

    removed redundant brackets

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -42,15 +42,15 @@ export const PeoplePage: React.FC = () => {
       <h1 className="title">People Page</h1>
       <div className="block">
         <div className="box table-container">
-          {(isLoading) && <Loader />}
+          {isLoading && <Loader />}
 
-          {(hasError) && (
+          {hasError && (
             <p data-cy="peopleLoadingError" className="has-text-danger">
               Something went wrong
             </p>
           )}
 
-          {(loadingSuccess) && (
+          {loadingSuccess && (
             <PeopleTable
               people={people}
               personSlug={slug}
