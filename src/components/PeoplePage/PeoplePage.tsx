fafd9963d44892595a7945bc18fc2775416f682e
Oleh Holovnykh <shankar.oleh@gmail.com>
2023-02-26 19:05:11 +0200

    move errorMessage to peopleTable

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getPeople } from '../../api';
 import { Person } from '../../types';
@@ -9,56 +9,42 @@ import { PeopleTable } from '../PeopleTable';
 export const PeoplePage: React.FC = () => {
   const { slug = '' } = useParams();
   const [people, setPeople] = useState<Person[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
 
-  const fetchPeople = useCallback(async () => {
-    setIsLoading(true);
-
-    try {
-      const peopleFromServer = await getPeople();
-
-      setPeople(peopleFromServer);
-    } catch {
-      setHasError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  }, []);
-
   useEffect(() => {
+    const fetchPeople = async () => {
+      setIsLoading(true);
+
+      try {
+        const peopleFromServer = await getPeople();
+        const peopleWithParents = preparedPeople(peopleFromServer);
+
+        setPeople(peopleWithParents);
+      } catch {
+        setHasError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
     fetchPeople();
   }, []);
-
-  const peopleWithParents = preparedPeople(people);
-  const hasData = people.length > 0 && !isLoading;
-  const hasEmpty = (people.length === 0) && (!hasError) && (!isLoading);
 
   return (
     <>
       <h1 className="title">People Page</h1>
       <div className="block">
         <div className="box table-container">
-          {isLoading && <Loader />}
-
-          {hasError && (
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
-          )}
-
-          {hasData && (
-            <PeopleTable
-              people={peopleWithParents}
-              personSlug={slug}
-            />
-          )}
-
-          {hasEmpty && (
-            <p data-cy="noPeopleMessage">
-              There are no people on the server
-            </p>
-          )}
+          {isLoading
+            ? <Loader />
+            : (
+              <PeopleTable
+                people={people}
+                personSlug={slug}
+                hasError={hasError}
+              />
+            )}
         </div>
       </div>
     </>
--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -4,32 +4,55 @@ import { PersonString } from '../PersonString';
 interface Props {
   people: Person[],
   personSlug: string,
+  hasError: boolean,
 }
 
-export const PeopleTable: React.FC<Props> = ({ people, personSlug }) => (
-  <table
-    data-cy="peopleTable"
-    className="table is-striped is-hoverable is-narrow is-fullwidth"
-  >
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Sex</th>
-        <th>Born</th>
-        <th>Died</th>
-        <th>Mother</th>
-        <th>Father</th>
-      </tr>
-    </thead>
+export const PeopleTable: React.FC<Props> = ({
+  people,
+  personSlug,
+  hasError,
+}) => {
+  if (hasError) {
+    return (
+      <p data-cy="peopleLoadingError" className="has-text-danger">
+        Something went wrong
+      </p>
+    );
+  }
 
-    <tbody>
-      {people.map(person => (
-        <PersonString
-          key={person.slug}
-          person={person}
-          personSlug={personSlug}
-        />
-      ))}
-    </tbody>
-  </table>
-);
+  if (!people.length) {
+    return (
+      <p data-cy="noPeopleMessage">
+        There are no people on the server
+      </p>
+    );
+  }
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <PersonString
+            key={person.slug}
+            person={person}
+            personSlug={personSlug}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+};
