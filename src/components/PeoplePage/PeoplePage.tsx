2bfa4786cc8a592aa46df9d438d598a006e6698c
annatkachenko <tkachenko.anna835@gmail.com>
2023-02-27 23:39:22 +0200

    add people filters

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -4,6 +4,7 @@ import { PeopleTable } from '../PeopleTable';
 import { getPeople } from '../../api';
 import { Person } from '../../types';
 import { getPreparedPeople } from '../../utils/getPreparedPeople';
+import { PeopleFilters } from '../PeopleFilters';
 
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
@@ -38,26 +39,34 @@ export const PeoplePage = () => {
       <h1 className="title">People Page</h1>
 
       <div className="block">
-        <div className="box table-container">
-          {isLoading && (
-            <Loader />
-          )}
+        <div className="columns is-desktop is-flex-direction-row-reverse">
+          <div className="column is-7-tablet is-narrow-desktop">
+            <PeopleFilters />
+          </div>
 
-          {isFetchError && (
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
-          )}
+          <div className="column">
+            <div className="box table-container">
+              {isLoading && (
+                <Loader />
+              )}
 
-          {isPeopleVisible && (
-            <PeopleTable people={people} />
-          )}
+              {isFetchError && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
 
-          {isNoPeopleMessageVisible && (
-            <p data-cy="noPeopleMessage">
-              There are no people on the server
-            </p>
-          )}
+              {isPeopleVisible && (
+                <PeopleTable people={people} />
+              )}
+
+              {isNoPeopleMessageVisible && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
