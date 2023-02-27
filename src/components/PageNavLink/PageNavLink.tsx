bdea0171a92a19ef305918ac881e5b814c3bdb4c
Sasha Semenyuk <gud.semenyuk@gmail.com>
2023-02-27 19:35:09 +0200

    Solution

---
+++ src/components/PageNavLink/PageNavLink.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+type Props = {
+  to: string
+  title: string
+};
+
+export const PageNavLink: React.FC<Props> = ({ to, title }) => (
+  <NavLink
+    className={({ isActive }) => (classNames('navbar-item', {
+      'has-background-grey-lighter': isActive,
+    }))}
+    to={to}
+  >
+    {title}
+  </NavLink>
+);
