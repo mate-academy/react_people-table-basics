bfb941eebf586aab05480ba4c64b522a6240a2ba
Anatolii-Polishchuk <polishchuk.anatol@gmaile.com>
2023-03-31 10:58:34 +0200

    Develpo People-Table-Basics

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,12 @@
-import { Person } from './types/Person';
+import { PersonType } from './types/Person';
 
 // eslint-disable-next-line max-len
 const API_URL = 'https://mate-academy.github.io/react_people-table/api/people.json';
-
 function wait(delay: number) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
 
-export function getPeople(): Promise<Person[]> {
+export function getPeople(): Promise<PersonType[]> {
   // keep this delay for testing purpose
   return wait(500)
     .then(() => fetch(API_URL))
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as BrouserRouter } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
@@ -8,7 +8,7 @@ import { App } from './App';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
   .render(
-    <Router>
+    <BrouserRouter>
       <App />
-    </Router>,
+    </BrouserRouter>,
   );
