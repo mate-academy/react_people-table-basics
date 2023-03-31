c839311df36952d1c01de00a13806f376119fb76
Anatolii-Polishchuk <polishchuk.anatol@gmaile.com>
2023-03-31 10:59:40 +0200

    Develpo People-Table-Basics

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,7 @@ import { PersonType } from './types/Person';
 
 // eslint-disable-next-line max-len
 const API_URL = 'https://mate-academy.github.io/react_people-table/api/people.json';
+
 function wait(delay: number) {
   return new Promise(resolve => setTimeout(resolve, delay));
 }
