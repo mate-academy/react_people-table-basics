e622fcc2635cab3a89e540bf8bc76bec5105ffca
Valeriia Yarzhemska <lera.strikalo@icloud.com>
2023-04-13 17:10:52 +0300

    fixed error

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -47,7 +47,7 @@ export const PeoplePage: FC = () => {
                 </p>
               )}
 
-              {people.length < 0 && (
+              {people.length <= 0 && (
                 <p data-cy="noPeopleMessage">
                   There are no people on the server
                 </p>
