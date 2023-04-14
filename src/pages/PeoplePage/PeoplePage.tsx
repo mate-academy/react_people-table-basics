c71065008afe3f7c00961ab7ee59a278cd540903
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-04-14 10:40:51 +0300

    react_people-table-basics solution

---
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../../api';
+import { findParent } from '../../helpers';
+import { Loader } from '../../components/Loader';
+import { Person } from '../../types';
+import { PeopleTable } from '../../components/PeopleTable';
+
+
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoadError, setIsLoadError] = useState(false);
+
+  useEffect(() => {
+    getPeople()
+      .then(loadedPeople => {
+        const preparedPeople = loadedPeople.map(person => ({
+          ...person,
+          mother: findParent(loadedPeople, person.motherName),
+          father: findParent(loadedPeople, person.fatherName),
+        }));
+
+        setPeople(preparedPeople);
+      })
+      .catch(() => setIsLoadError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading
+            ? (
+              <Loader />
+            )
+            : (
+              <PeopleTable people={people} />
+            )}
+
+          {isLoadError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!isLoading && !people.length && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              <p data-cy="noPeopleMessage">
+                There are no people on the server
+              </p>
+            </p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
