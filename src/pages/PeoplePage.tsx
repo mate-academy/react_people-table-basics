d9e698d090e8da9f8b1053fb82f073c102aea005
Vladyslav Mosor <mosorvlad@gmail.com>
2023-04-14 09:31:27 +0200

    small fix

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -47,9 +47,13 @@ export const PeoplePage = () => {
             <p data-cy="noPeopleMessage">There are no people on the server</p>
           )}
 
-          {isLoading ? (
+          {isLoading && (
             <Loader />
-          ) : (
+          )}
+
+          {!hasLoadingError
+          && !hasDataError
+          && !isLoading && (
             <PeopleTable people={people} />
           )}
         </div>
