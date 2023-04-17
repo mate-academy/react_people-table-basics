72d28559b59187f456f6475bee550da0b7079795
Vadym Kolomiiets <maestrovadkol@gmail.com>
2023-04-17 19:42:52 +0300

    Solution

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,20 @@
+import { PageNavLink } from '../PageNavLink/PageNavLink';
+
+export const Navigation = () => {
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <PageNavLink to="/" text="Home" />
+
+          <PageNavLink to="/people" text="People" />
+        </div>
+      </div>
+    </nav>
+  );
+};
