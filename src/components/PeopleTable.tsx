e6c777d9e1e2acb2958daa3ea54c758b39b0ac2c
Olecsiy <infernoregent@gmail.com>
2023-04-17 18:22:14 +0300

    add new component

--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,8 +1,7 @@
-import classNames from 'classnames';
 import { FC } from 'react';
 import { useParams } from 'react-router-dom';
 import { Person } from '../types';
-import { PeopleLink } from './PeopleLink';
+import { PeopleInfo } from './PeopleInfo';
 
 interface Props {
   people: Person[];
@@ -41,52 +40,12 @@ export const PeopleTable: FC<Props> = ({ people }) => {
 
       <tbody>
         {people.map((person) => {
-          const {
-            sex,
-            born,
-            slug,
-            died,
-            fatherName,
-            motherName,
-            father,
-            mother,
-          } = person;
-
           return (
-            <tr
-              data-cy="person"
-              key={slug}
-              className={classNames({
-                'has-background-warning': slug === selectedPersonSlug,
-              })}
-            >
-              <td>
-                <PeopleLink person={person} />
-              </td>
-              <td>
-                {sex}
-              </td>
-              <td>
-                {born}
-              </td>
-              <td>
-                {died}
-              </td>
-              <td>
-                {mother ? (
-                  <PeopleLink person={mother} />
-                ) : (
-                  motherName || '-'
-                )}
-              </td>
-              <td>
-                {father ? (
-                  <PeopleLink person={father} />
-                ) : (
-                  fatherName || '-'
-                )}
-              </td>
-            </tr>
+            <PeopleInfo
+              key={person.slug}
+              person={person}
+              selectedPersonSlug={selectedPersonSlug}
+            />
           );
         })}
       </tbody>
