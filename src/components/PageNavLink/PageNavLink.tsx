94b3f272b1ddb52c217a304293c91a2f5ddfd72b
vadosik15 <vadim.bratus@gmail.com>
2023-04-19 10:19:05 +0300

    solution

---
+++ src/components/PageNavLink/PageNavLink.tsx
@@ -0,0 +1,20 @@
+import classnames from 'classnames';
+import React, { FC } from 'react';
+import { NavLink } from 'react-router-dom';
+
+type PageNavLinkProps = {
+  to: string,
+  title: string;
+};
+
+export const PageNavLink: FC<PageNavLinkProps> = ({ to, title }) => {
+  return (
+    <NavLink
+      className={({ isActive }) => classnames('navbar-item',
+        { 'has-background-grey-lighter': isActive })}
+      to={to}
+    >
+      {title}
+    </NavLink>
+  );
+};
---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Person } from '../../types';
+import { getPeople } from '../../api';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
+import { Loader } from '../Loader';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const { slug = '' } = useParams();
+
+  const loadPeople = async () => {
+    setIsLoading(true);
+    try {
+      const peopleFromServer = await getPeople();
+
+      const peopleArray = peopleFromServer.map((person) => {
+        const personCopy = { ...person };
+        const mother = peopleFromServer.find((mom) => (
+          mom.name === person.motherName
+        ));
+
+        const father = peopleFromServer.find((dad) => (
+          dad.name === person.fatherName
+        ));
+
+        if (mother) {
+          personCopy.mother = mother;
+        }
+
+        if (father) {
+          personCopy.father = father;
+        }
+
+        return personCopy;
+      });
+
+      setPeople(peopleArray);
+    } catch (error) {
+      setErrorMessage((error as Error).message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadPeople();
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && <Loader />}
+
+          {people.length > 0 && !isLoading && !errorMessage
+            && <PeopleTable people={people} selectedPerson={slug} />}
+
+          {people.length === 0 && !isLoading && !errorMessage && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+
+          {errorMessage
+            && (
+              <p data-cy="peopleLoadingError" className="has-text-danger">
+                Something went wrong
+              </p>
+            )}
+        </div>
+      </div>
+    </div>
+  );
+};
