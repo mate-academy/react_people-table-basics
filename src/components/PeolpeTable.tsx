b98286d25604639ec4d1daa18c4543232116478b
Kate <novak5683@gmail.com>
2023-04-19 14:43:01 +0200

    people table done

---
+++ src/components/PeolpeTable.tsx
@@ -0,0 +1,59 @@
+import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../types';
+import { PersonLink } from './PersonLink';
+
+type Props = {
+  people: Person[],
+};
+
+export const PeopleTable: FC<Props> = ({ people }) => {
+  const { slug } = useParams<{ slug: string }>();
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            data-cy="person"
+            key={person.slug}
+            className={classNames({
+              'has-background-warning': person.slug === slug,
+            })}
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              {person.mother ? <PersonLink person={person.mother} />
+                : person.motherName || '-'}
+            </td>
+            <td>
+              {person.father ? <PersonLink person={person.father} />
+                : person.fatherName || '-'}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
