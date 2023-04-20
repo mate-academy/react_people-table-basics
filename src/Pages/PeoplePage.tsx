eb3d54bfb62dfb4d38dcb21b400ead719fb8ef8f
Maksym <maksymmaliuk98@gmail.com>
2023-04-20 17:42:28 +0300

    The table is render if there are only no errors

--- src/Pages/PeoplePage.tsx
+++ src/Pages/PeoplePage.tsx
@@ -5,13 +5,13 @@ import { PeopleTable } from '../components/PeopleTable';
 import { Loader } from '../components/Loader';
 
 const findParent = (people: Person[], name: string) => {
-  return people.find(person => person.name === name);
+  return people.find(person => person.name === name) || null;
 };
 
 export const PeoplePage: React.FC = () => {
   const [peopleTable, setPeopleTable] = useState<Person[]>([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
 
   useEffect(() => {
     const getPeopleTableFromServer = async () => {
@@ -34,8 +34,9 @@ export const PeoplePage: React.FC = () => {
         });
 
         setPeopleTable(visiblePeople);
+        setIsError(null);
       } catch (error) {
-        setIsError(true);
+        setIsError('Error loading people data from the server.');
       } finally {
         setIsLoading(false);
       }
@@ -48,26 +49,27 @@ export const PeoplePage: React.FC = () => {
     <div className="container">
       <h1 className="title">People Page</h1>
 
-      <div className="block">
-        <div className="box table-container">
-          {isError && (
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
-          )}
+      {isError && (
+        <p data-cy="peopleLoadingError" className="has-text-danger">
+          {isError}
+        </p>
+      )}
+      {!isError && (
+        <div className="block">
+          <div className="box table-container">
 
-          {(!peopleTable.length && !isLoading) && (
-            <p data-cy="noPeopleMessage">
-              There are no people on the server
-            </p>
-          )}
+            {(!peopleTable.length && !isLoading) && (
+              <p data-cy="noPeopleMessage">
+                There are no people on the server
+              </p>
+            )}
 
-          {isLoading
-            ? <Loader />
-            : <PeopleTable people={peopleTable} />}
-
+            {(isLoading)
+              ? <Loader />
+              : <PeopleTable people={peopleTable} />}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
