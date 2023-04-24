ce1b6348deb02819bfa427f2324f656361df56fc
Maksym Huriev <makspsih.73@gmail.com>
2023-04-24 12:49:13 +0300

    react_people-table-basics

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,18 @@
+import classNames from 'classnames';
+import { Link } from 'react-router-dom';
+import { Person } from '../../types/Person';
+
+type Props = {
+  person: Person,
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  return (
+    <Link
+      to={person.slug}
+      className={classNames({ 'has-text-danger': person.sex === 'f' })}
+    >
+      {person.name}
+    </Link>
+  );
+};
