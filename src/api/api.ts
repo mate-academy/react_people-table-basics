aea32a2960d298dce527976c246f3e55e5c7cc5e
Yelisieiev Kim <rbv271@outlook.com>
2023-05-18 15:58:11 +0300

    react people table

---
+++ src/api/api.ts
@@ -0,0 +1,15 @@
+import { Person } from '../types/Person';
+
+// eslint-disable-next-line max-len
+const API_URL = 'https://mate-academy.github.io/react_people-table/api/people.json';
+
+function wait(delay: number) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
+
+export async function getPeople(): Promise<Person[]> {
+  await wait(500);
+  const response = await fetch(API_URL);
+
+  return response.json();
+}
