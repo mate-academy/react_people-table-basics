c5ae5de1d5bb7b94dbeeaa9bd85a99320d16e7d9
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-05-20 18:50:00 +0300

    aded people table and person link components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
-import { Loader } from './components/Loader';
 import './App.scss';
 import { HomePage } from './components/HomePage';
 import { Navbar } from './components/Navbar';
+import { PeoplePage } from './components/PeoplePage';
 
 export const App = () => (
   <div data-cy="app">
     <Navbar />
 
     <main className="section">
-      <Loader />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/people" />
+        <Route path="/people">
+          <Route index element={<PeoplePage />} />
+        </Route>
       </Routes>
 
     </main>
