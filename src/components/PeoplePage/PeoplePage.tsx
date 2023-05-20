e6b880876b4b20aa6e1e9dfc675bd65c723ec52a
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-05-20 13:10:02 +0300

    init comit, start from splitting components

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,14 @@
+import { FC } from "react"
+
+export const PeoplePage: FC = () => {
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+      <div className="block">
+        <div className="box table-container">
+         content will be here
+        </div>
+      </div>
+    </>
+  );
+}
