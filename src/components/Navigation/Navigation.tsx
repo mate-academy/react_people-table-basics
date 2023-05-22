3f855f6ed9b13734202f9eba47a531f75d871640
Yevhen-Ivanchuk <e.ivanchyk@gmail.com>
2023-05-22 10:48:47 +0300

    react people table basics

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,42 @@
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import { PageRoutes } from '../../types/PageRoutes';
+
+export const Navigation = () => {
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            className={({ isActive }) => classNames(
+              'navbar-item',
+              {
+                'has-background-grey-lighter': isActive,
+              },
+            )}
+            to={PageRoutes.HomePage}
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) => classNames(
+              'navbar-item',
+              {
+                'has-background-grey-lighter': isActive,
+              },
+            )}
+            to={PageRoutes.PeoplePage}
+          >
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../../types';
+import { PageRoutes } from '../../types/PageRoutes';
+
+type Props = {
+  person: Person,
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const { sex, name, slug } = person;
+
+  return (
+    <Link
+      className={classNames(
+        { 'has-text-danger': sex === 'f' },
+      )}
+      to={`${PageRoutes.PeoplePage}/${slug}`}
+    >
+      {name}
+    </Link>
+  );
+};
