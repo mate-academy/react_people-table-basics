65e08f2b31e3b9df82cdcf7c5dd338c205f59a5d
Peti51 <galpeter31@gmail.com>
2023-06-07 20:40:02 +0200

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import { NotFoundPage } from './components/NotFoundPage';
 export const App: React.FC = () => {
   const location = useLocation();
 
-  const isHomePage = location.pathname.endsWith('/home');
+  const isHomePageOn = location.pathname.endsWith('/home');
 
   return (
     <div data-cy="app">
@@ -48,7 +48,7 @@ export const App: React.FC = () => {
         </div>
       </nav>
 
-      {isHomePage && <Navigate to="/" replace />}
+      {isHomePageOn && <Navigate to="/" replace />}
       <Routes>
         <Route
           path="/"
