16da28e00924154978ca5640b0a64400fc3528f5
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-06-12 16:13:59 +0200

    solution

--- src/components/PeoplePages/PeoplePage.tsx
+++ src/components/PeoplePages/PeoplePage.tsx
@@ -19,13 +19,6 @@ export const PeoplePage = () => {
       }
 
       const peopleWithParents = dataPeople.map(person => {
-        // const mother = dataPeople.find((motherPerson) => {
-        //   return motherPerson.name === person.motherName;
-        // });
-        // const father = dataPeople.find((fatherPerson) => {
-        //   return fatherPerson.name === person.fatherName;
-        // });
-
         const findParentByName = (parentName: string) => {
           return dataPeople
             .find(parentPerson => parentPerson.name === parentName);
