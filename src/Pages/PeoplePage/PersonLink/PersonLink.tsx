c20cb13645fde8461508ba76a7fb9b023c553a96
Nigga <yomzfk@gmail.com>
2023-06-14 18:53:08 +0300

    solution

---
+++ src/Pages/PeoplePage/PersonLink/PersonLink.tsx
@@ -0,0 +1,23 @@
+import classNames from 'classnames';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import { Person } from '../../../types';
+
+type Props = {
+  person: Person,
+};
+
+export const PersonLink: FC<Props> = ({ person }) => {
+  const {
+    name,
+  } = person;
+
+  return (
+    <Link
+      to={`../${person.slug}`}
+      className={classNames({ 'has-text-danger': person.sex === 'f' })}
+    >
+      {name}
+    </Link>
+  );
+};
