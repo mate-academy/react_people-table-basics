3344fe9a8fade982ad5c7437f05e849c71797fc1
Denys Didenko <ddidenko441@gmail.com>
2023-06-26 15:31:49 +0300

    add-people-table

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,19 @@
+import { PageNavLink } from '../PageNavLink/PageNavLink';
+
+export const Navigation: React.FC = () => {
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <PageNavLink to="/" title="Home" />
+          <PageNavLink to="people" title="People" />
+        </div>
+      </div>
+    </nav>
+  );
+};
