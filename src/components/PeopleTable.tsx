900f8ac8d8f0ab41f44f4f4cfa73952f8d55ea58
Maryna Kucher <marina.kucher8@gmail.com>
2023-06-29 18:59:47 +0300

    Task "react_people-table-basics" has been solved

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,54 @@
+import { FC } from 'react';
+import { Person } from '../types';
+import { PersonLink } from './PersonLink';
+import { Loader } from './Loader';
+
+type Props = {
+  people: Person [];
+  isLoading: boolean
+};
+
+export const PeopleTable: FC<Props> = ({ people, isLoading }) => {
+  const checkingMotherName = (personParentName: string | null) => {
+    return personParentName
+      ? people.find(person => person.name === personParentName) || null
+      : null;
+  };
+
+  return (
+    <div className="container">
+      <div className="block">
+        <div className="box table-container">
+          {isLoading ? <Loader /> : (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  people.map(person => (
+                    <PersonLink
+                      person={person}
+                      key={person.slug}
+                      checkingPersonName={checkingMotherName}
+                    />
+                  ))
+                }
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
