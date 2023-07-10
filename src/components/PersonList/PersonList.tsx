2cd75a364cbab7cf3df77665377d1fcb16d0da6d
Oleksii Lysenko <nano77@ukr.net>
2023-07-10 10:13:03 +0300

    remove coments, change <a> to <Link>

--- src/components/PersonList/PersonList.tsx
+++ src/components/PersonList/PersonList.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import classNames from 'classnames';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Person } from '../../types';
 
 type Props = {
@@ -50,14 +50,14 @@ export const PersonList: FC<Props> = ({ persons }) => {
 
             if (foundPerson) {
               return (
-                <a
+                <Link
                   className={classNames({
                     'has-text-danger': foundPerson.sex === 'f',
                   })}
-                  href={`#/people/${foundPerson.slug}`}
+                  to={`#/people/${foundPerson.slug}`}
                 >
                   {foundPerson.name}
-                </a>
+                </Link>
               );
             }
 
@@ -73,14 +73,14 @@ export const PersonList: FC<Props> = ({ persons }) => {
               })}
             >
               <td>
-                <a
+                <Link
                   className={classNames({
                     'has-text-danger': sex === 'f',
                   })}
-                  href={`#/people/${person.slug}`}
+                  to={`#/people/${person.slug}`}
                 >
                   {name}
-                </a>
+                </Link>
               </td>
 
               <td>{sex}</td>
@@ -91,96 +91,6 @@ export const PersonList: FC<Props> = ({ persons }) => {
             </tr>
           );
         })}
-
-        {/* <tr data-cy="person">
-          <td>
-            <a href="#/people/philibert-haverbeke-1907">
-              Philibert Haverbeke
-            </a>
-          </td>
-
-          <td>m</td>
-          <td>1907</td>
-          <td>1997</td>
-
-          <td>
-            <a
-              className="has-text-danger"
-              href="#/people/emma-de-milliano-1876"
-            >
-              Emma de Milliano
-            </a>
-          </td>
-
-          <td>
-            <a href="#/people/emile-haverbeke-1877">
-              Emile Haverbeke
-            </a>
-          </td>
-        </tr>
-
-        <tr data-cy="person" className="has-background-warning">
-          <td>
-            <a href="#/people/jan-frans-van-brussel-1761">
-              Jan Frans van Brussel
-            </a>
-          </td>
-
-          <td>m</td>
-          <td>1761</td>
-          <td>1833</td>
-          <td>-</td>
-
-          <td>
-            <a href="#/people/jacobus-bernardus-van-brussel-1736">
-              Jacobus Bernardus van Brussel
-            </a>
-          </td>
-        </tr>
-
-        <tr data-cy="person">
-          <td>
-            <a
-              className="has-text-danger"
-              href="#/people/lievijne-jans-1542"
-            >
-              Lievijne Jans
-            </a>
-          </td>
-
-          <td>f</td>
-          <td>1542</td>
-          <td>1582</td>
-          <td>-</td>
-          <td>-</td>
-        </tr>
-
-        <tr data-cy="person">
-          <td>
-            <a href="#/people/bernardus-de-causmaecker-1721">
-              Bernardus de Causmaecker
-            </a>
-          </td>
-
-          <td>m</td>
-          <td>1721</td>
-          <td>1789</td>
-
-          <td>
-            <a
-              className="has-text-danger"
-              href="#/people/livina-haverbeke-1692"
-            >
-              Livina Haverbeke
-            </a>
-          </td>
-
-          <td>
-            <a href="#/people/lieven-de-causmaecker-1696">
-              Lieven de Causmaecker
-            </a>
-          </td>
-        </tr> */}
       </tbody>
     </table>
   );
