38899daa5c02cb180e3661adcd4cdea99fecd1ab
Vasyl Kozak <sasdaiv@gmail.com>
2023-07-22 12:53:43 +0300

    fixes

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -19,39 +19,40 @@ export const PeopleTable: React.FC<Props> = ({
     <div className="block">
       <div className="box table-container">
         {isLoading && <Loader />}
+        {!isLoading && !isError && (
+          <>
+            {people.length > 0 ? (
+              <table
+                data-cy="peopleTable"
+                className="table is-striped is-hoverable is-narrow is-fullwidth"
+              >
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Sex</th>
+                    <th>Born</th>
+                    <th>Died</th>
+                    <th>Mother</th>
+                    <th>Father</th>
+                  </tr>
+                </thead>
 
-        {isError && !isLoading && (
+                <tbody>
+                  {people.map((person) => (
+                    <PersonItem key={person.slug} person={person} />
+                  ))}
+                </tbody>
+              </table>
+            ) : (
+              <p data-cy="noPeopleMessage">There are no people on the server</p>
+            )}
+          </>
+        )}
+
+        {!isLoading && isError && (
           <p data-cy="peopleLoadingError" className="has-text-danger">
             Something went wrong
           </p>
-        )}
-
-        {!isError && !isLoading && !people.length && (
-          <p data-cy="noPeopleMessage">There are no people on the server</p>
-        )}
-
-        {!isError && !isLoading && people.length > 0 && (
-          <table
-            data-cy="peopleTable"
-            className="table is-striped is-hoverable is-narrow is-fullwidth"
-          >
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Sex</th>
-                <th>Born</th>
-                <th>Died</th>
-                <th>Mother</th>
-                <th>Father</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {people.map((person) => (
-                <PersonItem key={person.slug} person={person} />
-              ))}
-            </tbody>
-          </table>
         )}
       </div>
     </div>
