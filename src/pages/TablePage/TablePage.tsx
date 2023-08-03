eeb21cca03f450e050ea29da25732f7c097a48f5
Skrudj <ssinilov@gmail.com>
2023-08-03 12:48:18 +0300

    implement TablePage component

--- src/pages/TablePage/TablePage.tsx
+++ src/pages/TablePage/TablePage.tsx
@@ -1,5 +1,10 @@
+import { useParams } from 'react-router-dom';
+import { Table } from '../../components/Table';
+
 export const TablePage = () => {
+  const { selectedSlug } = useParams();
+
   return (
-    <h1>Some content</h1>
+    <Table selectedSlug={selectedSlug} />
   );
 };
