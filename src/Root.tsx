40b5e73b5a01bbbac2dda28d31c48f0382c947c2
Vitalii Drabynko <vpdrabynko@gmail.com>
2023-08-03 17:50:30 +0300

    solution

---
+++ src/Root.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  HashRouter as Router, Routes, Route, Navigate,
+} from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './components/Pages/HomePage';
+import { PeoplePage } from './components/Pages/PeoplePage';
+import { NotFoundPage } from './components/Pages/NotFoundPage';
+
+export const Root: React.FC = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+
+        <Route path="people">
+          <Route index element={<PeoplePage />} />
+          <Route path=":slug" element={<PeoplePage />} />
+        </Route>
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  </Router>
+);
