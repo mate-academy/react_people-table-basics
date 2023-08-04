13d4d49401d4181005fabcbf2ae830352ae10c7a
Eugene Kulish <genekulish13@gmail.com>
2023-08-04 09:55:48 +0300

    fix directories

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import { Root } from './pages.tsx/Rooot';
+import { Root } from './pages/Root';
 
 createRoot(document.getElementById('root') as HTMLDivElement)
   .render(
