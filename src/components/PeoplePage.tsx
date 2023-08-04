d80dcea58ba07f99acf53e4c18007e73c6f608c9
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-08-04 17:27:58 +0300

    Add task solution

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { Loader } from './Loader';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { PeopleTable } from './PeopleTable';
+
+const getPerson = (name: string, people: Person[]): Person | undefined => {
+  return people.find(person => person.name === name);
+};
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(peopleFromServer => {
+        setPeople(peopleFromServer.map(person => ({
+          ...person,
+          mother: getPerson(person.motherName || '', peopleFromServer),
+          father: getPerson(person.fatherName || '', peopleFromServer),
+        })));
+      })
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading ? (<Loader />) : (
+            <>
+              {hasError && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  Something went wrong
+                </p>
+              )}
+
+              {people.length === 0 && !hasError && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
+
+              {people.length !== 0 && !hasError && (
+                <PeopleTable people={people} />
+              )}
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
