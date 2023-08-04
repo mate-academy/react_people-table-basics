9c0cf17538316061e0f04d2b4c19eb1551d833ca
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-08-04 16:06:37 +0300

    Divided project  into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
 import { Loader } from './components/Loader';
 
 import './App.scss';
+import { Navigation } from './components/Navigation';
+import { Home } from './components/Home';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="#/">
-            Home
-          </a>
-
-          <a
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
-          >
-            People
-          </a>
-        </div>
-      </div>
-    </nav>
+    <Navigation />
 
     <main className="section">
       <div className="container">
-        <h1 className="title">Home Page</h1>
+        <Home />
+
         <h1 className="title">People Page</h1>
         <h1 className="title">Page not found</h1>
 
