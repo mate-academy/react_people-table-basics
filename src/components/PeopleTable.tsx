9f769fb93116bc511627a8d6c517de0857d74e9c
Yana <yanaklymenko.od@gmail.com>
2023-08-07 01:25:19 +0300

    add task solution

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,28 @@
+import { PersonLink } from './PersonLink';
+import { Person } from '../types/Person';
+
+type Props = {
+  people: Person[]
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => (
+  <table
+    data-cy="peopleTable"
+    className="table is-striped is-hoverable is-narrow is-fullwidth"
+  >
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Sex</th>
+        <th>Born</th>
+        <th>Died</th>
+        <th>Mother</th>
+        <th>Father</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {people.map(person => <PersonLink person={person} key={person.slug} />)}
+    </tbody>
+  </table>
+);
