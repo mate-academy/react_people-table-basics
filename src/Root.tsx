2c8b371278ef394956b78f8aa14c7b59f64b7f8c
Nazar Baraban <barabannazar44@gmail.com>
2023-08-07 10:15:47 +0300

    add task solution

---
+++ src/Root.tsx
@@ -0,0 +1,25 @@
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
+import React from 'react';
+import { App } from './App';
+import { PeoplePage } from './components/PeoplePage';
+
+export const Root = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route index element={<h1 className="title">Home Page</h1>} />
+        <Route path="people">
+          <Route index element={<PeoplePage />} />
+          <Route path=":personSlug" element={<PeoplePage />} />
+        </Route>
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </Router>
+);
