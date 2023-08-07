f8c00e366bae801c05f72ffc056005d427e2ccbb
Valeriy Lopatko <vlopatko1999@gmail.com>
2023-08-07 20:23:31 +0300

    fixing for deploy issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Root } from './Root';
-import React from 'react';
 
 ReactDOM.render(
   <Root />,
