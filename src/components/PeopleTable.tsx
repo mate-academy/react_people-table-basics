fe9f62fd49ca713f2784fdf64d53bef0d625ba14
Mac <mac@MacBook-Pro-Mac.local>
2023-08-10 22:17:39 +0300

    add task solution

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,80 @@
+import classNames from 'classnames';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { Person } from '../types';
+import { PersonLink } from './PersonLink';
+
+type Props = {
+  people: Person[],
+  onError: boolean,
+};
+
+export const PeopleTable: React.FC<Props> = ({
+  people,
+  onError,
+}) => {
+  const { slug } = useParams();
+
+  return (
+    <>
+      {!onError && (
+        people
+          ? (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {people.map(person => (
+                  <tr
+                    data-cy="person"
+                    key={person.slug}
+                    className={classNames({
+                      'has-background-warning': person.slug === slug,
+                    })}
+                  >
+                    <td>
+                      <PersonLink person={person} />
+                    </td>
+
+                    <td>{person.sex}</td>
+                    <td>{person.born}</td>
+                    <td>{person.died}</td>
+                    <td>
+                      {person.mother
+                        ? (
+                          <PersonLink person={person.mother} />
+                        ) : (person.motherName || '-')}
+                    </td>
+
+                    <td>
+                      {person.father
+                        ? (
+                          <PersonLink person={person.father} />
+                        ) : (person.fatherName || '-')}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )
+          : (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )
+      )}
+    </>
+  );
+};
