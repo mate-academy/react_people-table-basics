315a9311a0b4228c732236b8850a7b04631d8170
Liza Strykharchuk <ol220681vol@gmail.com>
2023-08-14 16:17:47 +0300

    codeHome

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,7 +1,7 @@
 export const Home = () => (
   <div className="section">
     <div className="container">
-      <h1 className="title">Home page</h1>
+      <h1 className="title">Home Page</h1>
     </div>
   </div>
 );
