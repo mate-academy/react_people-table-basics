458f0f7b102f3a450f2fabe2b0cd0aadd195243c
Alina <kozlovska.a.mail@gmail.com>
2023-08-31 16:54:17 +0200

    improve code style

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -45,57 +45,66 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
       </thead>
 
       <tbody>
-        {people.map(person => (
-          <tr
-            key={person.slug}
-            data-cy="person"
-            className={cn({
-              'has-background-warning': person.slug === slug,
-            })}
-          >
-            <td>
-              <Personlink person={person} />
-            </td>
+        {people.map(person => {
+          const mother = person.motherName
+            ? getPersonByName(person.motherName)
+            : undefined;
+          const father = person.fatherName
+            ? getPersonByName(person.fatherName)
+            : undefined;
 
-            <td>{person.sex}</td>
-            <td>{person.born}</td>
-            <td>{person.died}</td>
-            <td>
-              {person.motherName ? (
-                <>
-                  {getPersonByName(person.motherName) ? (
-                    <Personlink
-                      person={getPersonByName(person.motherName) as Person}
-                    />
-                  ) : (
-                    <>
-                      {person.motherName}
-                    </>
-                  )}
-                </>
-              ) : (
-                '-'
-              )}
-            </td>
-            <td>
-              {person.fatherName ? (
-                <>
-                  {getPersonByName(person.fatherName) ? (
-                    <Personlink
-                      person={getPersonByName(person.fatherName) as Person}
-                    />
-                  ) : (
-                    <>
-                      {person.fatherName}
-                    </>
-                  )}
-                </>
-              ) : (
-                '-'
-              )}
-            </td>
-          </tr>
-        ))}
+          return (
+            <tr
+              key={person.slug}
+              data-cy="person"
+              className={cn({
+                'has-background-warning': person.slug === slug,
+              })}
+            >
+              <td>
+                <Personlink person={person} />
+              </td>
+
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+              <td>{person.died}</td>
+              <td>
+                {person.motherName ? (
+                  <>
+                    {mother ? (
+                      <Personlink
+                        person={mother}
+                      />
+                    ) : (
+                      <>
+                        {person.motherName}
+                      </>
+                    )}
+                  </>
+                ) : (
+                  '-'
+                )}
+              </td>
+              <td>
+                {person.fatherName ? (
+                  <>
+                    {father ? (
+                      <Personlink
+                        person={father}
+                      />
+                    ) : (
+                      <>
+                        {person.fatherName}
+                      </>
+                    )}
+                  </>
+                ) : (
+                  '-'
+                )}
+              </td>
+            </tr>
+          );
+        })}
       </tbody>
     </table>
   );
