89f4a2cf16c3d7359b57f2d3944be7956288dd62
Hania Nassalska <hanna.nassalska@gmail.com>
2023-09-22 11:54:03 +0200

    Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import './App.scss';
-import { Navigation } from './components/Navigation/Navigation';
-import { PeoplePage } from './components/PeoplePage/PeoplePage';
-import { HomePage } from './components/HomePage/HomePage';
-import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { Navigation } from './components/Navigation';
 
 export const App = () => (
   <div data-cy="app">
     <Navigation />
-    <HomePage />
-    <PeoplePage />
-    <NotFoundPage />
   </div>
 );
