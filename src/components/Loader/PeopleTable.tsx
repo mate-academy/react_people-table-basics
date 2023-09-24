164bdab505ae124b6f3d1157724d588ed3375411
Yevhenii <zheki2004@gmail.com>
2023-09-24 23:43:16 +0300

    done

---
+++ src/components/Loader/PeopleTable.tsx
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react';
+import { Loader } from './Loader';
+import { PersoLink } from './PersonLink';
+import { Person } from '../../types';
+import { getPeople } from '../../api';
+
+export const PeopleTable: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const fetchPeople = async () => {
+    setLoading(true);
+    setError(false);
+
+    try {
+      const peopleFormServer = await getPeople();
+      const childrenWithParents = peopleFormServer.map(person => ({
+        ...person,
+        father: peopleFormServer
+          .find(father => father.name === person.fatherName),
+        mother: peopleFormServer
+          .find(mother => mother.name === person.motherName),
+      }));
+
+      setPeople(childrenWithParents);
+    } catch {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPeople();
+  }, []);
+
+  const noPeopleOnServer = !people.length && !error && !loading;
+
+  return (
+    <div className="block">
+      <div className="box table-container">
+        {loading && <Loader />}
+
+        {error && (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            Something went wrong
+          </p>
+        )}
+
+        {noPeopleOnServer && (
+          <p data-cy="noPeopleMessage">
+            There are no people on the server
+          </p>
+        )}
+
+        {people.length !== 0 && (
+          <table
+            data-cy="peopleTable"
+            className="table is-striped is-hoverable is-narrow is-fullwidth"
+          >
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Sex</th>
+                <th>Born</th>
+                <th>Died</th>
+                <th>Mother</th>
+                <th>Father</th>
+              </tr>
+            </thead>
+            <tbody>
+              {people.map(person => (
+                <PersoLink
+                  key={person.slug}
+                  person={person}
+                />
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/Loader/PersonLink.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersoLink: React.FC<Props> = ({ person }) => {
+  const { userSlug } = useParams();
+
+  return (
+    <tr
+      data-cy="person"
+      className={classNames({
+        'has-background-warning': person.slug === userSlug,
+      })}
+    >
+      <td>
+        <Link
+          to={`/people/${person.slug}`}
+          className={classNames({ 'has-text-danger': person.sex === 'f' })}
+        >
+          {person.name}
+        </Link>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+      <td>
+        {person.mother ? (
+          <Link
+            to={`/people/${person.mother.slug}`}
+          >
+            {person.motherName}
+          </Link>
+        ) : (person.motherName || '-')}
+      </td>
+
+      <td>
+        {person.father ? (
+          <Link
+            to={`/people/${person.father.slug}`}
+          >
+            {person.fatherName}
+          </Link>
+        ) : (person.fatherName || '-')}
+      </td>
+    </tr>
+  );
+};
