e5458316fcca017d575173de1c7320853b8b845d
Anton Tsurkan <wesses1999@gmail.com>
2023-09-26 17:29:33 +0300

    add fetching people

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { HomePage } from './components/HomePage';
 import { NotFoundPage } from './components/NotFoundPage';
@@ -14,6 +14,7 @@ export const App = () => (
             <Route index element={<HomePage />} />
             <Route path="*" element={<NotFoundPage />} />
             <Route path="people" element={<PeoplePage />} />
+            <Route path="home" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
       </div>
--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -1,143 +1,177 @@
+import React, { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
+import { Person } from '../../types';
+import { getPeople } from '../../api';
 
-export const PeoplePage = () => {
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoad, setIsLoad] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const isShowLoad = isLoad && !isError && !people.length;
+  const isShowError = !isLoad && isError && !people.length;
+  const isShowNoPeople = !isLoad && !isError && !people.length;
+  const isShowPeople = !isLoad && !isError && !!people.length;
+
+  useEffect(() => {
+    setIsError(false);
+    setIsLoad(true);
+
+    (async () => {
+      try {
+        setPeople(await getPeople());
+      } catch {
+        setIsError(true);
+      } finally {
+        setIsLoad(false);
+      }
+    })();
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="box table-container">
-          <Loader />
+          {isShowLoad && <Loader />}
 
-          <p data-cy="peopleLoadingError" className="has-text-danger">
-            Something went wrong
-          </p>
+          {isShowError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
 
-          <p data-cy="noPeopleMessage">
-            There are no people on the server
-          </p>
+          {isShowNoPeople && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
 
-          <table
-            data-cy="peopleTable"
-            className="table is-striped is-hoverable is-narrow is-fullwidth"
-          >
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Sex</th>
-                <th>Born</th>
-                <th>Died</th>
-                <th>Mother</th>
-                <th>Father</th>
-              </tr>
-            </thead>
+          {isShowPeople && (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              <tr data-cy="person">
-                <td>
-                  <a href="#/people/jan-van-brussel-1714">
-                    Jan van Brussel
-                  </a>
-                </td>
+              <tbody>
+                <tr data-cy="person">
+                  <td>
+                    <a href="#/people/jan-van-brussel-1714">
+                      Jan van Brussel
+                    </a>
+                  </td>
 
-                <td>m</td>
-                <td>1714</td>
-                <td>1748</td>
-                <td>Joanna van Rooten</td>
-                <td>Jacobus van Brussel</td>
-              </tr>
+                  <td>m</td>
+                  <td>1714</td>
+                  <td>1748</td>
+                  <td>Joanna van Rooten</td>
+                  <td>Jacobus van Brussel</td>
+                </tr>
 
-              <tr data-cy="person">
-                <td>
-                  <a href="#/people/philibert-haverbeke-1907">
-                    Philibert Haverbeke
-                  </a>
-                </td>
+                <tr data-cy="person">
+                  <td>
+                    <a href="#/people/philibert-haverbeke-1907">
+                      Philibert Haverbeke
+                    </a>
+                  </td>
 
-                <td>m</td>
-                <td>1907</td>
-                <td>1997</td>
+                  <td>m</td>
+                  <td>1907</td>
+                  <td>1997</td>
 
-                <td>
-                  <a
-                    className="has-text-danger"
-                    href="#/people/emma-de-milliano-1876"
-                  >
-                    Emma de Milliano
-                  </a>
-                </td>
+                  <td>
+                    <a
+                      className="has-text-danger"
+                      href="#/people/emma-de-milliano-1876"
+                    >
+                      Emma de Milliano
+                    </a>
+                  </td>
 
-                <td>
-                  <a href="#/people/emile-haverbeke-1877">
-                    Emile Haverbeke
-                  </a>
-                </td>
-              </tr>
+                  <td>
+                    <a href="#/people/emile-haverbeke-1877">
+                      Emile Haverbeke
+                    </a>
+                  </td>
+                </tr>
 
-              <tr data-cy="person" className="has-background-warning">
-                <td>
-                  <a href="#/people/jan-frans-van-brussel-1761">
-                    Jan Frans van Brussel
-                  </a>
-                </td>
+                <tr data-cy="person" className="has-background-warning">
+                  <td>
+                    <a href="#/people/jan-frans-van-brussel-1761">
+                      Jan Frans van Brussel
+                    </a>
+                  </td>
 
-                <td>m</td>
-                <td>1761</td>
-                <td>1833</td>
-                <td>-</td>
+                  <td>m</td>
+                  <td>1761</td>
+                  <td>1833</td>
+                  <td>-</td>
 
-                <td>
-                  <a href="#/people/jacobus-bernardus-van-brussel-1736">
-                    Jacobus Bernardus van Brussel
-                  </a>
-                </td>
-              </tr>
+                  <td>
+                    <a href="#/people/jacobus-bernardus-van-brussel-1736">
+                      Jacobus Bernardus van Brussel
+                    </a>
+                  </td>
+                </tr>
 
-              <tr data-cy="person">
-                <td>
-                  <a
-                    className="has-text-danger"
-                    href="#/people/lievijne-jans-1542"
-                  >
-                    Lievijne Jans
-                  </a>
-                </td>
+                <tr data-cy="person">
+                  <td>
+                    <a
+                      className="has-text-danger"
+                      href="#/people/lievijne-jans-1542"
+                    >
+                      Lievijne Jans
+                    </a>
+                  </td>
 
-                <td>f</td>
-                <td>1542</td>
-                <td>1582</td>
-                <td>-</td>
-                <td>-</td>
-              </tr>
+                  <td>f</td>
+                  <td>1542</td>
+                  <td>1582</td>
+                  <td>-</td>
+                  <td>-</td>
+                </tr>
 
-              <tr data-cy="person">
-                <td>
-                  <a href="#/people/bernardus-de-causmaecker-1721">
-                    Bernardus de Causmaecker
-                  </a>
-                </td>
+                <tr data-cy="person">
+                  <td>
+                    <a href="#/people/bernardus-de-causmaecker-1721">
+                      Bernardus de Causmaecker
+                    </a>
+                  </td>
 
-                <td>m</td>
-                <td>1721</td>
-                <td>1789</td>
+                  <td>m</td>
+                  <td>1721</td>
+                  <td>1789</td>
 
-                <td>
-                  <a
-                    className="has-text-danger"
-                    href="#/people/livina-haverbeke-1692"
-                  >
-                    Livina Haverbeke
-                  </a>
-                </td>
+                  <td>
+                    <a
+                      className="has-text-danger"
+                      href="#/people/livina-haverbeke-1692"
+                    >
+                      Livina Haverbeke
+                    </a>
+                  </td>
 
-                <td>
-                  <a href="#/people/lieven-de-causmaecker-1696">
-                    Lieven de Causmaecker
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                  <td>
+                    <a href="#/people/lieven-de-causmaecker-1696">
+                      Lieven de Causmaecker
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          )}
+
         </div>
       </div>
     </>
