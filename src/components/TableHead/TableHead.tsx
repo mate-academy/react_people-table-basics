bc0b9daed33a101cb264d8beb8031d48d4e53459
Andrii Kuz <andrykuz4556@gmail.com>
2023-10-02 13:39:38 +0300

    Solved task

---
+++ src/components/TableHead/TableHead.tsx
@@ -0,0 +1,12 @@
+export const TableHead = () => (
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Sex</th>
+      <th>Born</th>
+      <th>Died</th>
+      <th>Mother</th>
+      <th>Father</th>
+    </tr>
+  </thead>
+);
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line max-len
+export const API_URL = 'https://mate-academy.github.io/react_people-table/api/people.json';
+export const DOWNLOAD_ERROR_MESSAGE = 'Something went wrong';
+export const NOT_FOUND_PEOPLE = 'There are no people on the server';
+export const NO_DATA = '-';
