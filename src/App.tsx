49be756cab8d48f2443d9a0d1915b488cf868851
Nikita <Nehrim24@gmail.com>
2023-10-03 00:52:15 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,138 @@
+import { useState, useEffect } from 'react';
+import classNames from 'classnames';
+import {
+  Link, NavLink, Navigate, Route, Routes, useParams,
+} from 'react-router-dom';
 import { Loader } from './components/Loader';
-
 import './App.scss';
+import { getPeople } from './api';
+import { Person } from './types';
+
+const getLinkClass = ({ isActive }: { isActive: boolean }) => classNames(
+  'navbar-item', { 'has-background-grey-lighter': isActive },
+);
+
+const nothing = '-';
+
+const PeopleTable = ({ people }: { people: Person[] }) => {
+  const { slug } = useParams<{ slug: string }>();
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      {/* Table header */}
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map((person: Person) => (
+          <tr
+            key={person.slug}
+            data-cy="person"
+            className={classNames({
+              'has-background-warning': person.slug === slug,
+            })}
+          >
+            <td>
+              <Link to={`/people/${person.slug}`} className={classNames({ 'has-text-danger': person.sex === 'f' })}>
+                {person.name}
+              </Link>
+            </td>
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              {person.motherName
+                && people.some(man => man.name === person.motherName) ? (
+                  <Link
+                    to={`/people/${people.find(man => man.name === person.motherName)?.slug || ''
+                    }`}
+                    className="has-text-danger"
+                  >
+                    {person.motherName}
+                  </Link>
+                ) : (
+                  person.motherName || nothing
+                )}
+            </td>
+            <td>
+              {person.fatherName
+                && people.some(man => man.name === person.fatherName) ? (
+                  <Link
+                    to={`/people/${people.find(man => man.name === person.fatherName)?.slug || ''
+                    }`}
+                  >
+                    {person.fatherName}
+                  </Link>
+                ) : (
+                  person.fatherName || nothing
+                )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(false);
+
+  useEffect(() => {
+    getPeople()
+      .then((data) => {
+        setPeople(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error('Error fetching people data:', error);
+        setIsLoading(false);
+        setLoadingError(true);
+      });
+  }, []);
+
+  return (
+    <div data-cy="peoplePage">
+      <h1 className="title">People Page</h1>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {loadingError ? (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          ) : (
+            <div>
+              {people.length === 0 ? (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              ) : (
+                <PeopleTable people={people} />
+              )}
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+const NotFoundPage = () => <h1 className="title">Page not found</h1>;
 
 export const App = () => (
   <div data-cy="app">
@@ -12,159 +144,32 @@ export const App = () => (
     >
       <div className="container">
         <div className="navbar-brand">
-          <a className="navbar-item" href="#/">
+          <NavLink className={getLinkClass} to="/">
             Home
-          </a>
+          </NavLink>
 
-          <a
-            className="navbar-item has-background-grey-lighter"
-            href="#/people"
+          <NavLink
+            className={getLinkClass}
+            to="/people"
           >
             People
-          </a>
+          </NavLink>
         </div>
       </div>
     </nav>
 
     <main className="section">
       <div className="container">
-        <h1 className="title">Home Page</h1>
-        <h1 className="title">People Page</h1>
-        <h1 className="title">Page not found</h1>
-
         <div className="block">
           <div className="box table-container">
-            <Loader />
 
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
-
-            <p data-cy="noPeopleMessage">
-              There are no people on the server
-            </p>
-
-            <table
-              data-cy="peopleTable"
-              className="table is-striped is-hoverable is-narrow is-fullwidth"
-            >
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Sex</th>
-                  <th>Born</th>
-                  <th>Died</th>
-                  <th>Mother</th>
-                  <th>Father</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                <tr data-cy="person">
-                  <td>
-                    <a href="#/people/jan-van-brussel-1714">
-                      Jan van Brussel
-                    </a>
-                  </td>
-
-                  <td>m</td>
-                  <td>1714</td>
-                  <td>1748</td>
-                  <td>Joanna van Rooten</td>
-                  <td>Jacobus van Brussel</td>
-                </tr>
-
-                <tr data-cy="person">
-                  <td>
-                    <a href="#/people/philibert-haverbeke-1907">
-                      Philibert Haverbeke
-                    </a>
-                  </td>
-
-                  <td>m</td>
-                  <td>1907</td>
-                  <td>1997</td>
-
-                  <td>
-                    <a
-                      className="has-text-danger"
-                      href="#/people/emma-de-milliano-1876"
-                    >
-                      Emma de Milliano
-                    </a>
-                  </td>
-
-                  <td>
-                    <a href="#/people/emile-haverbeke-1877">
-                      Emile Haverbeke
-                    </a>
-                  </td>
-                </tr>
-
-                <tr data-cy="person" className="has-background-warning">
-                  <td>
-                    <a href="#/people/jan-frans-van-brussel-1761">
-                      Jan Frans van Brussel
-                    </a>
-                  </td>
-
-                  <td>m</td>
-                  <td>1761</td>
-                  <td>1833</td>
-                  <td>-</td>
-
-                  <td>
-                    <a href="#/people/jacobus-bernardus-van-brussel-1736">
-                      Jacobus Bernardus van Brussel
-                    </a>
-                  </td>
-                </tr>
-
-                <tr data-cy="person">
-                  <td>
-                    <a
-                      className="has-text-danger"
-                      href="#/people/lievijne-jans-1542"
-                    >
-                      Lievijne Jans
-                    </a>
-                  </td>
-
-                  <td>f</td>
-                  <td>1542</td>
-                  <td>1582</td>
-                  <td>-</td>
-                  <td>-</td>
-                </tr>
-
-                <tr data-cy="person">
-                  <td>
-                    <a href="#/people/bernardus-de-causmaecker-1721">
-                      Bernardus de Causmaecker
-                    </a>
-                  </td>
-
-                  <td>m</td>
-                  <td>1721</td>
-                  <td>1789</td>
-
-                  <td>
-                    <a
-                      className="has-text-danger"
-                      href="#/people/livina-haverbeke-1692"
-                    >
-                      Livina Haverbeke
-                    </a>
-                  </td>
-
-                  <td>
-                    <a href="#/people/lieven-de-causmaecker-1696">
-                      Lieven de Causmaecker
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <Routes>
+              <Route path="/" element={<h1 className="title">Home Page</h1>} />
+              <Route path="/home" element={<Navigate to="/" />} />
+              <Route path="/people" element={<PeoplePage />} />
+              <Route path="/people/:slug" element={<PeoplePage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
           </div>
         </div>
       </div>
