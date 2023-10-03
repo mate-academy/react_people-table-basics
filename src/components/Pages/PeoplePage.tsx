891d0c48c49dbc707c8b7f55fc7fc4604b0782cd
Vira Lyzohub <vira.vilchinska.dk@gmail.com>
2023-10-03 20:05:00 +0200

    Solution

---
+++ src/components/Pages/PeoplePage.tsx
@@ -0,0 +1,11 @@
+import { PeopleTable } from "../InfoAboutPeople/PeopleTable";
+
+export const PeoplePage = () => {
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <PeopleTable />
+    </>
+  );
+};
