e777745a5955f824f695045ae7bb1671ebc954c1
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-10-03 11:20:19 +0300

    add task solution

---
+++ src/components/Naviagation/Navigation.tsx
@@ -0,0 +1,32 @@
+import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+
+const getNavLinkClasses = ({ isActive }: { isActive: boolean }) => {
+  return classNames('navbar-item', { 'has-background-grey-lighter': isActive });
+};
+
+export const Navigation = () => {
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink className={getNavLinkClasses} to="/">
+            Home
+          </NavLink>
+
+          <NavLink
+            className={getNavLinkClasses}
+            to="/people"
+          >
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
+import { getPeople } from '../../api';
+import { Person } from '../../types';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
+import { getPreparedPeople } from '../../helpers/getPrepearedPeople';
+
+export const PeoplePage = () => {
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [people, setPeople] = useState<Person[]>([]);
+  const shouldPeopleShown = !isError && !isLoading;
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPeople()
+      .then(currentPeople => {
+        const peopleWithParents = getPreparedPeople(currentPeople);
+
+        setPeople(peopleWithParents);
+      })
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && <Loader />}
+          {isError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+          {shouldPeopleShown && <PeopleTable people={people} />}
+        </div>
+      </div>
+    </>
+  );
+};
