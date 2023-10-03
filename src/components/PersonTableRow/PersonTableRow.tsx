3e38166650f52e0968b08c8cc743ed56a40c8dae
Konrad <konrad.domalewski@gmail.com>
2023-10-03 12:30:34 +0200

    solution

---
+++ src/components/PersonTableRow/PersonTableRow.tsx
@@ -0,0 +1,39 @@
+import cn from 'classnames';
+import { useParams } from 'react-router-dom';
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink';
+
+type Props = {
+  person: Person
+};
+
+export const PersonTableRow: React.FC<Props> = ({ person }) => {
+  const { personSlug } = useParams();
+
+  return (
+    <tr
+      data-cy="person"
+      className={cn({
+        'has-background-warning': personSlug === person.slug,
+      })}
+    >
+      <td>
+        <PersonLink person={person} />
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+      <td>
+        {person.mother
+          ? <PersonLink person={person.mother} />
+          : person.motherName || '-'}
+      </td>
+      <td>
+        {person.father
+          ? <PersonLink person={person.father} />
+          : person.fatherName || '-'}
+      </td>
+    </tr>
+  );
+};
