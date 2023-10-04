b7edbc51ff0f000f4e3b6ce445e93f89f726d4c4
danulyk05 <ivan.danulyk2016@gmail.com>
2023-10-04 22:36:35 +0200

    solution router

---
+++ src/Root.tsx
@@ -0,0 +1,55 @@
+import {
+  Navigate,
+  Route,
+  HashRouter as Router,
+  Routes,
+} from 'react-router-dom';
+import { HomePage } from './Pages/HomePage';
+import { PeoplePage } from './Pages/PeoplePage';
+import { NotFoundPage } from './Pages/PageNotFound';
+import { App } from './App';
+
+export const Root = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route
+            path="/"
+            element={
+              <HomePage />
+            }
+          />
+
+          <Route
+            path="/people"
+            element={
+              <PeoplePage />
+            }
+          />
+          <Route
+            path="/people/:personSlug"
+            element={
+              <PeoplePage />
+            }
+          />
+
+          <Route
+            path="/home"
+            element={
+              <Navigate to="/" replace />
+            }
+          />
+
+          <Route
+            path="*"
+            element={
+              <NotFoundPage />
+            }
+          />
+        </Route>
+
+      </Routes>
+    </Router>
+  );
+};
---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,76 @@
+import { useParams } from 'react-router-dom';
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink/PersonLink';
+
+type PeoplePageTableProps = {
+  people: Person[],
+};
+
+export const PeopleTable: React.FC<PeoplePageTableProps> = (
+  { people },
+) => {
+  const { personSlug } = useParams();
+
+  const selectedPerson = people.find(person => person.slug === personSlug);
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            data-cy="person"
+            key={person.slug}
+            className={person.slug === selectedPerson?.slug
+              ? 'has-background-warning' : undefined}
+          >
+            <PersonLink
+              person={person}
+            />
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            {person.mother
+              ? (
+                <PersonLink
+                  person={person.mother}
+                />
+              )
+              : (
+                <td>
+                  {person.motherName !== null
+                    ? person.motherName : '-'}
+                </td>
+              )}
+            {person.father
+              ? (
+                <PersonLink
+                  person={person.father}
+                />
+              )
+              : (
+                <td>
+                  {person.fatherName !== null
+                    ? person.fatherName : '-'}
+                </td>
+              )}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
