2b82dc45f40c1510d628489be061f4dd24a3d57c
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-10-04 16:33:58 +0200

    add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,18 @@
-import cn from 'classnames';
 import {
   Navigate,
   Route,
   Routes,
-  NavLink,
 } from 'react-router-dom';
 
 import './App.scss';
 import { HomePage } from './pages/HomePage';
 import { PeoplePage } from './pages/PeoplePage';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { Navbar } from './components/Navbar';
 
 export const App = () => (
   <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={
-              ({ isActive }) => cn('navbar-item', {
-                'has-background-grey-lighter': isActive,
-              })
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/people"
-            className={
-              ({ isActive }) => cn('navbar-item', {
-                'has-background-grey-lighter': isActive,
-              })
-            }
-          >
-            People
-          </NavLink>
-        </div>
-      </div>
-    </nav>
+    <Navbar />
 
     <main className="section">
       <div className="container">
