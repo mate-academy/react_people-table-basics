d6c6070e350c4167631ac083c217a0db750587f6
Kamil <majcherczak.kamil@protonmail.com>
2023-10-04 13:15:52 +0200

    add task solution

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { Loader } from '../Loader/Loader';
+import { getPeople } from '../../api';
+import { Person } from '../../types';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
+
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPeople()
+      .then((data) => setPeople(data))
+      .catch(() => setError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          { isLoading
+            ? <Loader />
+            : error && (
+              <p data-cy="peopleLoadingError" className="has-text-danger">
+                Something went wrong
+              </p>
+            )}
+
+          { (!error && people.length === 0 && !isLoading) && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+
+          {(!error && people.length && !isLoading)
+          && <PeopleTable people={people} />}
+
+        </div>
+      </div>
+    </>
+  );
+};
