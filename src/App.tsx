fefdf6ad2862acc9cdb6a1792c38c37918812aba
haduigon <haduigon@gmail.com>
2023-10-05 18:44:26 +0300

    everything seems done

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
 import { Outlet, NavLink } from 'react-router-dom';
 import './App.scss';
+import classNames from 'classnames';
 
-export const App = () => (
-  <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink className="navbar-item" to="/">
-            Home
-          </NavLink>
+export const App = () => {
+  const linkClass = (
+    { isActive }: { isActive: boolean },
+  ) => classNames('navbar-item', { 'has-background-grey-lighter': isActive });
 
-          <NavLink
-            className="navbar-item has-background-grey-lighter"
-            to="/people"
-          >
-            People
-          </NavLink>
+  return (
+    <div data-cy="app">
+      <nav
+        data-cy="nav"
+        className="navbar is-fixed-top has-shadow"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink className={linkClass} to="/">
+              Home
+            </NavLink>
+
+            <NavLink
+              className={linkClass}
+              to="/people"
+            >
+              People
+            </NavLink>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
 
-    <main className="section">
-      <div className="container">
-        <Outlet />
-      </div>
-    </main>
-  </div>
-);
+      <main className="section">
+        <div className="container">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+};
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,26 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import {
+  HashRouter,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 import { App } from './App';
 import { People } from './components/PeoplePage';
 
 export const Root = () => (
   <HashRouter>
     <Routes>
+      <Route path="/home" element={<Navigate to="/" replace />} />
       <Route path="/" element={<App />}>
         <Route index element={<h1 className="title">Home Page</h1>} />
         <Route path="people">
           <Route index element={<People />} />
           <Route path=":personSlug" element={<People />} />
         </Route>
+        <Route
+          path="*"
+          element={<p className="title">Page not found</p>}
+        />
       </Route>
     </Routes>
   </HashRouter>
