2d48ad0f2afcd41db31e6154dab6f6bb9c1dbc3a
Piotr Dzialo <piotr.sluzbowy@wp.pl>
2023-10-06 09:38:14 +0200

    fixing people not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import './App.scss';
 import { Navigate, Route, Routes } from 'react-router-dom';
@@ -14,18 +15,11 @@ export const App: React.FC = () => {
           <Routes>
             <Route path="/" element={<Navigation />}>
               <Route index element={<h1 className="title">Home Page</h1>} />
-              <Route
-                path="*"
-                element={<h1 className="title">Page not found</h1>}
-              />
-              <Route
-                path="people"
-                element={<PeoplePage />}
-              >
-                <Route
-                  path="person/:personSlug"
-                  element={<PeoplePage />}
-                />
+              <Route path="*">
+                <Route index element={<h1 className="title">Page not found</h1>} />
+              </Route>
+              <Route path="people" element={<PeoplePage />}>
+                <Route path=":personSlug" element={<PeoplePage />} />
               </Route>
               <Route path="home" element={<Navigate to="/" replace />} />
             </Route>
