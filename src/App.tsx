31cbe8a4bb922879867337455fc4fe6ea4313c4b
arturwisnia <wisnia3008@interia.pl>
2023-10-09 13:11:46 +0200

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,27 @@
-import {
-  NavLink, Navigate, Route, Routes,
-} from 'react-router-dom';
-import cn from 'classnames';
-
 import './App.scss';
-import { PeoplePage } from './components/People';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+import { PageNotFound } from './components/PageNotFound';
+import { Navbar } from './components/Navbar';
 
-export const App = () => (
-  <div data-cy="app">
-    <nav
-      data-cy="nav"
-      className="navbar is-fixed-top has-shadow"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div className="container">
-        <div className="navbar-brand">
-          <NavLink
-            to="/"
-            className={({ isActive }) => cn('navbar-item',
-              { 'has-background-grey-lighter': isActive })}
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/people"
-            className={({ isActive }) => cn('navbar-item',
-              { 'has-background-grey-lighter': isActive })}
-          >
-            People
-          </NavLink>
+export const App = () => {
+  return (
+    <div data-cy="app">
+
+      <Navbar />
+
+      <main className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/people" element={<PeoplePage />} />
+            <Route path="/people/:slug" element={<PeoplePage />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
         </div>
-      </div>
-    </nav>
-
-    <main className="section">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<h1 className="title">Home Page</h1>} />
-          <Route
-            path="/home"
-            element={<Navigate to="/" />}
-          />
-          <Route
-            path="/people"
-            element={<PeoplePage />}
-          />
-          <Route
-            path="/people/:personSlug"
-            element={<PeoplePage />}
-          />
-          <Route
-            path="/*"
-            element={<h1 className="title">Page not found</h1>}
-          />
-        </Routes>
-      </div>
-    </main>
-  </div>
-);
+      </main>
+    </div>
+  );
+};
--- src/components/PersonInfo.tsx
+++ src/components/PersonInfo.tsx
@@ -1,61 +1,17 @@
-import { NavLink, useLocation } from 'react-router-dom';
-import cn from 'classnames';
-import { Person } from '../types';
+import { Link } from 'react-router-dom';
+import { PersonLinkProps } from '../types';
 
-type PersonInfoProps = {
-  person: Person;
-  people: Person[];
-};
+export const PersonLink: React.FC<PersonLinkProps> = ({ person, people }) => {
+  const existingPerson = people.find(p => p.name === person);
 
-export const PersonInfo = ({ person, people }: PersonInfoProps) => {
-  const location = useLocation();
-  const personFather = people.find(p => p.name === person.fatherName);
-  const personMother = people.find(p => p.name === person.motherName);
-
-  return (
-    <tr
-      data-cy="person"
-      className={cn({
-        'has-background-warning': location.pathname === `/people/${person.slug}`,
-      })}
+  return existingPerson ? (
+    <Link
+      to={`/people/${existingPerson.slug}`}
+      className={existingPerson.sex === 'f' ? 'has-text-danger' : ''}
     >
-      <td>
-        <NavLink
-          to={`/people/${person.slug}`}
-          className={cn({
-            'has-text-danger': person.sex === 'f',
-          })}
-        >
-          {person.name}
-        </NavLink>
-      </td>
-
-      <td>{person.sex}</td>
-      <td>{person.born}</td>
-      <td>{person.died}</td>
-      <td>
-        {personMother
-          ? (
-            <NavLink
-              className="has-text-danger"
-              to={`/people/${personMother.slug}`}
-            >
-              {personMother.name}
-            </NavLink>
-          ) : person.motherName || '-'}
-
-      </td>
-      <td>
-        {personFather
-          ? (
-            <NavLink
-              to={`/people/${personFather.slug}`}
-            >
-              {personFather.name}
-            </NavLink>
-          ) : person.fatherName || '-'}
-
-      </td>
-    </tr>
+      {person}
+    </Link>
+  ) : (
+    <span>{person || '-'}</span>
   );
 };
