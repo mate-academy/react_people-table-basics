533570bc985a779c183ea27230741aeba3cb4815
Daryna <108665898+daryna-hnidash@users.noreply.github.com>
2023-10-10 18:05:00 +0300

    Update src/components/PeoplePage/PeoplePage.tsx
    Co-authored-by: Lera <starynetsv@gmail.com>

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -31,7 +31,7 @@ export const PeoplePage = () => {
             </p>
           )}
 
-          {!isLoading && people.length === 0 && (
+          {!isLoading && !people.length && (
             <p data-cy="noPeopleMessage">
               There are no people on the server
             </p>
