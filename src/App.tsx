a977d946a007939835e5438827c7373b90011408
Nikita <Nehrim24@gmail.com>
2023-10-11 16:45:44 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,138 +1,14 @@
-import { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import {
-  Link, NavLink, Navigate, Route, Routes, useParams,
+  NavLink, Navigate, Route, Routes,
 } from 'react-router-dom';
-import { Loader } from './components/Loader';
 import './App.scss';
-import { getPeople } from './api';
-import { Person } from './types';
+import { NotFoundPage } from './components/NotFoundPage';
+import { PeoplePage } from './components/PeoplePage';
 
 const getLinkClass = ({ isActive }: { isActive: boolean }) => classNames(
   'navbar-item', { 'has-background-grey-lighter': isActive },
 );
-
-const NOTHING = '-';
-
-const PeopleTable = ({ people }: { people: Person[] }) => {
-  const { slug } = useParams<{ slug: string }>();
-
-  return (
-    <table
-      data-cy="peopleTable"
-      className="table is-striped is-hoverable is-narrow is-fullwidth"
-    >
-      {/* Table header */}
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Sex</th>
-          <th>Born</th>
-          <th>Died</th>
-          <th>Mother</th>
-          <th>Father</th>
-        </tr>
-      </thead>
-
-      <tbody>
-        {people.map((person: Person) => (
-          <tr
-            key={person.slug}
-            data-cy="person"
-            className={classNames({
-              'has-background-warning': person.slug === slug,
-            })}
-          >
-            <td>
-              <Link to={`/people/${person.slug}`} className={classNames({ 'has-text-danger': person.sex === 'f' })}>
-                {person.name}
-              </Link>
-            </td>
-            <td>{person.sex}</td>
-            <td>{person.born}</td>
-            <td>{person.died}</td>
-            <td>
-              {person.motherName
-                && people.some(man => man.name === person.motherName) ? (
-                  <Link
-                    to={`/people/${people.find(man => man.name === person.motherName)?.slug || ''
-                    }`}
-                    className="has-text-danger"
-                  >
-                    {person.motherName}
-                  </Link>
-                ) : (
-                  person.motherName || NOTHING
-                )}
-            </td>
-            <td>
-              {person.fatherName
-                && people.some(man => man.name === person.fatherName) ? (
-                  <Link
-                    to={`/people/${people.find(man => man.name === person.fatherName)?.slug || ''
-                    }`}
-                  >
-                    {person.fatherName}
-                  </Link>
-                ) : (
-                  person.fatherName || NOTHING
-                )}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-};
-
-export const PeoplePage = () => {
-  const [people, setPeople] = useState<Person[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadingError, setLoadingError] = useState(false);
-
-  useEffect(() => {
-    getPeople()
-      .then((data) => {
-        setPeople(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        // eslint-disable-next-line no-console
-        console.error('Error fetching people data:', error);
-        setIsLoading(false);
-        setLoadingError(true);
-      });
-  }, []);
-
-  return (
-    <div data-cy="peoplePage">
-      <h1 className="title">People Page</h1>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div>
-          {loadingError ? (
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
-          ) : (
-            <div>
-              {people.length === 0 ? (
-                <p data-cy="noPeopleMessage">
-                  There are no people on the server
-                </p>
-              ) : (
-                <PeopleTable people={people} />
-              )}
-            </div>
-          )}
-        </div>
-      )}
-    </div>
-  );
-};
-
-const NotFoundPage = () => <h1 className="title">Page not found</h1>;
 
 export const App = () => (
   <div data-cy="app">
