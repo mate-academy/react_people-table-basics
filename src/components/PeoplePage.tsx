ecec1b01de049beaf1260b792df7ff8981ebfc79
Anna Grechana <grechanaanna@gmial.com>
2023-10-16 00:06:26 +0300

    1st version

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,117 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { Loader } from './Loader';
+import { Error } from './Error';
+
+export const PeoplePage = () => {
+  const [peopleFromServer, setPeopleFromServer] = useState<Person[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const { slugName } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    getPeople()
+      .then((people) => {
+        setPeopleFromServer(people);
+        setLoading(false);
+      })
+      .catch(() => {
+        <Error />;
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+
+          {loading && <Loader />}
+
+          {!loading && peopleFromServer.length < 1 && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+
+          {!loading && (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {peopleFromServer.map(person => {
+                  const mother = peopleFromServer
+                    .find(pers => pers.name === person.motherName);
+                  const father = peopleFromServer
+                    .find(pers => pers.name === person.fatherName);
+
+                  return (
+                    <tr
+                      data-cy="person"
+                      key={person.slug}
+                      className={classNames({
+                        'has-background-warning': person.slug === slugName,
+                      })}
+                    >
+                      <td>
+                        <Link
+                          className={
+                            person.sex === 'f' ? 'has-text-danger' : ''
+                          }
+                          to={`/people/${person.slug}`}
+                        >
+                          {person.name}
+                        </Link>
+                      </td>
+
+                      <td>{person.sex}</td>
+                      <td>{person.born}</td>
+                      <td>{person.died}</td>
+
+                      {mother ? (
+                        <td>
+                          <Link to={`/people/${mother.slug}`} className="has-text-danger">
+                            {mother.name}
+                          </Link>
+                        </td>
+                      ) : (
+                        <td>{person.motherName || '-'}</td>
+                      )}
+
+                      {father ? (
+                        <td>
+                          <Link to={`/people/${father.slug}`}>
+                            {father.name}
+                          </Link>
+                        </td>
+                      ) : (
+                        <td>{person.fatherName || '-'}</td>
+                      )}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
