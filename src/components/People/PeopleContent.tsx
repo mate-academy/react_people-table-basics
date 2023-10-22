7e0ab28a4e8880d039b12b5aa7399cbff24b0ce5
donizer <bogdan.ua45@gmail.com>
2023-10-22 18:35:52 +0300

    fix

--- src/components/People/PeopleContent.tsx
+++ src/components/People/PeopleContent.tsx
@@ -21,7 +21,7 @@ export const PeopleContent = () => {
           <p data-cy="noPeopleMessage">There are no people on the server</p>
         )}
 
-        {people.length && <PeopleTable />}
+        {!!people.length && <PeopleTable />}
       </div>
     </>
   );
