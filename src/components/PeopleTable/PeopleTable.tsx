7ff071b83eb8e3545401dea8fc76ad1336f311cf
Maksym Bevskyi <bevskiymaks@gmail.com>
2023-10-30 14:54:47 +0100

    add task soltuion

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,68 @@
+import { useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink/PersonLink';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const { slugId } = useParams();
+
+  const getParentLink = (name: string | null) => {
+    if (!name) {
+      return '-';
+    }
+
+    const parent = people.find(par => par.name === name);
+
+    if (parent) {
+      return (
+        <PersonLink person={parent} />
+      );
+    }
+
+    return name;
+  };
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            data-cy="person"
+            key={person.slug}
+            className={classNames({
+              'has-background-warning': person.slug === slugId,
+            })}
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>{getParentLink(person.motherName)}</td>
+            <td>{getParentLink(person.fatherName)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
