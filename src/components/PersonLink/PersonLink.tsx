8311c9df3d279dd34121898af6cc9a14ac45cd11
dima.rogkov <dima.rogkov@cheitgroup.com>
2023-11-10 20:44:27 +0200

    add solution

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import cn from 'classnames';
+import { Person } from '../../types';
+
+type Props = {
+  person: Person;
+};
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  const { slug } = useParams();
+
+  return (
+    <Link
+      to={slug ? `../${person.slug}` : `${person.slug}`}
+      className={cn({
+        'has-text-danger': person.sex === 'f',
+      })}
+    >
+      {person.name}
+    </Link>
+  );
+};
