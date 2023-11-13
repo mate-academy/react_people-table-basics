60e41b1516b7b137a0dd0bd9b458f99ae96d5769
Yuliia Dub <yuliia.kasaraba@gmail.com>
2023-11-13 16:49:02 +0200

    add task solution

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
-import { Person } from '../../types';
 import { Link, useParams } from 'react-router-dom';
+import { Person } from '../../types';
 
 type Props = {
   person: Person
