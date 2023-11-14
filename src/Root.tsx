d492fe3208a5c2cf50e0c6e18b45007ef2bec9b0
Roman Ostrous <ostrous.roman@gmail.com>
2023-11-14 15:21:25 +0200

    add task solution

---
+++ src/Root.tsx
@@ -0,0 +1,31 @@
+import {
+  Navigate,
+  Route,
+  HashRouter as Router,
+  Routes,
+} from 'react-router-dom';
+
+import { HomePage } from './pages/HomePage';
+import { PeoplePage } from './pages/PeoplePage';
+import { App } from './App';
+
+export const Root = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="people">
+          <Route path=":slug?" element={<PeoplePage />} />
+        </Route>
+
+        <Route
+          path="*"
+          element={
+            <h1 className="title">Page not found</h1>
+          }
+        />
+      </Route>
+    </Routes>
+  </Router>
+);
---
+++ src/pages/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,136 @@
+import { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../../types';
+import { getPeople } from '../../api';
+import { Loader } from '../../components/Loader';
+
+const preparePeople = (people: Person[]) => {
+  return people.map(person => {
+    const mother = people.find(pers => pers.name === person.motherName);
+    const father = people.find(pers => pers.name === person.fatherName);
+
+    return ({
+      ...person,
+      mother,
+      father,
+    });
+  });
+};
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getPeople()
+      .then((response) => {
+        setPeople(preparePeople(response));
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  const { slug } = useParams();
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {loading && (
+            <Loader />
+          )}
+
+          {error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!loading && !people.length && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
+
+          {people.length > 0 && (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {people.map(person => (
+                  <tr
+                    data-cy="person"
+                    key={person.slug}
+                    className={classNames({
+                      'has-background-warning': person.slug === slug,
+                    })}
+                  >
+                    <td>
+                      <Link
+                        className={classNames({
+                          'has-text-danger': person.sex === 'f',
+                        })}
+                        to={`../${person.slug}`}
+                      >
+                        {person.name}
+                      </Link>
+                    </td>
+
+                    <td>{person.sex}</td>
+                    <td>{person.born}</td>
+                    <td>{person.died}</td>
+
+                    <td>
+                      {person.mother ? (
+                        <Link
+                          className="has-text-danger"
+                          to={`../${person.mother.slug}`}
+                        >
+                          {person.motherName}
+                        </Link>
+                      ) : (
+                        person.motherName || '-'
+                      )}
+                    </td>
+
+                    <td>
+                      {person.father ? (
+                        <Link to={`../${person.father.slug}`}>
+                          {person.fatherName}
+                        </Link>
+                      ) : (
+                        person.fatherName || '-'
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
