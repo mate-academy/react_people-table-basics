a21494847e8fa38e55768a03e3dd41646959243b
Oleh Kuper <ok1804@ukr.net>
2023-11-15 16:36:14 +0200

    add people table solution

---
+++ src/components/People/People.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
+import { Person } from '../../types';
+import { getPeople } from '../../api';
+import { PeopleTable } from '../PeopleTable';
+
+export const People = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isShownPeople, setIsShownPeople] = useState(false);
+  const [shownError, setShownError] = useState('');
+
+  useEffect(() => {
+    getPeople()
+      .then((data) => {
+        setPeople(data);
+        setIsShownPeople(true);
+        if (!data.length) {
+          setShownError('There are no people on the server');
+        }
+      })
+      .catch((error) => {
+        setShownError('Something went wrong');
+        throw error;
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="block">
+      <div className="box table-container">
+        {isLoading && <Loader />}
+
+        {shownError === 'Something went wrong' && (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            {shownError}
+          </p>
+        )}
+
+        {shownError === 'There are no people on the server' && (
+          <p data-cy="noPeopleMessage">
+            {shownError}
+          </p>
+        )}
+
+        {isShownPeople && <PeopleTable people={people} />}
+      </div>
+    </div>
+  );
+};
