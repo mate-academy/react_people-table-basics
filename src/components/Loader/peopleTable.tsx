f1b2febb05636f7368a31b975cd6b7a53d858b42
Dmytro Panin <dmitriyp1598@gmail.com>
2023-11-16 23:12:31 +0200

    add task solution

---
+++ src/components/Loader/peopleTable.tsx
@@ -0,0 +1,236 @@
+import { useEffect, useState } from 'react';
+import cn from 'classnames';
+import { Link, useParams } from 'react-router-dom';
+import { getPeople } from '../../api';
+import { Person } from '../../types';
+import { Loader } from './Loader';
+
+export const PeopleTable: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const { personId } = useParams();
+  const selectedPerson = people.find(person => person.slug === personId);
+  const [isPeopleLoaded, setIsPeopleLoaded] = useState(true);
+  const [isPeopleLoading, setIsPeopleLoading] = useState(false);
+
+  const loadPeople = async () => {
+    setIsPeopleLoading(true);
+
+    try {
+      const peopleData = await getPeople();
+
+      setPeople(peopleData);
+    } catch {
+      setIsPeopleLoaded(false);
+    } finally {
+      setIsPeopleLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadPeople();
+  }, []);
+
+  const isMotherExistInList = (motherName?: string | null) => {
+    return people.find(person => person.name === motherName);
+  };
+
+  const isFatherExistInList = (fatherName?: string | null) => {
+    return people.find(person => person.name === fatherName);
+  };
+
+  const getParent = (person: Person, parentType: 'father' | 'mother') => {
+    const parentTypeName = parentType === 'mother'
+      ? 'motherName'
+      : 'fatherName';
+
+    if (!person[parentTypeName]) {
+      return <td>-</td>;
+    }
+
+    const foundPerson = parentType === 'mother'
+      ? isMotherExistInList(person[parentTypeName])
+      : isFatherExistInList(person[parentTypeName]);
+
+    if (foundPerson) {
+      return (
+        <td>
+          <Link
+            className={cn({
+              'has-text-danger': foundPerson.sex === 'f',
+            })}
+            to={`/people/${foundPerson.slug}`}
+          >
+            {foundPerson.name}
+          </Link>
+        </td>
+      );
+    }
+
+    return (
+      <td>
+        {person[parentTypeName]}
+      </td>
+    );
+  };
+
+  // console.log(people);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      {/* {isPeopleLoaded && !isPeopleLoading */}
+      {isPeopleLoading && (<Loader />)}
+
+      {!isPeopleLoading && isPeopleLoaded && (
+        <table
+          data-cy="peopleTable"
+          className="table is-striped is-hoverable is-narrow is-fullwidth"
+        >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Sex</th>
+              <th>Born</th>
+              <th>Died</th>
+              <th>Mother</th>
+              <th>Father</th>
+            </tr>
+          </thead>
+
+          {people.map(person => (
+            <tbody
+              key={person.slug}
+              className={cn({
+                'has-background-warning': person.slug === selectedPerson?.slug,
+              })}
+            >
+              <tr data-cy="person">
+                <td>
+                  <Link
+                    to={`/people/${person.slug}`}
+                    className={cn({
+                      'has-text-danger': person.sex === 'f',
+                    })}
+                  >
+                    {person.name}
+                  </Link>
+                </td>
+
+                <td>{person.sex}</td>
+                <td>{person.born}</td>
+                <td>{person.died}</td>
+                {getParent(person, 'mother')}
+                {getParent(person, 'father')}
+              </tr>
+
+              {/* <tr data-cy="person">
+              <td>
+                <a href="#/people/philibert-haverbeke-1907">
+                  Philibert Haverbeke
+                </a>
+              </td>
+
+              <td>m</td>
+              <td>1907</td>
+              <td>1997</td>
+
+              <td>
+                <a
+                  className="has-text-danger"
+                  href="#/people/emma-de-milliano-1876"
+                >
+                  Emma de Milliano
+                </a>
+              </td>
+
+              <td>
+                <a href="#/people/emile-haverbeke-1877">
+                  Emile Haverbeke
+                </a>
+              </td>
+            </tr> */}
+
+              {/* <tr data-cy="person" className="has-background-warning">
+            <td>
+              <a href="#/people/jan-frans-van-brussel-1761">
+                Jan Frans van Brussel
+              </a>
+            </td>
+
+            <td>m</td>
+            <td>1761</td>
+            <td>1833</td>
+            <td>-</td>
+
+            <td>
+              <a href="#/people/jacobus-bernardus-van-brussel-1736">
+                Jacobus Bernardus van Brussel
+              </a>
+            </td>
+          </tr>
+
+          <tr data-cy="person">
+            <td>
+              <a
+                className="has-text-danger"
+                href="#/people/lievijne-jans-1542"
+              >
+                Lievijne Jans
+              </a>
+            </td>
+
+            <td>f</td>
+            <td>1542</td>
+            <td>1582</td>
+            <td>-</td>
+            <td>-</td>
+          </tr>
+
+          <tr data-cy="person">
+            <td>
+              <a href="#/people/bernardus-de-causmaecker-1721">
+                Bernardus de Causmaecker
+              </a>
+            </td>
+
+            <td>m</td>
+            <td>1721</td>
+            <td>1789</td>
+
+            <td>
+              <a
+                className="has-text-danger"
+                href="#/people/livina-haverbeke-1692"
+              >
+                Livina Haverbeke
+              </a>
+            </td>
+
+            <td>
+              <a href="#/people/lieven-de-causmaecker-1696">
+                Lieven de Causmaecker
+              </a>
+            </td>
+          </tr> */}
+            </tbody>
+          ))}
+        </table>
+      )}
+
+      {!isPeopleLoaded && (
+        <div className="block">
+          <div className="box table-container">
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
