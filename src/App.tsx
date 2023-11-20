58f8cb122d0723bdc0e18227b3bd944e194ed57d
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-11-20 14:35:42 +0200

    remove some useless code

--- src/App.tsx
+++ src/App.tsx
@@ -13,14 +13,14 @@ export const App = () => {
           <div className="navbar-brand">
             <NavLink
               to="/"
-              className={({ isActive }) => `navbar-item ${isActive ? 'navbar-item has-background-grey-lighter' : ''}`}
+              className={({ isActive }) => `navbar-item ${isActive && 'navbar-item has-background-grey-lighter'}`}
             >
               Home
             </NavLink>
 
             <NavLink
               to="/people"
-              className={({ isActive }) => `navbar-item ${isActive ? 'navbar-item has-background-grey-lighter' : ''}`}
+              className={({ isActive }) => `navbar-item ${isActive && 'navbar-item has-background-grey-lighter'}`}
             >
               People
             </NavLink>
