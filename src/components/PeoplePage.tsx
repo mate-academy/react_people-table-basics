9acd584f82e3dab65eeb5b1ba01682784aed93b8
Maksym_Kos <maxkos18@gmail.com>
2023-12-06 11:29:02 +0200

    fix

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -42,19 +42,18 @@ export const PeoplePage: React.FC = () => {
             )}
 
           {noData
-            ? (
+            && (
               <p data-cy="noPeopleMessage">
                 There are no people on the server
               </p>
-            )
-            : ''}
+            )}
 
           <table
             data-cy="peopleTable"
             className="table is-striped is-hoverable is-narrow is-fullwidth"
           >
             <tbody>
-              {people ? <PeopleTable people={people} /> : ''}
+              {people && <PeopleTable people={people} />}
             </tbody>
           </table>
         </div>
--- src/components/PeopleTable.tsx
+++ src/components/PeopleTable.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PersonType } from '../types';
 import { Person } from './Person';
 
@@ -7,36 +7,44 @@ type Props = {
   people: PersonType[];
 };
 
-export const PeopleTable: React.FC<Props> = ({ people }) => (
-  <table
-    data-cy="peopleTable"
-    className="table is-striped is-hoverable is-narrow is-fullwidth"
-  >
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Sex</th>
-        <th>Born</th>
-        <th>Died</th>
-        <th>Mother</th>
-        <th>Father</th>
-      </tr>
-    </thead>
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  useEffect(() => {
+    people.map(person => {
+      if (person.motherName) {
+        person.mother = people.find(p => p.name === person.motherName);
+      }
 
-    <tbody>
-      {people.map(person => {
-        if (person.motherName) {
-          person.mother = people.find(p => p.name === person.motherName);
-        }
+      if (person.fatherName) {
+        person.father = people.find(p => p.name === person.fatherName);
+      }
 
-        if (person.fatherName) {
-          person.father = people.find(p => p.name === person.fatherName);
-        }
+      return people;
+    });
+  });
 
-        return (
-          <Person person={person} key={person.slug} />
-        );
-      })}
-    </tbody>
-  </table>
-);
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => {
+          return (
+            <Person person={person} key={person.slug} />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
