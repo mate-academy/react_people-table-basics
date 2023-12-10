1f39cf857b6c4dba278e73c0e393b980412aec05
Aleksey-Aleksey <l0734062646@gmail.com>
2023-12-10 15:19:17 +0200

    done

---
+++ src/PeoplePage.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react';
+import { Loader } from './components/Loader';
+import { Person } from './types';
+import { getPeople } from './api';
+import { PersonLink } from './PersonLink';
+
+export const PeoplePage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(pers => setPeople(pers))
+      .catch(() => {
+        setErrorMessage('Something went wrong');
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">People Page</h1>
+
+        <div className="block">
+          <div className="box table-container">
+            {isLoading && <Loader />}
+
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              {errorMessage}
+            </p>
+
+            {people.length === 0 && !isLoading && (
+              <p data-cy="noPeopleMessage">
+                There are no people on the server
+              </p>
+            )}
+
+            <PersonLink
+              people={people}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
