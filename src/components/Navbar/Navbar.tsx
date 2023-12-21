0981366acd7492e2749ae429577e1a5d8de95a0c
Iryna Konovalova <iconovalova19@gmail.com>
2023-12-21 12:57:58 +0100

    Solution

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import {
+  NavLink,
+  Navigate, Outlet, useLocation,
+} from 'react-router-dom';
+import cn from 'classnames';
+import { LinkPath } from '../../types/LinkPath';
+
+export const Navbar = () => {
+  const { pathname } = useLocation();
+
+  if (pathname === LinkPath.Home) {
+    return <Navigate to="/" replace />;
+  }
+
+  return (
+    <div data-cy="app">
+      <nav
+        data-cy="nav"
+        className="navbar is-fixed-top has-shadow"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink
+              to="/"
+              className={({ isActive }) => cn(
+                'navbar-item',
+                {
+                  'has-background-grey-lighter': isActive,
+                },
+              )}
+            >
+              Home
+            </NavLink>
+
+            <NavLink
+              to="people"
+              className={({ isActive }) => cn(
+                'navbar-item',
+                {
+                  'has-background-grey-lighter': isActive,
+                },
+              )}
+            >
+              People
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+
+      <main className="section">
+        <div className="container">
+          <Outlet />
+        </div>
+      </main>
+    </div>
+  );
+};
