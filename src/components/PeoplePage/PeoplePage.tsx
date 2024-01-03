f5c6aef0af9739e9f7390efefbf960b35a8aeae7
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-01-03 14:02:15 +0100

    Introduce loading and error handling in PeoplePage

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -6,10 +6,16 @@ import { PeopleTable } from '../PeopleTable';
 
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
+  const [error, setError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     getPeople()
-      .then(allPeople => setPeople(allPeople));
+      .then(allPeople => setPeople(allPeople))
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => setIsLoading(false));
   }, []);
 
   return (
@@ -17,41 +23,47 @@ export const PeoplePage = () => {
       <h1 className="title">People Page</h1>
       <div className="block">
         <div className="box table-container">
-          <Loader />
+          {isLoading && <Loader />}
 
-          <p data-cy="peopleLoadingError" className="has-text-danger">
-            Something went wrong
-          </p>
+          {error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
 
-          <p data-cy="noPeopleMessage">
-            There are no people on the server
-          </p>
+          {(people.length === 0 && !isLoading) && (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          )}
 
-          <table
-            data-cy="peopleTable"
-            className="table is-striped is-hoverable is-narrow is-fullwidth"
-          >
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Sex</th>
-                <th>Born</th>
-                <th>Died</th>
-                <th>Mother</th>
-                <th>Father</th>
-              </tr>
-            </thead>
+          {!isLoading && (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
 
-            <tbody>
-              {people.map(person => (
-                <PeopleTable
-                  key={person.slug}
-                  people={people}
-                  person={person}
-                />
-              ))}
-            </tbody>
-          </table>
+              <tbody>
+                {people.map(person => (
+                  <PeopleTable
+                    key={person.slug}
+                    people={people}
+                    person={person}
+                  />
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
 
