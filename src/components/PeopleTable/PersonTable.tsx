dbed1019f2007ab033267eb0785217ac00dd70b8
Oleksandr Kosenko <d1izel@ukr.net>
2024-01-12 11:06:48 +0200

    add react_people-table-basic

---
+++ src/components/PeopleTable/PersonTable.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Person } from '../../types';
+import { PersonLink } from '../PersonLink';
+
+type Props = {
+  people: Person[]
+};
+
+export const PersonTable: React.FC<Props> = ({ people }) => {
+  const peoplethDuplications = [...people].map(person => {
+    const mother = people.find(p => p.name === person.motherName);
+    const father = people.find(p => p.name === person.fatherName);
+
+    return { ...person, mother, father };
+  });
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {peoplethDuplications.map((person) => {
+          const { slug } = person;
+
+          return (
+            <PersonLink key={slug} person={person} />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
