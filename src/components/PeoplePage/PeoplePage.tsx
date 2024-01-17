ef31a2cbdbe54518b2c27a24473fabf0781cffbc
aurelia <aurelia.gorzna.dev@gmail.com>
2024-01-17 14:39:27 +0100

    moved states to PeopleTable

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -1,51 +1,13 @@
 /* eslint-disable max-len */
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { Person } from '../../types';
-import { Loader } from '../Loader';
-import { getPeople } from '../../api';
+import React from 'react';
 import { PeopleTable } from '../PeopleTable/PeopleTable';
 
 export const PeoplePage: React.FC = () => {
-  const [people, setPeople] = useState<Person[]>([]);
-  const [loader, setLoader] = useState<boolean>(true);
-  const [hasError, setHasError] = useState<boolean>(false);
-  const { personName } = useParams();
-
-  useEffect(() => {
-    getPeople()
-      .then(setPeople)
-      .catch(() => setHasError(true))
-      .finally(() => setLoader(false));
-  }, []);
-
   return (
     <div className="container">
       <h1 className="title">People Page</h1>
       <div className="block">
-        <div className="box table-container">
-          {loader ? <Loader /> : null}
-          {hasError ? (
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              Something went wrong
-            </p>
-          ) : null}
-
-          {
-            people.length === 0 && !loader
-              ? (
-                <p data-cy="noPeopleMessage">
-                  There are no people on the server
-                </p>
-              ) : null
-          }
-
-          {
-            people.length > 0 ? (
-              <PeopleTable people={people} personName={personName} />
-            ) : null
-          }
-        </div>
+        <PeopleTable />
       </div>
     </div>
   );
--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,47 +1,78 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import classNames from 'classnames';
 import { PersonLink } from '../PersonLink/PersonLink';
 import { Person } from '../../types';
+import { Loader } from '../Loader';
+import { getPeople } from '../../api';
 
 const getPersonString = (name: string, born: number) => {
   return `${name.toLocaleLowerCase().replaceAll(' ', '-')}-${born}`;
 };
 
-interface Props {
-  people: Person[]
-  personName: string | undefined
-}
+export const PeopleTable: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loader, setLoader] = useState<boolean>(true);
+  const [hasError, setHasError] = useState<boolean>(false);
 
-export const PeopleTable: React.FC<Props> = (props) => {
-  const { people, personName } = props;
+  useEffect(() => {
+    getPeople()
+      .then(setPeople)
+      .catch(() => setHasError(true))
+      .finally(() => setLoader(false));
+  }, []);
+
+  const { personName } = useParams();
 
   return (
-    <table
-      data-cy="peopleTable"
-      className="table is-striped is-hoverable is-narrow is-fullwidth"
-    >
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Sex</th>
-          <th>Born</th>
-          <th>Died</th>
-          <th>Mother</th>
-          <th>Father</th>
-        </tr>
-      </thead>
+    <div className="box table-container">
+      {loader ? <Loader /> : null}
+      {hasError ? (
+        <p data-cy="peopleLoadingError" className="has-text-danger">
+          Something went wrong
+        </p>
+      ) : null}
 
-      <tbody>
-        {people.map(person => (
-          <PersonLink
-            person={person}
-            className={classNames({ 'has-background-warning': personName === getPersonString(person.name, person.born) })}
-            motherBirth={people.find(mother => mother.name === person.motherName)?.born || null}
-            fatherBirth={people.find(father => father.name === person.fatherName)?.born || null}
-          />
-        ))}
-      </tbody>
-    </table>
+      {
+        people.length === 0 && !loader
+          ? (
+            <p data-cy="noPeopleMessage">
+              There are no people on the server
+            </p>
+          ) : null
+      }
+
+      {
+        people.length > 0 ? (
+          <table
+            data-cy="peopleTable"
+            className="table is-striped is-hoverable is-narrow is-fullwidth"
+          >
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Sex</th>
+                <th>Born</th>
+                <th>Died</th>
+                <th>Mother</th>
+                <th>Father</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {people.map(person => (
+                <PersonLink
+                  person={person}
+                  className={classNames({ 'has-background-warning': personName === getPersonString(person.name, person.born) })}
+                  motherBirth={people.find(mother => mother.name === person.motherName)?.born || null}
+                  fatherBirth={people.find(father => father.name === person.fatherName)?.born || null}
+                />
+              ))}
+            </tbody>
+          </table>
+        ) : null
+      }
+    </div>
   );
 };
