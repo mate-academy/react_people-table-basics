17d64f85ce5bd1a8d74b3e5bfa0ebbe42f5e162d
MrWhiteres <alexandr.leluk.99@gmail.com>
2024-02-05 23:37:53 +0200

    add task solutions

---
+++ src/utils/matchParents.tsx
@@ -0,0 +1,15 @@
+import { Person } from '../types';
+
+export const matchParents = (people:Person[]) => {
+  return people.map(child => {
+    const motherItem = child.motherName
+      ? people.find(mother => mother.name === child.motherName) || null
+      : null;
+
+    const fatherItem = child.fatherName
+      ? people.find(father => father.name === child.fatherName) || null
+      : null;
+
+    return { ...child, mother: motherItem, father: fatherItem };
+  });
+};
