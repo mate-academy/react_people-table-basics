dbe75b1df8593e17cb332ac4ca0da49ecc24b582
“Nikita <your@email>
2024-02-12 00:26:31 +0200

    add task solution

---
+++ src/components/PersonLink/PersonLink.tsx
@@ -0,0 +1,26 @@
+import classNames from 'classnames';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface Props {
+  sex: string,
+  name: string,
+  slug: string,
+}
+
+export const PersonLink: React.FC<Props> = ({
+  name,
+  sex,
+  slug,
+}) => {
+  return (
+    <Link
+      to={`/people/${slug}`}
+      className={classNames({
+        'has-text-danger': sex === 'f',
+      })}
+    >
+      {name}
+    </Link>
+  );
+};
