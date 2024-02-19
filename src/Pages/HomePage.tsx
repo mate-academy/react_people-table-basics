d153638fa47efcdd795935bfcf8c2cd548202928
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2024-02-19 19:47:16 +0100

    solution

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,10 +1,7 @@
 export const HomePage = () => {
   return (
-    <>
-      <h1 className="title">
-        Home Page
-      </h1>
-    </>
-
+    <h1 className="title">
+      Home Page
+    </h1>
   );
 };
