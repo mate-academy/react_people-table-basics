350bb3ffc8927472ca5a03a676be1be03d5d7fd7
Hovera-Liubomyr <ljusuk@gmail.com>
2024-03-20 22:41:16 +0200

    react_people-table-basics

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,66 @@
+import classNames from 'classnames';
+import { Person } from '../types/Person';
+import { useParams } from 'react-router-dom';
+import { HumanLink } from './HumanLink';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const { slug } = useParams();
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(human => (
+          <tr
+            data-cy="person"
+            key={human.slug}
+            className={classNames({
+              'has-background-warning': slug === human.slug,
+            })}
+          >
+            <td aria-label={`${human.name}`}>
+              <HumanLink person={human} />
+            </td>
+
+            <td>{human.sex}</td>
+            <td>{human.born}</td>
+            <td>{human.died}</td>
+
+            <td className="has-text-danger">
+              {human.mother ? (
+                <HumanLink person={human.mother} />
+              ) : (
+                human.motherName || '-'
+              )}
+            </td>
+
+            <td>
+              {human.father ? (
+                <HumanLink person={human.father} />
+              ) : (
+                human.fatherName || '-'
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
