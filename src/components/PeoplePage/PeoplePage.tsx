3bc41f22cdbdbdcd15252e388703e6cb30ab1d93
Dmytro Kravchenko <dmytro318@gmail.com>
2024-03-28 13:54:44 +0200

    initial commit

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Person } from '../../types';
+import { getPeople } from '../../api';
+import { Loader } from '../Loader';
+import { PeopleTable } from '../PeopleTable';
+
+export const PeoplePage: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {loading && <Loader />}
+
+          {error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!loading && !error && !people.length && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!!people.length && <PeopleTable people={people} />}
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Person } from '../../types';
+import { PersonTable } from '../PersonTable';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <PersonTable key={person.slug} person={person} people={people} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
