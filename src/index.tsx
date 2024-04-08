325a7b613800bf1cff504b2964f27ff7428f6ebd
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-04-08 13:00:22 +0200

    people table done

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,33 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  HashRouter as Router,
+  Routes,
+} from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
+import { Home } from './pages/Home';
+import { People } from './pages/People';
+import { NotFound } from './pages/NotFound';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <Router>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+
+        <Route path="home" element={<Navigate to=".." replace />} />
+
+        <Route path="/people" element={<People />}>
+          <Route path=":slug" element={<People />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   </Router>,
 );
