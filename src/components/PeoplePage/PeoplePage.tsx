137df66abc7986e19b98078214343613a3cf6563
Denis Kotov <dionisiy2904@gmail.com>
2024-04-08 14:06:23 +0300

    TS Table basics

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
+import { getPeople } from '../../api';
+import { Person } from '../../types';
+import { PersonItem } from '../PersonItems/PersonItem';
+
+export const PeoplePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPeople()
+      .then(setPeople)
+      .catch(() => {
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title">People Page</h1>
+
+        <div className="block">
+          <div className="box table-container">
+            {isError ? (
+              <p data-cy="peopleLoadingError" className="has-text-danger">
+                Something went wrong
+              </p>
+            ) : (
+              <>
+                {isLoading ? (
+                  <Loader />
+                ) : (
+                  <>
+                    {!people.length ? (
+                      <p data-cy="noPeopleMessage">
+                        There are no people on the server
+                      </p>
+                    ) : (
+                      <>
+                        <table
+                          data-cy="peopleTable"
+                          className="table is-striped is-hoverable is-narrow is-fullwidth"
+                        >
+                          <thead>
+                            <tr>
+                              <th>Name</th>
+                              <th>Sex</th>
+                              <th>Born</th>
+                              <th>Died</th>
+                              <th>Mother</th>
+                              <th>Father</th>
+                            </tr>
+                          </thead>
+
+                          <tbody>
+                            {people.map(person => (
+                              <PersonItem person={person} people={people} />
+                            ))}
+                          </tbody>
+                        </table>
+                      </>
+                    )}
+                  </>
+                )}
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
---
+++ src/components/PersonItems/PersonItem.tsx
@@ -0,0 +1,68 @@
+import classNames from "classnames";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { Person } from '../../types';
+
+interface Props {
+  person: Person;
+  people: Person[];
+}
+export const PersonItem: React.FC<Props> = ({ person, people }) => {
+  const { personId } = useParams();
+  const { motherName, fatherName } = person;
+
+  const motherNameFounded = people.find(person => person.name === motherName);
+  const fatherNameFounded = people.find(person => person.name === fatherName);
+
+  return (
+    <tr
+      data-cy="person"
+      className={classNames({
+        'has-background-warning': personId === person.slug,
+      })}
+      key={person.slug}
+    >
+      <td>
+        <Link
+          to={`/people/${person.slug}`}
+          className={classNames({
+            'has-text-danger': person.sex === 'f',
+          })}
+        >
+          {person.name}
+        </Link>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+      <td>
+        {person.motherName ? (
+          motherNameFounded ? (
+            <Link
+              className="has-text-danger"
+              to={`/people/${motherNameFounded.slug}`}
+            >
+              {person.motherName}
+            </Link>
+          ) : (
+            <span>{person.motherName}</span>
+          )
+        ) : (
+          '-'
+        )}
+      </td>
+      <td>
+        {person.fatherName ? (
+          fatherNameFounded ? (
+            <Link to={`/people/${fatherNameFounded.slug}`}>{person.fatherName}</Link>
+          ) : (
+            <span>{person.fatherName}</span>
+          )
+        ) : (
+          '-'
+        )}
+      </td>
+    </tr>
+  );
+};
