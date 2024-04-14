880fe35ab5ef672d49836145b835a4cce8ec669c
Lesia <voloshka2912@icloud.com>
2024-04-14 17:37:29 +0300

    add  new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,32 @@
-
-import classNames from 'classnames';
-import { NavLink, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './components/HomePage/HomePage';
+import { PeoplePage } from './components/PeoplePage/PeoplePage';
+import { Layout } from './components/Layout';
+import { Navbar } from './components/NavBar/Navbar';
 import './App.scss';
-import { HomePage } from './components/Loader/HomePage/HomePage';
-import { PeoplePage } from './components/Loader/PeoplePage/PeoplePage';
-
-
-
-const activeLink = ({ isActive }: { isActive: boolean }) =>
-  classNames('navbar-item', { 'has-background-grey-lighter': isActive });
 
 export const App = () => {
   return (
     <div data-cy="app">
-      <nav
-        data-cy="nav"
-        className="navbar is-fixed-top has-shadow"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <NavLink className={activeLink} to="/">
-              Home
-            </NavLink>
-
-            <NavLink
-              className={activeLink}
-              to="/people"
-            >
-              People
-            </NavLink>
-          </div>
-        </div>
-      </nav>
+      <Navbar />
 
       <main className="section">
         <div className="container">
           <Routes>
-          <Route path="/" element={<HomePage />} />
-            <Route path="/home" element={<Navigate to="/" replace />} />
-            <Route path="/people" element={<PeoplePage />}>
-               <Route path=":personSlug" element={<PeoplePage />} />
+            <Route path="/" element={<Layout />}>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/home" element={<Navigate to="/" replace />} />
+              <Route path="/people" element={<PeoplePage />}>
+                <Route path=":selectedSlug" element={<PeoplePage />} />
+              </Route>
+              <Route
+                path="*"
+                element={<h1 className="title">Page not found</h1>}
+              />
             </Route>
-            <Route path="*" element={<h1 className="title">Page not found</h1>} />
           </Routes>
-
         </div>
       </main>
     </div>
   );
-}
+};
