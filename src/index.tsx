7a3021454a7285d0b91876fbba1fb723f7c583c4
Nadiia Vlasenko <vlasenkonadia19@gmail.com>
2024-04-17 17:30:40 +0200

    changed

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client';
-
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { Root } from './Root';
-createRoot(document.getElementById('root') as HTMLDivElement)
-  .render(<Root />);
+
+createRoot(document.getElementById('root') as HTMLDivElement).render(<Root />);
