9b3ef3c5b5802b72b63185387cce4d6711172d14
Nadiia Vlasenko <vlasenkonadia19@gmail.com>
2024-04-19 16:06:48 +0200

    chnged code

--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -8,11 +8,11 @@ type Props = {
 
 export const PersonLink: React.FC<Props> = ({ person }) => {
   return (
-      <Link
-        className={classNames({ 'has-text-danger': person.sex === 'f' })}
-        to={`/people/${person.slug}`}
-      >
-        {person.name}
-      </Link>
+    <Link
+      className={classNames({ 'has-text-danger': person.sex === 'f' })}
+      to={`/people/${person.slug}`}
+    >
+      {person.name}
+    </Link>
   );
 };
