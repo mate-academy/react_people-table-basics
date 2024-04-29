d4e5604bc38c484da7e8c33dc60cb55ad15e47c2
Oleh Marushchak <westernpc22@gmail.com>
2024-04-30 02:44:28 +0300

    add test

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
+
+export const Nav: React.FC = () => {
+  const getClassLink = ({ isActive }: { isActive: boolean }) =>
+    cn('navbar-item', { 'has-background-grey-lighter': isActive });
+
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink className={getClassLink} to="/">
+            Home
+          </NavLink>
+
+          <NavLink className={getClassLink} to="/people">
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
---
+++ src/context/reducer.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useReducer } from 'react';
+import { Person } from '../types';
+import { SwithchError } from '../types/switchError';
+
+type Action =
+  | { type: 'setPeople'; payload: Person[] }
+  | { type: 'setFetch' }
+  | { type: 'disableFetch' }
+  | { type: 'setSwitchError'; message: SwithchError };
+
+interface State {
+  people: Person[];
+  fetch: boolean;
+  message: SwithchError;
+}
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'setPeople':
+      return {
+        ...state,
+        people: action.payload,
+      };
+
+    case 'setFetch':
+      return {
+        ...state,
+        fetch: true,
+      };
+
+    case 'disableFetch':
+      return {
+        ...state,
+        fetch: false,
+      };
+
+    case 'setSwitchError':
+      return {
+        ...state,
+        message: action.message,
+      };
+  }
+};
+
+const initialState: State = {
+  people: [],
+  fetch: false,
+  message: SwithchError.Default,
+};
+
+export const StateContex = React.createContext(initialState);
+export const DispatchContext = React.createContext((_action: Action) => {});
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export const GlobalProvider: React.FC<Props> = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <StateContex.Provider value={state}>
+      <DispatchContext.Provider value={dispatch}>
+        {children}
+      </DispatchContext.Provider>
+    </StateContex.Provider>
+  );
+};
