a14b34e96af0233f3d3cab8901974b83a3d34fdd
Rafał Górski <rafal.gorski@wp.pl>
2024-05-14 00:09:36 +0200

    solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,41 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import { RouterProvider, createHashRouter } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
+import { Home } from './components/Home';
+import { Error } from './components/Error';
+import { People } from './components/People';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/people',
+        element: <People />,
+        children: [
+          {
+            path: ':personSlug',
+            element: <People />,
+          },
+        ],
+      },
+      {
+        path: '*',
+        element: <Error />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <Router>
-    <App />
-  </Router>,
+  <RouterProvider router={router} />,
 );
