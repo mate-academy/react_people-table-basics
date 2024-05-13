a9aa70ac828e5486aaf8bf717a31de1d1b72cd7d
svyatoslav <svyatoslavik9@gmail.com>
2024-05-13 15:41:09 +0200

    update solution

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -10,13 +10,6 @@ export const PeoplePage = () => {
 
       <div className="block">
         <div className="box table-container">
-          {/* <Loader /> */}
-
-          {/* <p data-cy="peopleLoadingError" className="has-text-danger">
-            Something went wrong
-          </p>
-
-          <p data-cy="noPeopleMessage">There are no people on the server</p> */}
           <TablePeople
             people={dataPeople}
             isLoading={isLoading}
