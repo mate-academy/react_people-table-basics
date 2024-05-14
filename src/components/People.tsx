ead196052d4600b3d30bf13296f0f8ffeace43ee
Rafał Górski <158020641+itsXenthis@users.noreply.github.com>
2024-05-14 12:44:29 +0200

    Update People.tsx

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -30,42 +30,48 @@ export const People = () => {
       .finally(() => setIsLoading(false));
   }, []);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (hasError) {
+    return (
+      <p data-cy="peopleLoadingError" className="has-text-danger">
+        Something went wrong
+      </p>
+    );
+  }
+
+  if (!people.length) {
+    return <p data-cy="noPeopleMessage">There are no people on the server</p>;
+  }
+
   return (
     <>
       <h1 className="title">People Page</h1>
       <div className="block">
         <div className="box table-container">
-          {(isLoading && <Loader />) ||
-            (hasError && (
-              <p data-cy="peopleLoadingError" className="has-text-danger">
-                Something went wrong
-              </p>
-            )) ||
-            (!people.length ? (
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
-            ) : (
-              <table
-                data-cy="peopleTable"
-                className="table is-striped is-hoverable is-narrow is-fullwidth"
-              >
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Sex</th>
-                    <th>Born</th>
-                    <th>Died</th>
-                    <th>Mother</th>
-                    <th>Father</th>
-                  </tr>
-                </thead>
+          <table
+            data-cy="peopleTable"
+            className="table is-striped is-hoverable is-narrow is-fullwidth"
+          >
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Sex</th>
+                <th>Born</th>
+                <th>Died</th>
+                <th>Mother</th>
+                <th>Father</th>
+              </tr>
+            </thead>
 
-                <tbody>
-                  {people.map(person => (
-                    <PersonItem key={person.slug} person={person} />
-                  ))}
-                </tbody>
-              </table>
-            ))}
+            <tbody>
+              {people.map(person => (
+                <PersonItem key={person.slug} person={person} />
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </>
