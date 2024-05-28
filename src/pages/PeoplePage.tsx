2e3d083425bb91b8ce09dd0e71050d6fe2a6d06d
Oleksii Dunchyk <oleksiidunchik@gmail.com>
2024-05-28 14:41:24 +0300

    refactor PeoplePage

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -2,117 +2,55 @@ import { useEffect, useState } from 'react';
 import { Person } from '../types';
 import { getPeople } from '../api';
 import { useParams } from 'react-router-dom';
-import { PersonLink } from '../components/PersonLink/PersonLink';
 import { Loader } from '../components/Loader/Loader';
-
-function findPersonByName(name: string | null, people: Person[]) {
-  return people.find(person => person.name === name);
-}
+import PeopleTable from '../components/PeopleTable';
 
 export const PeoplePage = () => {
-  const { personSlug } = useParams();
-  const [people, setPeople] = useState<Person[]>();
+  const { personSlug } = useParams<{ personSlug: string }>();
+  const [people, setPeople] = useState<Person[]>([]);
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(true);
+    const fetchPeople = async () => {
+      setIsLoading(true);
+      try {
+        const peopleData = await getPeople();
 
-    getPeople()
-      .then(setPeople)
-      .catch(() => setErrorMessage('Something went wrong'))
-      .finally(() => setIsLoading(false));
+        setPeople(peopleData);
+      } catch (error) {
+        setErrorMessage('Something went wrong');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchPeople();
   }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (errorMessage) {
+    return (
+      <p data-cy="peopleLoadingError" className="has-text-danger">
+        Something went wrong
+      </p>
+    );
+  }
+
+  if (people.length === 0) {
+    return <p data-cy="noPeopleMessage">There are no people on the server</p>;
+  }
+
   return (
     <>
       <h1 className="title">People Page</h1>
-
       <div className="container">
         <div className="block">
           <div className="box table-container">
-            {isLoading ? (
-              <Loader />
-            ) : (
-              <>
-                {errorMessage !== '' && (
-                  <p data-cy="peopleLoadingError" className="has-text-danger">
-                    {errorMessage}
-                  </p>
-                )}
-
-                {people?.length === 0 ? (
-                  <p data-cy="noPeopleMessage">
-                    There are no people on the server
-                  </p>
-                ) : (
-                  <table
-                    data-cy="peopleTable"
-                    className="table is-striped is-hoverable is-narrow is-fullwidth"
-                  >
-                    <thead>
-                      <tr>
-                        <th>Name</th>
-                        <th>Sex</th>
-                        <th>Born</th>
-                        <th>Died</th>
-                        <th>Mother</th>
-                        <th>Father</th>
-                      </tr>
-                    </thead>
-
-                    <tbody>
-                      {people?.map(person => {
-                        const mother = findPersonByName(
-                          person.motherName,
-                          people,
-                        );
-                        const father = findPersonByName(
-                          person.fatherName,
-                          people,
-                        );
-                        return (
-                          <tr
-                            data-cy="person"
-                            key={person.slug}
-                            className={
-                              person.slug === personSlug
-                                ? 'has-background-warning'
-                                : ''
-                            }
-                          >
-                            <td>
-                              <PersonLink person={person} />
-                            </td>
-
-                            <td>{person.sex}</td>
-                            <td>{person.born}</td>
-                            <td>{person.died}</td>
-                            <td>
-                              {mother ? (
-                                <PersonLink person={mother} />
-                              ) : person.motherName ? (
-                                person.motherName
-                              ) : (
-                                '-'
-                              )}
-                            </td>
-                            <td>
-                              {father ? (
-                                <PersonLink person={father} />
-                              ) : person.fatherName ? (
-                                person.fatherName
-                              ) : (
-                                '-'
-                              )}
-                            </td>
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </table>
-                )}
-              </>
-            )}
+            <PeopleTable people={people} personSlug={personSlug} />
           </div>
         </div>
       </div>
