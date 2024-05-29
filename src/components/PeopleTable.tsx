49d39cc3a8d3863137ffd099b50fadabc4d5dcca
Mariia Lutska <mariialutskayar@gmail.com>
2024-05-29 15:37:34 +0300

    Solution

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,68 @@
+import { FC } from 'react';
+
+import PersonLink from './PersonLink';
+import { Person } from '../types';
+
+interface Props {
+  people: Person[];
+  selectedSlug?: string;
+}
+
+const PeopleTable: FC<Props> = ({ people, selectedSlug }) => {
+  const getParent = (parentName: string | null) => {
+    if (!parentName) {
+      return '-';
+    }
+
+    const parent = people.find(person => person.name === parentName);
+
+    if (parent) {
+      return <PersonLink person={parent} />;
+    }
+
+    return parentName;
+  };
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            key={person.slug}
+            data-cy="person"
+            className={
+              selectedSlug === person.slug ? 'has-background-warning' : ''
+            }
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+
+            <td>{getParent(person.motherName)}</td>
+            <td>{getParent(person.fatherName)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default PeopleTable;
