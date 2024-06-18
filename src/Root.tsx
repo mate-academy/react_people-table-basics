998722b5c9c1e86462c0f1f39c0d4b39dd845047
Maria Shmakova <masha.shmakova@gmail.com>
2024-06-18 10:49:01 +0300

    react people table basics

---
+++ src/Root.tsx
@@ -0,0 +1,20 @@
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { App } from './App';
+import { HomePage } from './components/HomePage';
+import { PeoplePage } from './components/PeoplePage';
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="people">
+          <Route index element={<PeoplePage />} />
+          <Route path=":slug" element={<PeoplePage />} />
+        </Route>
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
