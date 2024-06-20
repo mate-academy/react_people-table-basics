897629b35acb665e5fe3e8b0605e95cb307bc814
Nazar Pastuschak <kns19pna@gmail.com>
2024-06-20 20:13:03 +0300

    add task solution

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,41 @@
+import { Person } from "../../types";
+import { PersonLink } from "../PersonLink/PersonLink";
+
+type Props = {
+  people: Person[];
+}
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  const filteredPeople = people.map(person => ({
+    ...person,
+    mother: people.find(
+      currentPerson => currentPerson.name === person.motherName,
+    ),
+    father: people.find(
+      currentPerson => currentPerson.name === person.fatherName,
+    ),
+  }));
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {filteredPeople.map(person => (
+          <PersonLink person={person} key={person.slug} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
