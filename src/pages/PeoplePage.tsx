eb628522d042db636374ff98f4658b82c9c2a2ef
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2024-06-26 10:02:29 +0100

    iprovements

--- src/pages/PeoplePage.tsx
+++ src/pages/PeoplePage.tsx
@@ -42,11 +42,11 @@ export const PeoplePage = () => {
             </p>
           )}
 
-          {people.length < 1 && !loading && (
+          {people.length < 1 && !loading && !hasError && (
             <p data-cy="noPeopleMessage">There are no people on the server</p>
           )}
 
-          {!loading && <PeopleTable people={people} />}
+          {!loading && !hasError && people.length > 0 && <PeopleTable people={people} />}
         </div>
       </div>
     </>
