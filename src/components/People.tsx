bacf21a0abd2ab2bcbbad5c1d35751dfba2fdf31
Kacper Grosicki <kacpergrosicki2002@gmail.com>
2024-06-26 08:31:49 +0200

    fixed

--- src/components/People.tsx
+++ src/components/People.tsx
@@ -36,37 +36,37 @@ export const People = () => {
       <h1 className="title">People Page</h1>
       <div className="block">
         <div className="box table-container">
-          {(isLoading && <Loader />) ||
-            (hasError && (
-              <p data-cy="peopleLoadingError" className="has-text-danger">
-                Something went wrong
-              </p>
-            )) ||
-            (!people.length ? (
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
-            ) : (
-              <table
-                data-cy="peopleTable"
-                className="table is-striped is-hoverable is-narrow is-fullwidth"
-              >
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>Sex</th>
-                    <th>Born</th>
-                    <th>Died</th>
-                    <th>Mother</th>
-                    <th>Father</th>
-                  </tr>
-                </thead>
+          {isLoading ? (
+            <Loader />
+          ) : hasError ? (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          ) : !people.length ? (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          ) : (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
 
-                <tbody>
-                  {people.map(person => (
-                    <PersonItem key={person.slug} person={person} />
-                  ))}
-                </tbody>
-              </table>
-            ))}
+              <tbody>
+                {people.map(person => (
+                  <PersonItem key={person.slug} person={person} />
+                ))}
+              </tbody>
+            </table>
+          )}
         </div>
       </div>
     </>
