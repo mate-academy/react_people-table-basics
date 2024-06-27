2735e5860b7983edb4e160c3b1391bb55a096aa8
Danylo-Chumakov <danil26032003@gmail.com>
2024-06-27 14:13:09 +0300

    add task solution

---
+++ src/components/Loader/Navbar.tsx
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+
+const Navbar: React.FC = () => (
+  <div className="container">
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <NavLink
+          className={({ isActive }) =>
+            classNames('navbar-item', {
+              'has-background-grey-lighter': isActive,
+            })
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            classNames('navbar-item', {
+              'has-background-grey-lighter': isActive,
+            })
+          }
+          to="/people"
+        >
+          People
+        </NavLink>
+      </div>
+    </nav>
+  </div>
+);
+
+export default Navbar;
