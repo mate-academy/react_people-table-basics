e3dc472d7abe2f436c3d7b89bc10509486081d5c
Maksym Prokopovych <prokopovych2006@gmail.com>
2024-07-08 21:47:14 +0300

    develop

---
+++ src/components/Loader/PeopleItem.tsx
@@ -0,0 +1,42 @@
+import { NavLink, useParams } from "react-router-dom";
+import { Person } from "../../types";
+import classNames from "classnames";
+
+type PersonPageProps = {
+  person: Person;
+};
+
+
+export const PeopleItem: React.FC<PersonPageProps> = ({ person }) => {
+
+  const { personId } = useParams();
+  const selectedPersonId = personId ? personId : '';
+
+  console.log(selectedPersonId);
+
+  return (
+    <tr data-cy="person" className={classNames({ "has-background-warning": selectedPersonId === person.slug })}>
+      {/* className="has-background-warning" */}
+      <td>
+        <NavLink to={`/people/${person.slug}`} className={classNames({ "has-text-danger": person.sex === 'f' })}>
+          {person.name}
+        </NavLink>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+      {person.mother ? (
+        <td><NavLink to={`/people/${person.mother.slug}`} className='has-text-danger'>{person.motherName}</NavLink></td>
+      ) : (
+        <td>{person.motherName || '-'}</td>
+      )}
+
+      {person.father ? (
+        <td><NavLink to={`/people/${person.father.slug}`}>{person.fatherName}</NavLink></td>
+      ) : (
+        <td>{person.fatherName || '-'}</td>
+      )}
+    </tr>
+  );
+};
---
+++ src/components/Loader/PeoplePage.tsx
@@ -0,0 +1,17 @@
+import { Person } from "../../types";
+import { PeopleList } from "./PeopleList";
+
+type PeoplePageProps = {
+  people: Person[];
+  error: boolean;
+  loadingPeople: boolean;
+};
+
+export const PeoplePage: React.FC<PeoplePageProps> = ({ loadingPeople, error, people }) => {
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+      <PeopleList loadingPeople={loadingPeople} error={error} people={people} />
+    </>
+  );
+};
