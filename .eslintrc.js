a89b4f269bd84e13025e5112220b180c07438a32
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-07-12 12:27:48 +0300

    add solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,26 @@
 module.exports = {
-  extends: [
-    '@mate-academy/eslint-config-react-typescript',
-    'plugin:cypress/recommended',
-  ],
-  rules: {},
+  extends: ['@mate-academy/eslint-config-react-typescript'],
+  rules: {
+    'max-len': [
+      'error',
+      {
+        ignoreTemplateLiterals: true,
+        ignoreComments: true,
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        assert: 'either',
+      },
+    ],
+  },
 };
+
+// module.exports = {
+//   extends: [
+//     '@mate-academy/eslint-config-react-typescript',
+//     'plugin:cypress/recommended',
+//   ],
+//   rules: {},
+// };
