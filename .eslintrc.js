e93b23765a0a9ded720c5e0a65fc4ff41f577d16
Nazar Oshchepkov <nazzarin565@gmail.com>
2024-07-12 14:41:11 +0300

    add changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,7 @@
 module.exports = {
-  extends: ['@mate-academy/eslint-config-react-typescript'],
-  rules: {
-    'max-len': [
-      'error',
-      {
-        ignoreTemplateLiterals: true,
-        ignoreComments: true,
-      },
-    ],
-    'jsx-a11y/label-has-associated-control': [
-      'error',
-      {
-        assert: 'either',
-      },
-    ],
-  },
+  extends: [
+    '@mate-academy/eslint-config-react-typescript',
+    'plugin:cypress/recommended',
+  ],
+  rules: {},
 };
-
-// module.exports = {
-//   extends: [
-//     '@mate-academy/eslint-config-react-typescript',
-//     'plugin:cypress/recommended',
-//   ],
-//   rules: {},
-// };
