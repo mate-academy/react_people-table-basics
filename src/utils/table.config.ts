e0493c2bcb0371dc03a50a6ec9ea3a1a20ccafae
Arthur Tolkachov <babanjerelo@gmail.com>
2024-07-12 14:27:37 +0300

    [Feature] added people table

---
+++ src/utils/table.config.ts
@@ -0,0 +1,26 @@
+export const TABLE_HEAD = [
+  {
+    key: 'name',
+    title: 'Name',
+  },
+  {
+    key: 'sex',
+    title: 'Sex',
+  },
+  {
+    key: 'born',
+    title: 'Born',
+  },
+  {
+    key: 'died',
+    title: 'Died',
+  },
+  {
+    key: 'mother',
+    title: 'Mother',
+  },
+  {
+    key: 'father',
+    title: 'Father',
+  },
+];
