51b09a02caf0f17b1f192e71eba615d8e9524361
Makhmud <bmm0065@gmail.com>
2024-07-17 19:58:07 +0300

    add task solution

--- src/components/PeopleList/PeopleList.tsx
+++ src/components/PeopleList/PeopleList.tsx
@@ -22,7 +22,7 @@ export const PeopleList: React.FC<PeoplePageProps> = ({
           </p>
         )}
 
-        {!loadingPeople && people.length === 0 &&  (
+        {!loadingPeople && people.length === 0 && (
           <p data-cy="noPeopleMessage">There are no people on the server</p>
         )}
 
