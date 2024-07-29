92d71732fc20c71958271e30521beebd08ce2bee
Andrii Dudko <Annbpiu@gmail.com>
2024-07-29 15:24:58 +0300

    fix code

--- src/components/HomePage/personLink/personLink.tsx
+++ src/components/HomePage/personLink/personLink.tsx
@@ -14,14 +14,27 @@ export const PersonLink: React.FC<PersonLinkProps> = ({
   selectedSlug,
   people,
 }) => {
-  const findPersonSlugByName = (name: string | undefined) => {
-    const foundPerson = people.find(p => p.name === name);
+  const { name, sex, born, died, fatherName, motherName, slug } = person;
+
+  const findPersonSlugByName = (PeopleName: string | undefined) => {
+    const foundPerson = people.find(
+      currentPerson => currentPerson.name === PeopleName,
+    );
 
     return foundPerson ? foundPerson.slug : '';
   };
 
   const hasMother = findPersonSlugByName(person.motherName ?? undefined);
   const hasFather = findPersonSlugByName(person.fatherName ?? undefined);
+
+  const isFather = classNames({
+    'has-text-danger': people.find(p => p.name === fatherName)?.sex === 'f',
+    'is-disabled': !hasFather,
+  });
+  const isMother = classNames({
+    'has-text-danger': people.find(p => p.name === motherName)?.sex === 'f',
+    'is-disabled': !hasMother,
+  });
 
   const isSelected = person.slug === selectedSlug;
 
@@ -31,59 +44,51 @@ export const PersonLink: React.FC<PersonLinkProps> = ({
       className={classNames({ 'has-background-warning': isSelected })}
     >
       <td>
-        {person.name ? (
+        {name ? (
           <Link
-            to={`/people/${person.slug}`}
+            to={`/people/${slug}`}
             className={classNames({
-              'has-text-danger': person.sex === 'f',
+              'has-text-danger': sex === 'f',
             })}
           >
-            {person.name}
+            {name}
           </Link>
         ) : (
-          person.name
+          name
         )}
       </td>
-      <td>{person.sex}</td>
-      <td>{person.born}</td>
-      <td>{person.died}</td>
+      <td>{sex}</td>
+      <td>{born}</td>
+      <td>{died}</td>
       <td>
-        {person.motherName ? (
+        {motherName ? (
           hasMother ? (
             <Link
               to={`/people/${hasMother}`}
-              className={classNames({
-                'has-text-danger':
-                  people.find(p => p.name === person.motherName)?.sex === 'f',
-                'is-disabled': !hasMother,
-              })}
+              className={isMother}
               aria-disabled={!hasMother}
             >
-              {person.motherName}
+              {motherName}
             </Link>
           ) : (
-            <span>{person.motherName}</span>
+            <span>{motherName}</span>
           )
         ) : (
           '-'
         )}
       </td>
       <td>
-        {person.fatherName ? (
+        {fatherName ? (
           hasFather ? (
             <Link
               to={`/people/${hasFather}`}
-              className={classNames({
-                'has-text-danger':
-                  people.find(p => p.name === person.fatherName)?.sex === 'f',
-                'is-disabled': !hasFather,
-              })}
+              className={isFather}
               aria-disabled={!hasFather}
             >
-              {person.fatherName}
+              {fatherName}
             </Link>
           ) : (
-            <span>{person.fatherName}</span>
+            <span>{fatherName}</span>
           )
         ) : (
           '-'
