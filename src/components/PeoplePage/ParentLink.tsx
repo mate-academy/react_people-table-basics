95d770869798b4b07de2a6425a22c65661a64507
MelnykVlad <21vladmel15@gmail.com>
2024-08-04 14:35:06 +0300

    add people table

---
+++ src/components/PeoplePage/ParentLink.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Person } from '../../types';
+import { PersonLink } from './PersonLink';
+
+interface Props {
+  parentName: string | null;
+  people: Person[];
+}
+
+export const ParentLink: React.FC<Props> = ({ parentName, people }) => {
+  const parent = people.find(person => person.name === parentName);
+
+  return (
+    <td>
+      {parentName === null ? (
+        '-'
+      ) : parent ? (
+        <PersonLink person={parent} />
+      ) : (
+        parentName
+      )}
+    </td>
+  );
+};
---
+++ src/components/PeoplePage/PersonLink.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Person } from '../../types';
+import classNames from 'classnames';
+
+interface Props {
+  person: Person;
+}
+
+export const PersonLink: React.FC<Props> = ({ person }) => {
+  return (
+    <Link
+      to={`/people/${person.slug}`}
+      className={classNames({ 'has-text-danger': person.sex === 'f' })}
+    >
+      {person.name}
+    </Link>
+  );
+};
