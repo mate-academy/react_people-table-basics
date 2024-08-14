914b5e52d1b367dd5b6f3bd4eee5b9add585a94d
GTXtab <malahovskinazar007@gmail.com>
2024-08-14 15:38:06 +0300

    solution

---
+++ src/components/PersonInfo.tsx
@@ -0,0 +1,71 @@
+import { Link, useParams } from 'react-router-dom';
+import { Person } from '../types';
+import classNames from 'classnames';
+
+type Props = {
+  person: Person;
+  findPersonByName: (name: string) => Person | undefined;
+};
+
+const NO_INFO = '-';
+const FEMALE = 'f';
+
+const getLinkClass = (personForClass: Person | undefined) => {
+  if (personForClass) {
+    return classNames({
+      'has-text-danger': personForClass.sex === FEMALE,
+    });
+  }
+
+  return;
+};
+
+export const PersonInfo: React.FC<Props> = ({ person, findPersonByName }) => {
+  const { personSlug } = useParams<{ personSlug: string }>();
+
+  const getSelectedPersonClass = () => {
+    return classNames({
+      'has-background-warning': person.slug === personSlug,
+    })
+  }
+
+  return (
+    <tr data-cy="person" key={person.slug} className={getSelectedPersonClass()}>
+      <td>
+        <Link className={getLinkClass(person)} to={`${person.slug}`}>
+          {person.name}
+        </Link>
+      </td>
+
+      <td>{person.sex}</td>
+      <td>{person.born}</td>
+      <td>{person.died}</td>
+
+      <td>
+        {person.motherName && findPersonByName(person.motherName) ? (
+          <Link
+            to={`${findPersonByName(person.motherName)?.slug}`}
+            className={getLinkClass(findPersonByName(person.motherName))}
+          >
+            {person.motherName}
+          </Link>
+        ) : (
+          <span>{person.motherName || NO_INFO}</span>
+        )}
+      </td>
+
+      <td>
+        {person.fatherName && findPersonByName(person.fatherName) ? (
+          <Link
+            to={`${findPersonByName(person.fatherName)?.slug}`}
+            className={getLinkClass(findPersonByName(person.fatherName))}
+          >
+            {person.fatherName}
+          </Link>
+        ) : (
+          <span>{person.fatherName || NO_INFO}</span>
+        )}
+      </td>
+    </tr>
+  );
+};
---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import { Loader } from "../components/Loader"
+import { Person } from "../types"
+import { getPeople } from "../api";
+import { PeopleTable } from "../components/PeopleTable";
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getPeople()
+      .then(setPeople)
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+      <div className="block">
+        <h1 className="title">People Page</h1>
+
+        <div className="box table-container">
+          {isLoading && <Loader />}
+
+          {!isLoading && !people.length && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!isLoading && <PeopleTable people={people} />}
+
+          {isError && (
+            <p data-cy="peopleLoadingError" className="has-text=danger">
+              Something went wrong
+            </p>
+          )}
+        </div>
+      </div>
+  )
+}
