7df0c03f20e2d9ee13922d5c1c0ec23135676103
Rostuk Dudchak <rostikdudchak15@gmail.com>
2024-08-26 12:26:23 +0300

    finish

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -28,111 +28,118 @@ export const PeoplePage = () => {
 
   const namesSet = new Set(people.map(person => person.name));
 
-  if (loading) {
-    return <Loader />;
-  }
-
   return (
     <>
-      <h1 className="title">People Page</h1>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <h1 className="title">People Page</h1>
 
-      <div className="block">
-        <div className="box table-container">
-          {error && (
-            <p data-cy="peopleLoadingError" className="has-text-danger">
-              {error}
-            </p>
-          )}
+          <div className="block">
+            <div className="box table-container">
+              {error && (
+                <p data-cy="peopleLoadingError" className="has-text-danger">
+                  {error}
+                </p>
+              )}
 
-          {!error && people.length === 0 && (
-            <p data-cy="noPeopleMessage">There are no people on the server</p>
-          )}
+              {!error && people.length === 0 && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-          {people.length > 0 && (
-            <table
-              data-cy="peopleTable"
-              className="table is-striped is-hoverable is-narrow is-fullwidth"
-            >
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Sex</th>
-                  <th>Born</th>
-                  <th>Died</th>
-                  <th>Mother</th>
-                  <th>Father</th>
-                </tr>
-              </thead>
-              <tbody>
-                {people.map(person => (
-                  <tr
-                    data-cy="person"
-                    key={person.slug}
-                    className={
-                      person.slug === slug ? 'has-background-warning' : ''
-                    }
-                  >
-                    <td>
-                      <Link
-                        to={`/people/${person.slug}`}
-                        className={person.sex === 'f' ? 'has-text-danger' : ''}
+              {people.length > 0 && (
+                <table
+                  data-cy="peopleTable"
+                  className="table is-striped
+                   is-hoverable is-narrow is-fullwidth"
+                >
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Sex</th>
+                      <th>Born</th>
+                      <th>Died</th>
+                      <th>Mother</th>
+                      <th>Father</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {people.map(person => (
+                      <tr
+                        data-cy="person"
+                        key={person.slug}
+                        className={
+                          person.slug === slug ? 'has-background-warning' : ''
+                        }
                       >
-                        {person.name}
-                      </Link>
-                    </td>
-                    <td>{person.sex}</td>
-                    <td>{person.born}</td>
-                    <td>{person.died}</td>
-                    <td
-                      className={
-                        namesSet.has(person.motherName || '')
-                          ? 'has-text-danger'
-                          : ''
-                      }
-                    >
-                      {person.motherName ? (
-                        namesSet.has(person.motherName) ? (
+                        <td>
                           <Link
-                            to={`/people/${people.find(p => p.name === person.motherName)?.slug}`}
-                            className="has-text-danger"
+                            to={`/people/${person.slug}`}
+                            className={
+                              person.sex === 'f' ? 'has-text-danger' : ''
+                            }
                           >
-                            {person.motherName}
+                            {person.name}
                           </Link>
-                        ) : (
-                          person.motherName
-                        )
-                      ) : (
-                        '-'
-                      )}
-                    </td>
-                    <td
-                      className={
-                        namesSet.has(person.fatherName || '')
-                          ? 'has-text-danger'
-                          : ''
-                      }
-                    >
-                      {person.fatherName ? (
-                        namesSet.has(person.fatherName) ? (
-                          <Link
-                            to={`/people/${people.find(p => p.name === person.fatherName)?.slug}`}
-                          >
-                            {person.fatherName}
-                          </Link>
-                        ) : (
-                          person.fatherName
-                        )
-                      ) : (
-                        '-'
-                      )}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
-      </div>
+                        </td>
+                        <td>{person.sex}</td>
+                        <td>{person.born}</td>
+                        <td>{person.died}</td>
+                        <td
+                          className={
+                            namesSet.has(person.motherName || '')
+                              ? 'has-text-danger'
+                              : ''
+                          }
+                        >
+                          {person.motherName ? (
+                            namesSet.has(person.motherName) ? (
+                              <Link
+                                to={`/people/${people.find(p => p.name === person.motherName)?.slug}`}
+                                className="has-text-danger"
+                              >
+                                {person.motherName}
+                              </Link>
+                            ) : (
+                              person.motherName
+                            )
+                          ) : (
+                            '-'
+                          )}
+                        </td>
+                        <td
+                          className={
+                            namesSet.has(person.fatherName || '')
+                              ? 'has-text-danger'
+                              : ''
+                          }
+                        >
+                          {person.fatherName ? (
+                            namesSet.has(person.fatherName) ? (
+                              <Link
+                                to={`/people/${people.find(p => p.name === person.fatherName)?.slug}`}
+                              >
+                                {person.fatherName}
+                              </Link>
+                            ) : (
+                              person.fatherName
+                            )
+                          ) : (
+                            '-'
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              )}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
