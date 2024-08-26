34b1ffb475154a259d0ebfc684b4cb235818f519
Rostuk Dudchak <rostikdudchak15@gmail.com>
2024-08-26 14:23:19 +0300

    fix code

--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react';
 import { Loader } from '../Loader';
 import { Person } from '../../types';
 import { getPeople } from '../../api';
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
 
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
@@ -11,22 +12,15 @@ export const PeoplePage = () => {
   const { slug } = useParams<{ slug?: string }>();
 
   useEffect(() => {
-    if (people.length === 0) {
-      setLoading(true);
-      getPeople()
-        .then(data => {
-          setPeople(data);
-        })
-        .catch(() => {
-          setError('Something went wrong');
-        })
-        .finally(() => {
-          setLoading(false);
-        });
-    }
-  }, [people.length]);
+    setLoading(true);
+    getPeople()
+      .then(setPeople)
+      .catch(() => setError('Something went wrong'))
+      .finally(() => setLoading(false));
+  }, []);
 
-  const namesSet = new Set(people.map(person => person.name));
+  const findSlugByName = (name: string | undefined) =>
+    people.find(person => person.name === name)?.slug || '';
 
   return (
     <>
@@ -35,7 +29,6 @@ export const PeoplePage = () => {
       ) : (
         <>
           <h1 className="title">People Page</h1>
-
           <div className="block">
             <div className="box table-container">
               {error && (
@@ -68,70 +61,13 @@ export const PeoplePage = () => {
                   </thead>
                   <tbody>
                     {people.map(person => (
-                      <tr
-                        data-cy="person"
+                      <PeopleTable
                         key={person.slug}
-                        className={
-                          person.slug === slug ? 'has-background-warning' : ''
-                        }
-                      >
-                        <td>
-                          <Link
-                            to={`/people/${person.slug}`}
-                            className={
-                              person.sex === 'f' ? 'has-text-danger' : ''
-                            }
-                          >
-                            {person.name}
-                          </Link>
-                        </td>
-                        <td>{person.sex}</td>
-                        <td>{person.born}</td>
-                        <td>{person.died}</td>
-                        <td
-                          className={
-                            namesSet.has(person.motherName || '')
-                              ? 'has-text-danger'
-                              : ''
-                          }
-                        >
-                          {person.motherName ? (
-                            namesSet.has(person.motherName) ? (
-                              <Link
-                                to={`/people/${people.find(p => p.name === person.motherName)?.slug}`}
-                                className="has-text-danger"
-                              >
-                                {person.motherName}
-                              </Link>
-                            ) : (
-                              person.motherName
-                            )
-                          ) : (
-                            '-'
-                          )}
-                        </td>
-                        <td
-                          className={
-                            namesSet.has(person.fatherName || '')
-                              ? 'has-text-danger'
-                              : ''
-                          }
-                        >
-                          {person.fatherName ? (
-                            namesSet.has(person.fatherName) ? (
-                              <Link
-                                to={`/people/${people.find(p => p.name === person.fatherName)?.slug}`}
-                              >
-                                {person.fatherName}
-                              </Link>
-                            ) : (
-                              person.fatherName
-                            )
-                          ) : (
-                            '-'
-                          )}
-                        </td>
-                      </tr>
+                        person={person}
+                        slug={slug}
+                        motherSlug={findSlugByName(person.motherName || '')}
+                        fatherSlug={findSlugByName(person.fatherName || '')}
+                      />
                     ))}
                   </tbody>
                 </table>
