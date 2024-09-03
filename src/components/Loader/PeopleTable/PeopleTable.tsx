7ad6f6862ae63d051374d0d8aa8b92ab250bb114
piyopiyo0 <tetyana0204@gmail.com>
2024-09-03 22:35:02 +0300

    improvement

--- src/components/Loader/PeopleTable/PeopleTable.tsx
+++ src/components/Loader/PeopleTable/PeopleTable.tsx
@@ -52,7 +52,7 @@ export const PeopleTable = () => {
             </p>
           )}
 
-          {people.length === 0 && !isLoading && (
+          {people.length && !isLoading && (
             <p data-cy="noPeopleMessage">There are no people on the server</p>
           )}
 
