b3840106cb9e220643bbfeb1a9e90114c078c838
Oksana Palahecha <oksanapalahecha@gmail.com>
2024-09-03 16:04:29 +0300

    improved readability

--- src/pages/peoplePage.tsx
+++ src/pages/peoplePage.tsx
@@ -7,17 +7,17 @@ import { getPeople } from '../api';
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
   const [loading, setLoading] = useState(false);
-  const [isLoadingSuccessful, setIsLoadingSuccessful] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     getPeople()
       .then(peopleFromServer => {
         setPeople(peopleFromServer);
-        setIsLoadingSuccessful(true);
+        setHasError(false);
       })
       .catch(() => {
-        setIsLoadingSuccessful(false);
+        setHasError(true);
       })
       .finally(() => {
         setLoading(false);
@@ -34,22 +34,40 @@ export const PeoplePage = () => {
     <p data-cy="noPeopleMessage">There are no people on the server</p>
   );
 
+  if (loading) {
+    return (
+      <>
+        <h1 className="title">People Page</h1>
+        <div className="block">
+          <div className="box table-container">
+            <Loader />
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <>
+        <h1 className="title">People Page</h1>
+        <div className="block">
+          <div className="box table-container">{loadingErrorMessage}</div>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="box table-container">
-          {loading ? (
-            <Loader />
-          ) : isLoadingSuccessful ? (
-            people.length > 0 ? (
-              <PeopleTable people={people} />
-            ) : (
-              emptyPeopleMessage
-            )
+          {people.length > 0 ? (
+            <PeopleTable people={people} />
           ) : (
-            loadingErrorMessage
+            emptyPeopleMessage
           )}
         </div>
       </div>
