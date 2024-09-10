662b954c29236ccc2e059b5704e93225aea5e0e4
valentyn19939 <valik.hai1993@gmail.com>
2024-09-10 17:27:01 +0200

    add task solution

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+
+import { getPeople } from '../api';
+import { Loader } from './Loader';
+import { PeopleTable } from './PeopleTable';
+
+export const PeoplePage = () => {
+  const [loaded, setLoaded] = useState(false);
+  const [hasError, setError] = useState(false);
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    setLoaded(false);
+
+    getPeople()
+      .then(peopleFromServer => {
+        const preparedPeople = peopleFromServer.map(p => ({ ...p }));
+
+        preparedPeople.forEach(person => {
+          Object.assign(person, {
+            mother: preparedPeople.find(m => m.name === person.motherName),
+            father: preparedPeople.find(f => f.name === person.fatherName),
+          });
+        });
+
+        setPeople(preparedPeople);
+      })
+      .catch(() => setError(true))
+      .finally(() => setLoaded(true));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <input type="text" onChange={e => e} />
+      <div className="block">
+        <div className="box table-container">
+          {!loaded && <Loader />}
+
+          {loaded && hasError && (
+            <p data-cy="peopleLoadingError">Something went wrong</p>
+          )}
+
+          {loaded && !hasError && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {loaded && !hasError && people.length > 0 && (
+            <PeopleTable people={people} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
