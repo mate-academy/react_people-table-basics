f229e531db3ed243e1578523e1cdac795e2e84d6
valentyn19939 <valik.hai1993@gmail.com>
2024-09-12 17:41:04 +0200

    add task solution

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -5,12 +5,12 @@ import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
 
 export const PeoplePage = () => {
-  const [loaded, setLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [hasError, setError] = useState(false);
   const [people, setPeople] = useState([]);
 
   useEffect(() => {
-    setLoaded(false);
+    setIsLoading(false);
 
     getPeople()
       .then(peopleFromServer => {
@@ -26,7 +26,7 @@ export const PeoplePage = () => {
         setPeople(preparedPeople);
       })
       .catch(() => setError(true))
-      .finally(() => setLoaded(true));
+      .finally(() => setIsLoading(true));
   }, []);
 
   return (
@@ -34,17 +34,17 @@ export const PeoplePage = () => {
       <h1 className="title">People Page</h1>
       <div className="block">
         <div className="box table-container">
-          {!loaded && <Loader />}
+          {!isLoading && <Loader />}
 
-          {loaded && hasError && (
+          {isLoading && hasError && (
             <p data-cy="peopleLoadingError">Something went wrong</p>
           )}
 
-          {loaded && !hasError && people.length === 0 && (
+           {isLoading && !hasError && !people.length && (
             <p data-cy="noPeopleMessage">There are no people on the server</p>
           )}
 
-          {loaded && !hasError && people.length > 0 && (
+           {isLoading && !hasError && !!people.length && (
             <PeopleTable people={people} />
           )}
         </div>
--- src/components/PersonLink.tsx
+++ src/components/PersonLink.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames';
 import { FC } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Person } from '../types/Person';
+import { Gender } from '../types/gender';
 
 type Props = {
   person: Person;
@@ -10,17 +11,19 @@ type Props = {
 export const PersonLink: FC<Props> = ({ person }) => {
   const { search } = useLocation();
 
+  const { slug, name, sex } = person;
+
   return (
     <Link
       to={{
-        pathname: `/people/${person.slug}`,
+        pathname: `/people/${slug}`,
         search,
       }}
       className={classNames({
-        'has-text-danger': person.sex === 'f',
+        'has-text-danger': sex === Gender.Female,
       })}
     >
-      {person.name}
+      {name} {}
     </Link>
   );
 };
