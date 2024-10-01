8b75ccba770378edc2ceccff898bc6e3783a5959
Aliona <ashelyaa17@gmail.com>
2024-10-01 18:30:59 +0300

    task

---
+++ src/Root.tsx
@@ -0,0 +1,17 @@
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { PeoplePage } from './pages/PeoplePage';
+import { App } from './App';
+
+export const Root = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="/home" element={<Navigate to="/" replace />} />
+        <Route path="/people" element={<PeoplePage />} />
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
+  </HashRouter>
+);
