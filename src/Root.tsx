3f2f78def1a25a9bf7aee77282dfad2df248ed4c
Maksym Sabov <maksim50.97@gmail.com>
2025-01-05 22:11:15 +0200

    solution

---
+++ src/Root.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
+import { useEffect } from 'react';
+import { App } from './App';
+import { HomePage } from './components/HomePage/HomePage';
+import { PeoplePage } from './components/PeoplePage/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+
+export const Root = () => {
+  useEffect(() => {
+    document.documentElement.classList.add('has-navbar-fixed-top');
+  }, []);
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="home" element={<Navigate to=".." replace={true} />} />
+          <Route path="people">
+            <Route index element={<PeoplePage />} />
+            <Route path=":slug" element={<PeoplePage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
