f1dc78ee48bfff9459fd794d17a41a09f2153999
Anna-Daryna Sobko <dar.sobko@gmail.com>
2025-01-27 14:20:04 +0200

    Solution

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
+import { getPeople } from '../../api';
+import { Person } from '../../types/Person';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
+
+const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    getPeople()
+      .then(data => {
+        setPeople(data);
+        setError(null);
+      })
+      .catch(() => setError('Failed to load people data'))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div className="block">
+      <h1 className="title">People Page</h1>
+
+      <div className="box table-container">
+        {isLoading && <Loader />}
+
+        {error && (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            {error}
+          </p>
+        )}
+
+        {!isLoading && !error && people.length === 0 && (
+          <p data-cy="noPeopleMessage">There are no people on the server</p>
+        )}
+
+        {!isLoading && !error && people.length > 0 && (
+          <PeopleTable people={people} />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PeoplePage;
