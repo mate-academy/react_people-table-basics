b8bb0668d0b99fee76d9d645421549a64f3b08f6
serbin.nick@gmail.com <serbin.nick@gmail.com>
2025-03-29 02:55:18 +0100

    sollution

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+import { Outlet } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+export const Navbar = () => {
+  const linkClicked = ({ isActive }: { isActive: boolean }) => {
+    return classNames('navbar-item', {
+      'has-background-grey-lighter': isActive,
+    });
+  };
+
+  return (
+    <>
+      <nav
+        data-cy="nav"
+        className="navbar is-fixed-top has-shadow "
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <NavLink className={linkClicked} to="/">
+              Home
+            </NavLink>
+
+            <NavLink className={linkClicked} to="people">
+              People
+            </NavLink>
+          </div>
+        </div>
+      </nav>
+
+      <main className="section">
+        <Outlet />
+      </main>
+    </>
+  );
+};
