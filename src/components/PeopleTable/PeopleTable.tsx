2c4b304067b972b7c440798d90033417d3a2caa6
kami2693 <kamikaze2693@gmail.com>
2025-04-09 15:47:54 +0200

    fix

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,67 @@
+import React, { useMemo } from 'react';
+import { Person } from '../../types/Person';
+import { Loader } from '../Loader/Loader';
+import { tableColumns } from './tableColumns';
+import { TableRow } from './TableRow/TableRow';
+
+interface Props {
+  people: Person[];
+  selectedSlug: string | null;
+  isLoading: boolean;
+  error: string | null;
+}
+
+export const PeopleTable: React.FC<Props> = ({
+  people,
+  selectedSlug,
+  isLoading,
+  error,
+}) => {
+  const memoizedPeople = useMemo(() => people, [people]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return (
+      <p data-cy="peopleLoadingError" className="has-text-danger">
+        {error}
+      </p>
+    );
+  }
+
+  if (!people.length) {
+    return <p data-cy="noPeopleMessage">There are no people on the server</p>;
+  }
+
+  return (
+    <div className="box table-container">
+      <table
+        data-cy="peopleTable"
+        className="table is-striped is-hoverable is-narrow is-fullwidth"
+      >
+        <thead>
+          <tr>
+            {tableColumns.map(({ title, key }) => (
+              <th key={key}>{title}</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          {people.map(person => (
+            <TableRow
+              key={person.slug}
+              person={person}
+              people={memoizedPeople}
+              isSelected={selectedSlug === person.slug}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+PeopleTable.displayName = 'PeopleTable';
