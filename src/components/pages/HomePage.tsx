1b29bc2971a52c1c42559b829466d8285a9192e2
Csillag61 <csillagok@gmail.com>
2025-04-12 17:46:25 -0400

    react_people-table

---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="title">Welcome to the Home Page</h1>
+      <p>This is the main content of the home page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
