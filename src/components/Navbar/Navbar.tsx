560830b306c279657e4d913a91b15ac93f2ef9b7
AVyhaa <artemvyhovanec1@gmail.com>
2025-05-19 14:44:06 +0300

    solution

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
+
+export const Navbar = () => {
+  const linkClass = ({ isActive }: { isActive: boolean }) => {
+    return cn('navbar-item', { 'has-background-grey-lighter': isActive });
+  };
+
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink className={linkClass} to="/">
+            Home
+          </NavLink>
+
+          <NavLink className={linkClass} to="/people">
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
