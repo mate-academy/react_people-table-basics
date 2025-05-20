7db4891cd5c44e3fa0bf016e49316ed4540cd273
Andriana Chepil <andriana1112.chepil@gmail.com>
2025-05-20 13:12:16 +0300

    add task solution

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,73 @@
+import { Person } from '../types/Person';
+import { PersonLink } from './PersonLink';
+
+interface Props {
+  people: Person[];
+  selectedSlug: string | null;
+}
+
+export const PeopleTable: React.FC<Props> = ({ people, selectedSlug }) => {
+  const findPersonByName = (name: string | null) => {
+    if (!name) {
+      return null;
+    }
+
+    return people.find(person => person.name === name) || null;
+  };
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => {
+          const mother = findPersonByName(person.motherName);
+          const father = findPersonByName(person.fatherName);
+
+          return (
+            <tr
+              key={person.slug}
+              data-cy="person"
+              className={
+                person.slug === selectedSlug ? 'has-background-warning' : ''
+              }
+            >
+              <td>
+                <PersonLink person={person} />
+              </td>
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+              <td>{person.died}</td>
+              <td>
+                {mother ? (
+                  <PersonLink person={mother} />
+                ) : (
+                  person.motherName || '-'
+                )}
+              </td>
+              <td>
+                {father ? (
+                  <PersonLink person={father} />
+                ) : (
+                  person.fatherName || '-'
+                )}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { getPeople } from '../api';
+import { Person } from '../types/Person';
+import { PeopleTable } from '../components/PeopleTable';
+import { Loader } from '../components/Loader';
+
+interface Props {
+  selectedSlug: string | null;
+}
+
+export const PeoplePage: React.FC<Props> = ({ selectedSlug }) => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setError(true))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {loading && <Loader />}
+
+          {error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!loading && !error && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!loading && !error && people.length > 0 && (
+            <PeopleTable people={people} selectedSlug={selectedSlug} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
