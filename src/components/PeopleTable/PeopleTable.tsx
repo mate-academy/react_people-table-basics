80ee6657fb016ed4f19fbc6fa13003b91c5c1461
RomanHasiuk <romanhasiuk1@gmail.com>
2025-05-29 15:11:45 +0300

    add task solution

---
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -0,0 +1,85 @@
+import { Person } from '../../types/Person';
+import cn from 'classnames';
+interface Props {
+  people: Person[];
+  selectedSlug: string | null;
+  PersonLinkComponent: React.FC<{ person: Person | null }>;
+  getPersonByName: (name: string | null) => Person | null;
+}
+
+export const PeopleTable: React.FC<Props> = ({
+  people,
+  selectedSlug,
+  PersonLinkComponent,
+  getPersonByName,
+}) => {
+  return (
+    <div className="block">
+      {people.length === 0 && (
+        <p data-cy="noPeopleMessage">There are no people on the server</p>
+      )}
+
+      <div className="box table-container">
+      <table
+        data-cy="peopleTable"
+        className="table is-striped is-hoverable is-narrow is-fullwidth"
+      >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Sex</th>
+            <th>Born</th>
+            <th>Died</th>
+            <th>Mother</th>
+            <th>Father</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.map(person => (
+            <tr
+              key={person.slug}
+              data-cy="person"
+              className={cn({
+                'has-background-warning': selectedSlug === person.slug,
+              })}
+            >
+              <td>
+                <PersonLinkComponent person={person} />
+              </td>
+              <td>{person.sex}</td>
+              <td>{person.born}</td>
+              <td>{person.died}</td>
+              <td>
+                {person.motherName ? (
+                  getPersonByName(person.motherName) ? (
+                    <PersonLinkComponent
+                      person={getPersonByName(person.motherName)}
+                    />
+                  ) : (
+                    <span>{person.motherName}</span>
+                  )
+                ) : (
+                  '-'
+                )}
+              </td>
+              <td>
+                {person.fatherName ? (
+                  getPersonByName(person.fatherName) ? (
+                    <PersonLinkComponent
+                      person={getPersonByName(person.fatherName)}
+                    />
+                  ) : (
+                    <span>{person.fatherName}</span>
+                  )
+                ) : (
+                  '-'
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,51 @@
+import { useParams } from 'react-router-dom';
+import { getPeople } from '../api';
+import { useEffect, useState } from 'react';
+import { Person } from '../types/Person';
+import { Loader } from '../components/Loader';
+import { PersonLink } from '../components/PersonLink/PersonLink';
+import { PeopleTable } from '../components/PeopleTable/PeopleTable';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    getPeople()
+      .then(setPeople)
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  const getPersonByName = (name: string | null): Person | null => {
+    if (!name) {
+      return null;
+    }
+
+    return people.find(p => p.name === name) || null;
+  };
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      {isLoading && <Loader />}
+
+      {hasError && (
+        <p data-cy="peopleLoadingError" className="has-text-danger">
+          Something went wrong
+        </p>
+      )}
+      {!isLoading && !hasError && (
+        <PeopleTable
+          people={people}
+          selectedSlug={slug || ''}
+          PersonLinkComponent={PersonLink}
+          getPersonByName={getPersonByName}
+        />
+      )}
+    </>
+  );
+};
