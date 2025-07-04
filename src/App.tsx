94260d2b37f6226af406e68d85801b4bdcb7b85b
Lucas Mendes <164028753+14lucas-mendes@users.noreply.github.com>
2025-07-04 17:54:36 -0300

    implementado o componente Navigate e ajustado os links para mother e father

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,21 @@
 import { HomePage } from './components/HomePage/HomePage';
 import { PeoplePage } from './components/PeoplePage/PeoplePage';
-import { Error } from './components/Error/Error';
 import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 import './App.scss';
-import {
-  Link,
-  Route,
-  Routes,
-  useLocation,
-  useNavigate,
-} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import { Navigation } from './components/Navigation/Navigation';
 
 export const App = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
-  // Home fica ativo quando estamos na rota '/' (quando foi clicado)
-  const isActive =
-    location.pathname === '/' || location.pathname.startsWith('/people');
-
-  const handleHomeClick = () => {
-    navigate('/');
-  };
-
-  const handlePeopleClick = () => {
-    navigate('/people');
-  };
-
   return (
     <div data-cy="app">
-      <nav
-        data-cy="nav"
-        className="navbar is-fixed-top has-shadow"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link
-              to="/"
-              className={`navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`}
-              onClick={handleHomeClick}
-            >
-              Home
-            </Link>
-
-            <Link
-              to="/people"
-              className={`navbar-item ${isActive ? 'has-background-grey-lighter' : ''}`}
-              onClick={handlePeopleClick}
-            >
-              People
-            </Link>
-          </div>
-        </div>
-      </nav>
+      <Navigation />
 
       <main className="section">
         <div className="container">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/people" element={<PeoplePage />} />
-            <Route path="/people" element={<Error />} />
             <Route path="*" element={<NotFoundPage />} />
-            <Route path="/people/:slug" element={<PeoplePage />} />
           </Routes>
         </div>
       </main>
--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -9,62 +9,79 @@ export const PeopleTable = ({
   isLoading: boolean;
   people: Person[];
 }) => {
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (people.length === 0) {
+    return (
+      <>
+        <p data-cy="peopleLoadingError" className="has-text-danger">
+          Something went wrong
+        </p>
+        <p data-cy="noPeopleMessage">There are no people on the server</p>
+      </>
+    );
+  }
+
   return (
-    <>
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <table
-          data-cy="peopleTable"
-          className="table is-striped is-hoverable is-narrow is-fullwidth"
-        >
-          <thead>
-            <tr>
-              <th></th>
-              <th>Sex</th>
-              <th>Born</th>
-              <th>Died</th>
-              <th>Mother</th>
-              <th>Father</th>
-            </tr>
-          </thead>
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
 
-          <tbody>
-            {people.map(person => (
-              <tr
-                data-cy="person"
-                key={person.name}
-                className={
-                  person.name === person.slug ? 'has-background-warning' : ''
-                }
+      <tbody>
+        {people.map(person => (
+          <tr data-cy="person" key={person.name}>
+            <td>
+              <Link
+                to={`/people/${person.slug}`}
+                className={person.sex === 'f' ? 'has-text-danger' : ''}
               >
-                <td>
-                  <Link
-                    to={`/people/${person.slug}`}
-                    className={person.sex === 'f' ? 'has-text-danger' : ''}
-                  >
-                    {person.name}
-                  </Link>
-                </td>
+                {person.name}
+              </Link>
+            </td>
 
-                <td>{person.sex}</td>
-                <td>{person.born}</td>
-                <td>{person.died}</td>
-                {person.motherName === person.name ? (
-                  <Link to={`/people/${person.slug}`}>{person.motherName}</Link>
-                ) : (
-                  <td>{person.motherName || '-'}</td>
-                )}
-                {person.fatherName === person.name ? (
-                  <Link to={`/people/${person.slug}`}>{person.fatherName}</Link>
-                ) : (
-                  <td>{person.fatherName || '-'}</td>
-                )}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </>
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              {people.find(p => person.motherName === p.name) ? (
+                <Link
+                  to={`/people/${person.slug}`}
+                  className={
+                    person.sex ||
+                    people.find(p => p.name === person.motherName)?.sex === 'f'
+                      ? 'has-text-danger'
+                      : ''
+                  }
+                >
+                  {person.motherName}
+                </Link>
+              ) : (
+                person.motherName || '-'
+              )}
+            </td>
+            <td>
+              {people.find(p => person.fatherName === p.name) ? (
+                <Link to={`/people/${person.slug}`}>{person.fatherName}</Link>
+              ) : (
+                person.fatherName || '-'
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
