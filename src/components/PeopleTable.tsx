96a20127b9850845a800ee67234c150611a28c64
Sofiia13 <sophiyastanish@gmail.com>
2025-08-05 16:57:47 +0300

    add solution

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Person } from '../types';
+import { PersonLink } from './PersonLink';
+
+type Props = {
+  people: Person[];
+  selectedSlug?: string;
+};
+
+export const PeopleTable: React.FC<Props> = ({ people, selectedSlug }) => {
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            key={person.slug}
+            data-cy="person"
+            className={
+              selectedSlug === person.slug ? 'has-background-warning' : ''
+            }
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              {person.motherName ? (
+                person.mother ? (
+                  <PersonLink person={person.mother} />
+                ) : (
+                  person.motherName
+                )
+              ) : (
+                '-'
+              )}
+            </td>
+            <td>
+              {person.fatherName ? (
+                person.father ? (
+                  <PersonLink person={person.father} />
+                ) : (
+                  person.fatherName
+                )
+              ) : (
+                '-'
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
