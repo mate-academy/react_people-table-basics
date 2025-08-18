0d8dcf84adc0a61d0148eff36beff1a6dc2cf4d7
Viktoriia <viktoriamyhailiak@gmail.com>
2025-08-18 19:01:42 +0300

    add a solution

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from 'react';
+import { Loader } from './Loader';
+import { getPeople } from '../api';
+import { Person } from '../types';
+import { PeopleTable } from './PeopleTable';
+import { useParams } from 'react-router-dom';
+
+export const PeoplePage = () => {
+  const [error, setError] = useState(false);
+  const [people, setPeople] = useState<Person[] | []>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(result => {
+        setPeople(result);
+      })
+      .catch(() => setError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div className="section">
+      <div className="container">
+        <h1 className="title">People Page</h1>
+        <div className="block">
+          <div className="box table-container">
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <PeopleTable people={people} selected={slug?.toString() || ''} />
+            )}
+
+            {error && (
+              <p data-cy="peopleLoadingError" className="has-text-danger">
+                Something went wrong
+              </p>
+            )}
+
+            {people.length === 0 && !isLoading && (
+              <p data-cy="noPeopleMessage">There are no people on the server</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
