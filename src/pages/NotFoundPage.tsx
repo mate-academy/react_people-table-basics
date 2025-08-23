9333d54055da677b8e3ebe165129be4fed54f7bf
AG <uuuu07489@gmail.com>
2025-08-23 20:04:06 +0300

    fix

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export const NotFoundPage: React.FC = () => (
   <div className="p-8">
-    <h1 className="title">404 — Page Not Found</h1>
+    <h1 className="title">Page not found</h1>
     <p style={{ marginTop: 8 }}>
       <NavLink to="/">Go to Home</NavLink>
     </p>
