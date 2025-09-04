715fbb91b860eebe3075a40623126a86fe138ac3
David <davidhurmanchuk@gmail.com>
2025-09-04 19:54:34 +0300

    Solution

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,36 @@
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav
+      data-cy="nav"
+      className="navbar is-fixed-top has-shadow"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="container">
+        <div className="navbar-brand">
+          <NavLink
+            className={({ isActive }) =>
+              'navbar-item' + (isActive ? ' has-background-grey-lighter' : '')
+            }
+            to="/"
+          >
+            Home
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              'navbar-item' + (isActive ? ' has-background-grey-lighter' : '')
+            }
+            to="/people"
+          >
+            People
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
