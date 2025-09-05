147ea9a07f19a5d13248b88b33e7686564cd50c2
YuliaProkipchuk <yulayulayula2003@gmail.com>
2025-09-05 18:10:11 +0300

    update

--- src/components/PeopleTable/PeopleTable.tsx
+++ src/components/PeopleTable/PeopleTable.tsx
@@ -1,10 +1,14 @@
+import classNames from 'classnames';
 import { Person } from '../../types';
-import { PersonCell } from '../PersonCell';
+import { PersonLink } from '../PersonLink';
+import { useParams } from 'react-router-dom';
 
 type Props = {
   people: Person[];
 };
 export function PeopleTable({ people }: Props) {
+  const { personId } = useParams();
+
   return (
     <table
       data-cy="peopleTable"
@@ -23,7 +27,61 @@ export function PeopleTable({ people }: Props) {
 
       <tbody>
         {people.map(person => (
-          <PersonCell person={person} key={person.slug} />
+          <tr
+            key={person.slug}
+            data-cy="person"
+            className={classNames({
+              'has-background-warning': person.slug === personId,
+            })}
+          >
+            <td>
+              <PersonLink
+                person={{
+                  name: person.name,
+                  sex: person.sex,
+                  slug: person.slug,
+                }}
+              />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              {person.motherName ? (
+                person.mother ? (
+                  <PersonLink
+                    person={{
+                      name: person.motherName,
+                      sex: 'f',
+                      slug: person.mother?.slug,
+                    }}
+                  />
+                ) : (
+                  person.motherName
+                )
+              ) : (
+                '-'
+              )}
+            </td>
+            <td>
+              {person.fatherName ? (
+                person.father ? (
+                  <PersonLink
+                    person={{
+                      name: person.fatherName,
+                      sex: 'm',
+                      slug: person.father?.slug,
+                    }}
+                  />
+                ) : (
+                  person.fatherName
+                )
+              ) : (
+                '-'
+              )}
+            </td>
+          </tr>
         ))}
       </tbody>
     </table>
--- src/components/PersonLink/PersonLink.tsx
+++ src/components/PersonLink/PersonLink.tsx
@@ -3,24 +3,18 @@ import { Link } from 'react-router-dom';
 type Props = {
   person: {
     name: string;
-    slug: string | undefined;
+    slug: string;
     sex: string;
   };
 };
 
 export function PersonLink({ person }: Props) {
   return (
-    <td>
-      {person.slug ? (
-        <Link
-          to={person.slug}
-          className={`${person.sex === 'f' ? 'has-text-danger' : ''}`}
-        >
-          {person.name}
-        </Link>
-      ) : (
-        person.name
-      )}
-    </td>
+    <Link
+      to={person.slug}
+      className={`${person.sex === 'f' ? 'has-text-danger' : ''}`}
+    >
+      {person.name}
+    </Link>
   );
 }
