f526a44217206c759c8942149e2ef518bfc3f485
Назар ⠀ <nrxyouh@gmail.com>
2025-09-12 03:02:25 +0300

    solution

---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { Loader } from '../components/Loader';
+import { PeopleTable } from '../components/PeopleTable';
+import { useParams } from 'react-router-dom';
+
+export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const { slug } = useParams<{ slug: string }>();
+
+  useEffect(() => {
+    getPeople()
+      .then(data => {
+        setPeople(data);
+        setLoading(false);
+      })
+      .catch(() => {
+        setError(true);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return (
+      <p className="has-text-danger" data-cy="peopleLoadingError">
+        Something went wrong
+      </p>
+    );
+  }
+
+  if (!people.length) {
+    return <p data-cy="noPeopleMessage">There are no people on the server</p>;
+  }
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+      <PeopleTable people={people} selectedSlug={slug} />
+    </>
+  );
+};
