113fb8e58aad816ba8d85bed0a613e5805bad0a3
Paula Bezerra <paula.abezerra@gmail.com>
2025-09-22 11:08:43 -0300

    add task solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,30 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
+import { HomePage } from './components/HomePage';
+import { NotFoundPage } from './components/NotFoundPage';
+import { PeoplePage } from './components/PeoplePage';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <Router>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        {/* Deixe as duas rotas como irmãs, sem nesting em PeoplePage */}
+        <Route path="people" element={<PeoplePage />} />
+        <Route path="people/:slug" element={<PeoplePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   </Router>,
 );
