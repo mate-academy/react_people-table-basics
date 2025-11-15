40c2a25445ea08fef217a3b9e34b530cdc75e62c
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-11-15 17:57:36 +0200

    add task solution

---
+++ src/components/PeoplePage.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { Loader } from '../components/Loader';
+import { useParams } from 'react-router-dom';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { PeopleTable } from './PeopleTable';
+
+export const PeoplePage: React.FC = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const { slug } = useParams<{ slug: string }>();
+
+  useEffect(() => {
+    setLoading(true);
+    setError('');
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => {
+        setError('Something went wrong');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {loading && <Loader />}
+
+          {error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              {error}
+            </p>
+          )}
+
+          {!loading && !error && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!loading && !error && people.length > 0 && (
+            <PeopleTable people={people} selectedSlug={slug} />
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
