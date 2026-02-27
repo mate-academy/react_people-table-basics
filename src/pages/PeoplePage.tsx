7bb8d1f21008650a220156524ca7f3a34b40fe79
Valentyna Chernova <valentyna19960410@gmail.com>
2026-02-27 21:56:28 +0200

    add task solution

---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '../components/Loader';
+import { PeopleTable } from '../components/PeopleTable/PeopleTable';
+import { Person } from '../types';
+import { getPeople } from '../api';
+
+export const PeoplePage = () => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  useEffect(() => {
+    const loadPeople = async() => {
+      try {
+        const data = await getPeople();
+
+        setPeople(data);
+      } catch (error) {
+        setErrorMessage('Something went wrong');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadPeople();
+  });
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {loading && <Loader />}
+
+          {errorMessage && <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+          </p>
+          }
+
+          {people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          <PeopleTable people={people}/>
+        </div>
+      </div>
+    </>
+  );
+};
