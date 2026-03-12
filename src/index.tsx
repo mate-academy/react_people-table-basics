9e047bd750e14ed703b7a6933156d58b9bb958a3
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-03-12 13:56:46 +0200

    Extract routing logic into a new Root component

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-import { App } from './App';
-import { HomePage } from './components/HomePage';
-import { PeoplePage } from './components/PeoplePage';
-import { NotFoundPage } from './components/NotFoundPage';
+import { Root } from './Root';
 
-createRoot(document.getElementById('root') as HTMLDivElement).render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<HomePage />} />
-        <Route path="people/:peopleName?" element={<PeoplePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
-  </Router>,
-);
+createRoot(document.getElementById('root') as HTMLDivElement).render(<Root />);
