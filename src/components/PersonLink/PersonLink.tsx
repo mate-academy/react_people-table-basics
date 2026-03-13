9b75f65b8b689e3c74c2a6a816015b2fcc03d036
Mykhailo Podaniev <mpodaniev@gmail.com>
2026-03-13 10:12:20 +0200

    Update PersonLink to include `/people/` prefix and apply conditional styling

--- src/components/PersonLink/PersonLink.tsx
+++ src/components/PersonLink/PersonLink.tsx
@@ -1,10 +1,16 @@
 import { Person } from '../../types';
 import { Link } from 'react-router-dom';
+import classNames from 'classnames';
 
 type PersonLinkProps = {
   person: Person;
 };
 
-export const PersonLink = ({ person }: PersonLinkProps) => {
-  return <Link to={person.slug}>{person.name}</Link>;
-};
+export const PersonLink = ({ person }: PersonLinkProps) => (
+  <Link
+    to={`/people/${person.slug}`}
+    className={classNames({ 'has-text-danger': person.sex === 'f' })}
+  >
+    {person.name}
+  </Link>
+);
