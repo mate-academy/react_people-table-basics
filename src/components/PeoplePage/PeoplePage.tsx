e568efc82c5da6ef3a77b4e106a6ce62488ab4c6
Yar14k <tabachuk1406@gmail.com>
2026-03-17 20:00:48 +0100

    Add solution

---
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { Loader } from '../Loader';
+import { useParams } from 'react-router-dom';
+import PeopleTable from './PeopleTable';
+import { Person } from '../../types/Person';
+import { getPeople } from '../../api';
+
+const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<boolean>(false);
+
+  const { slug } = useParams();
+
+  useEffect(() => {
+    getPeople()
+      .then(data => setPeople(data))
+      .catch(() => setError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="title">People Page</h1>
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && <Loader />}
+
+          {error && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!isLoading && !error && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!isLoading && !error && people.length > 0 && (
+            <PeopleTable people={people} selectedSlug={slug} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PeoplePage;
---
+++ src/components/PeoplePage/PeopleTable.tsx
@@ -0,0 +1,69 @@
+import PersonLink from './PersonLink';
+import { Person } from '../../types';
+
+type PeopleTableProps = {
+  people: Person[];
+  selectedSlug: string | null | undefined;
+};
+
+const PeopleTable = ({ people, selectedSlug }: PeopleTableProps) => {
+  return (
+    <table
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+      data-cy="peopleTable"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr
+            data-cy="person"
+            key={person.slug}
+            className={
+              person.slug === selectedSlug ? 'has-background-warning' : ''
+            }
+          >
+            <td>
+              <PersonLink person={person} />
+            </td>
+
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+
+            <td>
+              {people.find(p => p.name === person.motherName) ? (
+                <PersonLink
+                  person={people.find(p => p.name === person.motherName)!}
+                />
+              ) : (
+                person.motherName || '-'
+              )}
+            </td>
+
+            <td>
+              {people.find(p => p.name === person.fatherName) ? (
+                <PersonLink
+                  person={people.find(p => p.name === person.fatherName)!}
+                />
+              ) : (
+                person.fatherName || '-'
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default PeopleTable;
