42dd04342561bbba537d34a53d8498d103b1c77b
bcheban <128683111+bcheban@users.noreply.github.com>
2026-04-27 19:41:18 +0300

    done

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,30 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter as Router } from 'react-router-dom';
+import {
+  HashRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
 import { App } from './App';
+import { HomePage } from './pages/HomePage';
+import People from './pages/People';
+import { NotFoundPage } from './pages/NotFoundPage';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
   <Router>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="people">
+          <Route path=":slug?" element={<People />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   </Router>,
 );
