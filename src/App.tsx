2f0f827acaa692144222a2252f4e4f7475857684
Vlad Chudin <chudintttks@gmail.com>
2026-05-11 16:13:54 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,27 @@ import { Navbar } from './components/Navbar';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { HomePage } from './components/HomePage';
 import { PeoplePage } from './components/PeoplePage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { HashRouter } from 'react-router-dom';
 
 import './App.scss';
 
 export const App = () => (
-  <div data-cy="app">
-    <Navbar />
+  <HashRouter>
+    <div data-cy="app">
+      <Navbar />
 
-    <main className="section">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/home" element={<Navigate to="/" replace />}></Route>
-          <Route path="people" element={<PeoplePage />} />
-          <Route path="people/:slug" element={<PeoplePage />} />
-          <Route
-            path="*"
-            element={<h1 className="title">Page not found</h1>}
-          ></Route>
-        </Routes>
-      </div>
-    </main>
-  </div>
+      <main className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />}></Route>
+            <Route path="/home" element={<Navigate to="/" replace />}></Route>
+            <Route path="people" element={<PeoplePage />} />
+            <Route path="people/:slug" element={<PeoplePage />} />
+            <Route path="*" element={<NotFoundPage />}></Route>
+          </Routes>
+        </div>
+      </main>
+    </div>
+  </HashRouter>
 );
