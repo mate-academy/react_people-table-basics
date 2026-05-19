888650ab8c637df8a581ff073f20d6dfaa68b580
freelinex <freelinex7@gmail.com>
2026-05-19 18:28:01 +0300

    add task solution

---
+++ src/components/Loader/PeopleTable.tsx
@@ -0,0 +1,79 @@
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
+import { Person } from '../../types';
+import { PersonLink } from './PersonLink';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable = ({ people }: Props) => {
+  const { slug } = useParams();
+
+  return (
+    <div className="block">
+      <table
+        data-cy="peopleTable"
+        className="table is-striped is-hoverable is-narrow is-fullwidth"
+      >
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Sex</th>
+            <th>Born</th>
+            <th>Died</th>
+            <th>Mother</th>
+            <th>Father</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {people.map(person => {
+            const mother = people.find(
+              currentPerson => currentPerson.name === person.motherName,
+            );
+            const father = people.find(
+              currentPerson => currentPerson.name === person.fatherName,
+            );
+
+            return (
+              <tr
+                data-cy="person"
+                key={person.slug}
+                className={classNames({
+                  'has-background-warning': slug === person.slug,
+                })}
+              >
+                <td>
+                  <PersonLink person={person} />
+                </td>
+
+                <td>{person.sex}</td>
+                <td>{person.born}</td>
+                <td>{person.died}</td>
+                <td>
+                  {!person.motherName ? (
+                    '-'
+                  ) : mother ? (
+                    <PersonLink person={mother} />
+                  ) : (
+                    person.motherName
+                  )}
+                </td>
+                <td>
+                  {!person.fatherName ? (
+                    '-'
+                  ) : father ? (
+                    <PersonLink person={father} />
+                  ) : (
+                    person.fatherName
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
