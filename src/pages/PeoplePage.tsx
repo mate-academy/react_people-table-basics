ce669a7ca7928ecb45f7d0b1bf1ceec3ce7b7823
inikiforov <ievgen.nikiforov@tedata.de>
2026-05-27 10:20:01 +0200

    solution

---
+++ src/pages/PeoplePage.tsx
@@ -0,0 +1,105 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { getPeople } from '../api';
+import { Loader } from '../components/Loader';
+import { PersonLink } from '../components/PersonLink';
+import type { Person } from '../types';
+
+export default function PeoplePage() {
+  const { slug } = useParams();
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getPeople()
+      .then(data => {
+        setPeople(data);
+      })
+      .catch(() => {
+        setHasError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
+  const getPersonByName = (name: string | null) =>
+    people.find(person => person.name === name);
+
+  const renderParent = (parentName: string | null) => {
+    if (!parentName) {
+      return '-';
+    }
+
+    const parent = getPersonByName(parentName);
+
+    return parent ? <PersonLink person={parent} /> : parentName;
+  };
+
+  return (
+    <>
+      <h1 className="title">People Page</h1>
+
+      <div className="block">
+        <div className="box table-container">
+          {isLoading && <Loader />}
+
+          {hasError && (
+            <p data-cy="peopleLoadingError" className="has-text-danger">
+              Something went wrong
+            </p>
+          )}
+
+          {!isLoading && !hasError && people.length === 0 && (
+            <p data-cy="noPeopleMessage">There are no people on the server</p>
+          )}
+
+          {!isLoading && !hasError && people.length > 0 && (
+            <table
+              data-cy="peopleTable"
+              className="table is-striped is-hoverable is-narrow is-fullwidth"
+            >
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Sex</th>
+                  <th>Born</th>
+                  <th>Died</th>
+                  <th>Mother</th>
+                  <th>Father</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {people.map(person => {
+                  return (
+                    <tr
+                      key={person.slug}
+                      data-cy="person"
+                      className={
+                        person.slug === slug ? 'has-background-warning' : ''
+                      }
+                    >
+                      <td>
+                        <PersonLink person={person} />
+                      </td>
+                      <td>{person.sex}</td>
+                      <td>{person.born}</td>
+                      <td>{person.died}</td>
+                      <td>{renderParent(person.motherName)}</td>
+                      <td>{renderParent(person.fatherName)}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
