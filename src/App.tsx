c24289f29c1c7e92e6af7b818211d86c5273b4e7
Andii Karamushko <karamushko1997@gmail.com>
2026-06-02 16:48:30 +0300

    changed HashRouterPosition

--- src/App.tsx
+++ src/App.tsx
@@ -2,59 +2,62 @@ import { Navigate, Routes, Route, NavLink } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
 import { PeoplePage } from './pages/PeoplePage';
 import { NotFoundPage } from './pages/NotFoundPage';
+import { HashRouter } from 'react-router-dom';
 
 import './App.scss';
 
 export const App = () => {
   return (
-    <div data-cy="app">
-      <nav
-        data-cy="nav"
-        className="navbar is-fixed-top has-shadow"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? 'has-background-grey-lighter' : ''
-              }
-              to="/"
-              end
-            >
-              Home
-            </NavLink>
-            <NavLink
-              className={({ isActive }) =>
-                isActive ? 'has-background-grey-lighter' : ''
-              }
-              to="/people"
-            >
-              People
-            </NavLink>
+    <HashRouter>
+      <div data-cy="app">
+        <nav
+          data-cy="nav"
+          className="navbar is-fixed-top has-shadow"
+          role="navigation"
+          aria-label="main navigation"
+        >
+          <div className="container">
+            <div className="navbar-brand">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? 'has-background-grey-lighter' : ''
+                }
+                to="/"
+                end
+              >
+                Home
+              </NavLink>
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? 'has-background-grey-lighter' : ''
+                }
+                to="/people"
+              >
+                People
+              </NavLink>
+            </div>
           </div>
-        </div>
-      </nav>
-      <main className="section">
-        <div className="container">
-          <Routes>
-            <Route path="/home" element={<Navigate to={'/'} replace />} />
-            <Route path="/" element={<HomePage title={'Home Page'} />} />
-            <Route path="/people">
-              <Route index element={<PeoplePage title={'People Page'} />} />
+        </nav>
+        <main className="section">
+          <div className="container">
+            <Routes>
+              <Route path="/home" element={<Navigate to={'/'} replace />} />
+              <Route path="/" element={<HomePage title={'Home Page'} />} />
+              <Route path="/people">
+                <Route index element={<PeoplePage title={'People Page'} />} />
+                <Route
+                  path=":slug"
+                  element={<PeoplePage title={'People Page'} />}
+                />
+              </Route>
               <Route
-                path=":slug"
-                element={<PeoplePage title={'People Page'} />}
+                path="*"
+                element={<NotFoundPage title={'Page not found'} />}
               />
-            </Route>
-            <Route
-              path="*"
-              element={<NotFoundPage title={'Page not found'} />}
-            />
-          </Routes>
-        </div>
-      </main>
-    </div>
+            </Routes>
+          </div>
+        </main>
+      </div>
+    </HashRouter>
   );
 };
