e630122c8c766a77e19ec7919db82d3a14b9edfe
masteryada378 <masteryada378@gmail.com>
2026-07-10 12:37:14 +0300

    table basics

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,51 @@
+import { useParams } from 'react-router-dom';
+import { Person } from '../types';
+import { PersonLink } from './PersonLink';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable = ({ people }: Props) => {
+  const { slug } = useParams();
+
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+      <tbody>
+        {people.map(person => (
+          <tr
+            key={person.slug}
+            data-cy="person"
+            className={person.slug === slug ? 'has-background-warning' : ''}
+          >
+            <td>
+              <PersonLink name={person.name} people={people} />
+            </td>
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>
+              <PersonLink name={person.motherName} people={people} />
+            </td>
+            <td>
+              <PersonLink name={person.fatherName} people={people} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
---
+++ src/components/PersonLink.tsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import { Person } from '../types';
+
+type Props = {
+  name: string | null;
+  people: Person[];
+};
+
+export const PersonLink = ({ name, people }: Props) => {
+  if (!name) {
+    return <>-</>;
+  }
+
+  const person = people.find(p => p.name === name);
+
+  if (!person) {
+    return <>{name}</>;
+  }
+
+  return (
+    <Link
+      to={`/people/${person.slug}`}
+      className={person.sex === 'f' ? 'has-text-danger' : ''}
+    >
+      {name}
+    </Link>
+  );
+};
