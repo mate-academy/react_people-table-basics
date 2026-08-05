24360ff3785c40fe7745949b2e78584953b484d7
student1232354 <tarasromanenko33@gmail.com>
2026-08-05 10:41:16 +0300

    third update

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,55 @@
 import React, { useState, useEffect } from 'react';
-import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
-import cn from 'classnames';
-import { Loader } from './components/Loader';
-import { PeopleTable } from './components/Loader/arrayList/list';
+import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
 import { getPeople } from './api';
 import { Person } from './types/Person';
+import { NotFoundPage } from './components/Loader/NotFoundPage';
+import { PeoplePage } from './components/Loader/PeoplePage';
+import { HomePage } from './components/Loader/HomePage';
+import { Navbar } from './Navbar';
+
 import './App.scss';
 
 export const App: React.FC = () => {
-  const [todos, setTodos] = useState<Person[]>([]);
+  const [people, setPeople] = useState<Person[]>([]);
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     getPeople()
-      .then(setTodos)
+      .then(setPeople)
       .catch(() => setErrorMessage(true))
       .finally(() => setLoading(false));
   }, []);
 
   return (
-    <div data-cy="app">
-      <nav
-        data-cy="nav"
-        className="navbar is-fixed-top has-shadow"
-        role="navigation"
-        aria-label="main navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <NavLink
-              className={({ isActive }) =>
-                cn('navbar-item', { 'has-background-grey-lighter': isActive })
-              }
-              to="/"
-            >
-              Home
-            </NavLink>
+    <HashRouter>
+      <div data-cy="app">
+        <Navbar />
+        <main className="section">
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/home" element={<Navigate to="/" replace />} />
 
-            <NavLink
-              className={({ isActive }) =>
-                cn('navbar-item', { 'has-background-grey-lighter': isActive })
-              }
-              to="/people"
-            >
-              People
-            </NavLink>
+              <Route path="/people">
+                <Route
+                  path=":slug?"
+                  element={
+                    <PeoplePage
+                      people={people}
+                      loading={loading}
+                      errorMessage={errorMessage}
+                    />
+                  }
+                />
+              </Route>
+
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
           </div>
-        </div>
-      </nav>
-
-      <main className="section">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<h1 className="title">Home Page</h1>} />
-            <Route path="/home" element={<Navigate to="/" replace />} />
-
-            <Route path="/people">
-              <Route
-                path=":slug?"
-                element={
-                  <>
-                    <h1 className="title">People Page</h1>
-                    <div className="block">
-                      <div className="box table-container">
-                        {loading && <Loader />}
-
-                        {errorMessage && (
-                          <p
-                            data-cy="peopleLoadingError"
-                            className="has-text-danger"
-                          >
-                            Something went wrong
-                          </p>
-                        )}
-
-                        {!loading && !errorMessage && todos.length === 0 && (
-                          <p data-cy="noPeopleMessage">
-                            There are no people on the server
-                          </p>
-                        )}
-
-                        {!loading && !errorMessage && todos.length > 0 && (
-                          <table
-                            data-cy="peopleTable"
-                            /* eslint-disable-next-line */
-                              className="table is-striped is-hoverable is-narrow is-fullwidth"
-                          >
-                            <thead>
-                              <tr>
-                                <th>Name</th>
-                                <th>Sex</th>
-                                <th>Born</th>
-                                <th>Died</th>
-                                <th>Mother</th>
-                                <th>Father</th>
-                              </tr>
-                            </thead>
-                            <PeopleTable people={todos} />
-                          </table>
-                        )}
-                      </div>
-                    </div>
-                  </>
-                }
-              />
-            </Route>
-
-            <Route
-              path="*"
-              element={<h1 className="title">Page not found</h1>}
-            />
-          </Routes>
-        </div>
-      </main>
-    </div>
+        </main>
+      </div>
+    </HashRouter>
   );
 };
--- src/components/Loader/arrayList/list.tsx
+++ src/components/Loader/arrayList/list.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Person } from '../../../types';
-import { Link as PersonLink, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import cn from 'classnames';
+import { Person } from '../../../types';
+import { PersonLink } from '../PersonLink';
 
 interface Props {
   people: Person[];
@@ -12,63 +13,43 @@ export const PeopleTable: React.FC<Props> = ({ people }) => {
 
   return (
     <tbody>
-      {people.map(cobj => {
-        const isSelected = cobj.slug === slug;
+      {people.map(person => {
+        const isSelected = person.slug === slug;
 
-        const motherPerson = people.find(
-          person => person.name === cobj.motherName,
-        );
+        const motherPerson = people.find(p => p.name === person.motherName);
 
-        const fatherPerson = people.find(
-          person => person.name === cobj.fatherName,
-        );
+        const fatherPerson = people.find(p => p.name === person.fatherName);
 
         return (
           <tr
-            key={cobj.slug}
+            key={person.slug}
             data-cy="person"
             className={cn({ 'has-background-warning': isSelected })}
           >
             <td>
-              <PersonLink
-                className={cn({
-                  'has-text-danger': cobj.sex === 'f',
-                })}
-                to={`/people/${cobj.slug}`}
-              >
-                {cobj.name}
-              </PersonLink>
+              <PersonLink person={person} />
             </td>
 
-            <td>{cobj.sex}</td>
-            <td>{cobj.born}</td>
-            <td>{cobj.died}</td>
-
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
             <td>
-              {cobj.motherName ? (
+              {person.motherName ? (
                 motherPerson ? (
-                  <PersonLink
-                    className="has-text-danger"
-                    to={`/people/${motherPerson.slug}`}
-                  >
-                    {cobj.motherName}
-                  </PersonLink>
+                  <PersonLink person={motherPerson} />
                 ) : (
-                  cobj.motherName
+                  person.motherName
                 )
               ) : (
                 '-'
               )}
             </td>
-
             <td>
-              {cobj.fatherName ? (
+              {person.fatherName ? (
                 fatherPerson ? (
-                  <PersonLink to={`/people/${fatherPerson.slug}`}>
-                    {cobj.fatherName}
-                  </PersonLink>
+                  <PersonLink person={fatherPerson} />
                 ) : (
-                  cobj.fatherName
+                  person.fatherName
                 )
               ) : (
                 '-'
