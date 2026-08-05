4f3d88a3f202d275996ca7cd2512814c6a407315
unknown <carlos.diaz@beeconsultoria.com>
2026-08-04 21:11:04 -0500

    fix: wrap app routes with HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  HashRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 import { Navigation } from './components/Navigation';
 import { HomePage } from './pages/HomePage';
@@ -8,19 +13,21 @@ import { PeoplePage } from './pages/PeoplePage';
 import './App.scss';
 
 export const App = () => (
-  <div data-cy="app">
-    <Navigation />
+  <HashRouter>
+    <div data-cy="app">
+      <Navigation />
 
-    <main className="section">
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/home" element={<Navigate to="/" replace />} />
-          <Route path="/people" element={<PeoplePage />} />
-          <Route path="/people/:slug" element={<PeoplePage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
-    </main>
-  </div>
+      <main className="section">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<Navigate to="/" replace />} />
+            <Route path="/people" element={<PeoplePage />} />
+            <Route path="/people/:slug" element={<PeoplePage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </main>
+    </div>
+  </HashRouter>
 );
