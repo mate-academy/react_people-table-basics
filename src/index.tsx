e89ec49bd90f08f4380c01670d9dba66001d0446
Vadim <sergiyivanskiy@gmail.com>
2026-08-28 15:17:49 +0300

    implement people table routing

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { HashRouter as Router } from 'react-router-dom';
 
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-
 import { Root } from './root/Root';
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
