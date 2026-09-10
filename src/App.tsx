36e703535204fe489d992a99533c8e84c1fdcf3f
Jhonatan <jvasconcelosbittencourt@gmail.com>
2026-09-10 16:00:45 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { Loader } from './components/Loader';
-import { useEffect, useState } from 'react';
-import { Routes, Route, Navigate, NavLink, Link } from 'react-router-dom';
+import { Routes, Route, Navigate, NavLink } from 'react-router-dom';
 
 import './App.scss';
 import Home from './Home';
 import PeoplePage from './PeoplePage';
-import { getPeople } from './api';
-import { Person } from './types';
 
 export const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [database, setDatabase] = useState<Person[]>([]);
-
-  useEffect(() => {
-    getPeople()
-      .then(data => setDatabase(data))
-      // eslint-disable-next-line no-console
-      .catch(err => console.error(err))
-      .finally(() => setIsLoading(false));
-  }, []);
-
   return (
     <>
       <div data-cy="app">
@@ -31,12 +16,23 @@ export const App = () => {
         >
           <div className="container">
             <div className="navbar-brand">
-              <NavLink className="navbar-item" to="/">
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive
+                    ? 'navbar-item has-background-grey-lighter'
+                    : 'navbar-item'
+                }
+              >
                 Home
               </NavLink>
 
               <NavLink
-                className="navbar-item has-background-grey-lighter"
+                className={({ isActive }) =>
+                  isActive
+                    ? 'navbar-item has-background-grey-lighter'
+                    : 'navbar-item'
+                }
                 to="/people"
               >
                 People
@@ -44,61 +40,6 @@ export const App = () => {
             </div>
           </div>
         </nav>
-
-        <main className="section">
-          <div className="container">
-            <div className="block">
-              <div className="box table-container">
-                {isLoading && <Loader />}
-
-                <p data-cy="peopleLoadingError" className="has-text-danger">
-                  Something went wrong
-                </p>
-
-                <p data-cy="noPeopleMessage">
-                  There are no people on the server
-                </p>
-
-                <table
-                  data-cy="peopleTable"
-                  className="table is-striped is-hoverable 
-                  is-narrow is-fullwidth"
-                >
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                      <th>Sex</th>
-                      <th>Born</th>
-                      <th>Died</th>
-                      <th>Mother</th>
-                      <th>Father</th>
-                    </tr>
-                  </thead>
-
-                  <tbody>
-                    {database.map(person => {
-                      return (
-                        <tr data-cy="person" key={person.slug}>
-                          <td>
-                            <Link to={`/people/${person.slug}`}>
-                              {person.name}
-                            </Link>
-                          </td>
-
-                          <td>{person.sex}</td>
-                          <td>{person.born}</td>
-                          <td>{person.died}</td>
-                          <td>{person.motherName || '-'}</td>
-                          <td>{person.fatherName || '-'}</td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-        </main>
       </div>
 
       <div className="section">
--- src/PeoplePage.tsx
+++ src/PeoplePage.tsx
@@ -1,8 +1,84 @@
+import { Link } from 'react-router-dom';
+
+import { Loader } from './components/Loader';
+import { useEffect, useState } from 'react';
+import { getPeople } from './api';
+import { Person } from './types';
+
 const PeoplePage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [database, setDatabase] = useState<Person[]>([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    getPeople()
+      .then(data => setDatabase(data))
+      .catch(err => setError(err))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <main className="section">
       <div className="container">
         <h1 className="title">People Page</h1>
+        <main className="section">
+          <div className="container">
+            <div className="block">
+              <div className="box table-container">
+                {isLoading && <Loader />}
+
+                {error.length !== 0 && (
+                  <p data-cy="peopleLoadingError" className="has-text-danger">
+                    Something went wrong
+                  </p>
+                )}
+
+                {database.length === 0 && (
+                  <p data-cy="noPeopleMessage">
+                    There are no people on the server
+                  </p>
+                )}
+
+                <table
+                  data-cy="peopleTable"
+                  className="table is-striped is-hoverable 
+                  is-narrow is-fullwidth"
+                >
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Sex</th>
+                      <th>Born</th>
+                      <th>Died</th>
+                      <th>Mother</th>
+                      <th>Father</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    {database.map(person => {
+                      return (
+                        <tr data-cy="person" key={person.slug}>
+                          <td>
+                            <Link to={`/people/${person.slug}`}>
+                              {person.name}
+                            </Link>
+                          </td>
+
+                          <td>{person.sex}</td>
+                          <td>{person.born}</td>
+                          <td>{person.died}</td>
+                          <td>{person.motherName || '-'}</td>
+                          <td>{person.fatherName || '-'}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </main>
       </div>
     </main>
   );
